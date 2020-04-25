ace32e2378a137e747e0d255aca3da2c71c86f2a
Andrew Countryman <apcountryman@gmail.com>
2020-04-25 14:48:35 -0700

    Add operation result wrapper for cases where no information is generated and the operation cannot fail
    Supports #32.

--- include/picolibrary/result.h
+++ include/picolibrary/result.h
@@ -24,6 +24,8 @@
 
 #include <type_traits>
 
+#include "picolibrary/utility.h"
+
 namespace picolibrary {
 
 /**
@@ -34,6 +36,106 @@ namespace picolibrary {
  */
 template<typename Value_Type, typename Error_Type, bool = std::is_trivially_destructible_v<Value_Type>>
 class Result;
+
+/**
+ * \brief Operation result wrapper specialized for cases where no information is
+ *        generated, and the operation cannot fail.
+ */
+template<>
+class [[nodiscard]] Result<Void, Void, true>
+{
+  public:
+    /**
+     * \brief Operation succeeded result type.
+     */
+    using Value = Void;
+
+    /**
+     * \brief Operation failed result type.
+     */
+    using Error = Void;
+
+    /**
+     * \brief Constructor.
+     */
+    constexpr Result() noexcept = default;
+
+    /**
+     * \brief Constructor.
+     */
+    constexpr Result( Void ) noexcept
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Result( Result && source ) noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] original The original to copy.
+     */
+    constexpr Result( Result const & original ) noexcept = default;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Result() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Result && expression ) noexcept->Result & = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Result const & expression ) noexcept->Result & = default;
+
+    /**
+     * \brief Check if the operation result is a value (operation succeeded).
+     *
+     * \return true (operation succeeded).
+     */
+    [[nodiscard]] constexpr auto is_value() const noexcept
+    {
+        return true;
+    }
+
+    /**
+     * \brief Check if the operation result is an error (operation failed).
+     *
+     * \return false (operation succeeded).
+     */
+    [[nodiscard]] constexpr auto is_error() const noexcept
+    {
+        return false;
+    }
+
+    /**
+     * \brief Access the result of a failed operation.
+     *
+     * \attention This function is only provided for interface consistency.
+     *
+     * \return picolibrary::Void.
+     */
+    [[nodiscard]] constexpr auto error() const noexcept
+    {
+        return Void{};
+    }
+};
 
 } // namespace picolibrary
 
