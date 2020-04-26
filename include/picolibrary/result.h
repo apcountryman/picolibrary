e6d7016fb9a20728eee38e6a89d68cd2f289b015
Andrew Countryman <apcountryman@gmail.com>
2020-04-26 11:34:37 -0700

    Add operation result wrapper for cases where no information is generated and operation can fail
    Supports #32.

--- include/picolibrary/result.h
+++ include/picolibrary/result.h
@@ -22,11 +22,25 @@
 #ifndef PICOLIBRARY_RESULT_H
 #define PICOLIBRARY_RESULT_H
 
+#include <new>
 #include <type_traits>
+#include <utility>
 
+#include "picolibrary/error.h"
 #include "picolibrary/utility.h"
 
 namespace picolibrary {
+
+/**
+ * \brief picolibrary::Result error constructor tag.
+ */
+struct Error_Tag {
+};
+
+/**
+ * \brief picolibrary::Result error constructor tag.
+ */
+constexpr auto ERROR = Error_Tag{};
 
 /**
  * \brief Operation result wrapper.
@@ -42,7 +56,7 @@ class Result;
  *        generated, and the operation cannot fail.
  */
 template<>
-class [[nodiscard]] Result<Void, Void, true>
+class [[nodiscard]] Result<Void, Void, true> final
 {
   public:
     /**
@@ -135,6 +149,252 @@ class [[nodiscard]] Result<Void, Void, true>
     {
         return Void{};
     }
+};
+
+/**
+ * \brief Operation result wrapper specialized for cases where no information is
+ *        generated, and the operation can fail.
+ */
+template<>
+class [[nodiscard]] Result<Void, Error_Code, true> final
+{
+  public:
+    /**
+     * \brief Operation succeeded result type.
+     */
+    using Value = Void;
+
+    /**
+     * \brief Operation failed result type.
+     */
+    using Error = Error_Code;
+
+    /**
+     * \brief Constructor.
+     */
+    constexpr Result() noexcept : m_is_value{ true }, m_value{}
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \tparam E A type implicitly convertible to Error.
+     *
+     * \param[in] error The object to construct from.
+     */
+    template<typename E, typename = typename std::enable_if_t<not std::is_same_v<std::decay_t<E>, Result> and std::is_convertible_v<E, Error>>>
+    constexpr Result( E && error, Error_Tag = {} ) noexcept :
+        m_is_value{ false },
+        m_error{ std::forward<E>( error ) }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \tparam Arguments Error constructor argument types.
+     *
+     * \param[in] arguments Error constructor arguments.
+     */
+    template<typename... Arguments>
+    constexpr Result( Error_Tag, Arguments && ... arguments ) noexcept :
+        m_is_value{ false },
+        m_error{ std::forward<Arguments>( arguments )... }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Result( Result && source ) noexcept : m_is_value{ source.m_is_value }
+    {
+        if ( not m_is_value ) {
+            new ( &m_error ) Error{ std::move( source.m_error ) };
+        } // if
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] original The original to copy.
+     */
+    constexpr Result( Result const & original ) noexcept :
+        m_is_value{ original.m_is_value }
+    {
+        if ( not m_is_value ) { new ( &m_error ) Error{ original.m_error }; } // if
+    }
+
+    /**
+     * \brief Destructor.
+     */
+    ~Result() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto & operator=( Result && expression ) noexcept
+    {
+        if ( m_is_value == expression.m_is_value ) {
+            if ( not m_is_value ) { m_error = std::move( expression.m_error ); } // if
+        } else {
+            if ( m_is_value ) {
+                new ( &m_error ) Error{ std::move( expression.m_error ) };
+            } // if
+
+            m_is_value = expression.m_is_value;
+        } // else
+
+        return *this;
+    }
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto & operator=( Result const & expression ) noexcept
+    {
+        if ( m_is_value == expression.m_is_value ) {
+            if ( not m_is_value ) { m_error = expression.m_error; } // if
+        } else {
+            if ( m_is_value ) { new ( &m_error ) Error{ expression.m_error }; } // if
+
+            m_is_value = expression.m_is_value;
+        } // else
+
+        return *this;
+    }
+
+    /**
+     * \brief Check if the operation result is a value (operation succeeded).
+     *
+     * \return true if the operation result is a value (operation succeeded).
+     * \return false if the operation result is not a value (operation failed).
+     */
+    [[nodiscard]] constexpr auto is_value() const noexcept
+    {
+        return m_is_value;
+    }
+
+    /**
+     * \brief Check if the operation result is an error (operation failed).
+     *
+     * \return true if the operation result is an error (operation failed).
+     * \return false if the operation result is not an error (operation succeeded).
+     */
+    [[nodiscard]] constexpr auto is_error() const noexcept
+    {
+        return not is_value();
+    }
+
+    /**
+     * \brief Access the result of a failed operation.
+     *
+     * \pre The operation failed.
+     *
+     * \warning Calling this function on the result of a successful operation results in
+     *          undefined behavior.
+     *
+     * \return The result error.
+     */
+    [[nodiscard]] constexpr auto error() && noexcept
+    {
+        return static_cast<Error &&>( m_error );
+    }
+
+    /**
+     * \brief Access the result of a failed operation.
+     *
+     * \pre The operation failed.
+     *
+     * \warning Calling this function on the result of a successful operation results in
+     *          undefined behavior.
+     *
+     * \return The result error.
+     */
+    [[nodiscard]] constexpr auto error() const && noexcept
+    {
+        return static_cast<Error const &&>( m_error );
+    }
+
+    /**
+     * \brief Access the result of a failed operation.
+     *
+     * \pre The operation failed.
+     *
+     * \warning Calling this function on the result of a successful operation results in
+     *          undefined behavior.
+     *
+     * \return The result error.
+     */
+    [[nodiscard]] constexpr auto error() & noexcept
+    {
+        return static_cast<Error &>( m_error );
+    }
+
+    /**
+     * \brief Access the result of a failed operation.
+     *
+     * \pre The operation failed.
+     *
+     * \warning Calling this function on the result of a successful operation results in
+     *          undefined behavior.
+     *
+     * \return The result error.
+     */
+    [[nodiscard]] constexpr auto error() const & noexcept
+    {
+        return static_cast<Error const &>( m_error );
+    }
+
+  private:
+    /**
+     * \brief Result type flag.
+     */
+    bool m_is_value{ true };
+
+    /**
+     * \cond ANONYMOUS_MEMBER
+     *
+     * \todo #33
+     */
+    union {
+        /**
+         * \todo #33
+         *
+         * \endcond
+         */
+
+        /**
+         * \brief Operation succeeded result.
+         */
+        Value m_value{};
+
+        /**
+         * \brief Operation failed result.
+         */
+        Error m_error;
+
+        /**
+         * \cond ANONYMOUS_MEMBER
+         *
+         * \todo #33
+         */
+    };
+    /**
+     * \todo #33
+     *
+     * \endcond
+     */
 };
 
 } // namespace picolibrary
