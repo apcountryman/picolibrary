77f7c602939339e0f4009c92e96ec0455548835f
Andrew Countryman <apcountryman@gmail.com>
2020-07-03 15:25:38 -0700

    Add mock functor
    Supports #51.

--- include/picolibrary/testing/unit/functor.h
+++ include/picolibrary/testing/unit/functor.h
@@ -22,7 +22,172 @@
 #ifndef PICOLIBRARY_TESTING_UNIT_FUNCTOR_H
 #define PICOLIBRARY_TESTING_UNIT_FUNCTOR_H
 
+#include <utility>
+
+#include "gmock/gmock.h"
+
 namespace picolibrary::Testing::Unit {
+
+/**
+ * \brief Mock functor.
+ *
+ * \tparam Return_Type The functor's return type.
+ * \tparam Argument_Types The functor's argument types.
+ */
+template<typename Return_Type, typename... Argument_Types>
+class Mock_Functor {
+  public:
+    /**
+     * \brief Movable, copyable mock functor handle.
+     */
+    class Handle {
+      public:
+        /**
+         * \brief Constructor.
+         */
+        Handle() noexcept = default;
+
+        /**
+         * \brief Constructor.
+         *
+         * \param[in] mock_functor The mock functor.
+         */
+        Handle( Mock_Functor const & mock_functor ) noexcept :
+            m_mock_functor{ &mock_functor }
+        {
+        }
+
+        /**
+         * \brief Constructor.
+         *
+         * \param[in] source The source of the move.
+         */
+        Handle( Handle && source ) noexcept = default;
+
+        /**
+         * \brief Constructor.
+         *
+         * \param[in] original The original to copy.
+         */
+        Handle( Handle const & original ) noexcept = default;
+
+        /**
+         * \brief Destructor.
+         */
+        ~Handle() noexcept = default;
+
+        /**
+         * \brief Assignment operator.
+         *
+         * \param[in] expression The expression to be assigned.
+         *
+         * \return The assigned to object.
+         */
+        auto operator=( Handle && expression ) noexcept -> Handle & = default;
+
+        /**
+         * \brief Assignment operator.
+         *
+         * \param[in] expression The expression to be assigned.
+         *
+         * \return The assigned to object.
+         */
+        auto operator=( Handle const & expression ) noexcept -> Handle & = default;
+
+        /**
+         * \brief Get the mock functor.
+         *
+         * \return The mock functor.
+         */
+        auto const & mock() const noexcept
+        {
+            return *m_mock_functor;
+        }
+
+        /**
+         * \brief Call operator.
+         *
+         * \tparam Arguments Functor argument types.
+         *
+         * \param[in] arguments The arguments to the functor.
+         *
+         * \return The functor result.
+         */
+        template<typename... Arguments>
+        auto operator()( Arguments &&... arguments ) const
+        {
+            return m_mock_functor->call( std::forward<Arguments>( arguments )... );
+        }
+
+      private:
+        /**
+         * \brief The mock functor.
+         */
+        Mock_Functor const * m_mock_functor{};
+    };
+
+    /**
+     * \brief Constructor.
+     */
+    Mock_Functor() = default;
+
+    /**
+     * \todo #29.
+     */
+    Mock_Functor( Mock_Functor && ) = delete;
+
+    /**
+     * \todo #29.
+     */
+    Mock_Functor( Mock_Functor const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Mock_Functor() noexcept = default;
+
+    /**
+     * \todo #29.
+     *
+     * \return
+     */
+    auto operator=( Mock_Functor && ) = delete;
+
+    /**
+     * \todo #29.
+     *
+     * \return
+     */
+    auto operator=( Mock_Functor const & ) = delete;
+
+    /**
+     * \brief Get a movable, copyable handle to the mock functor.
+     *
+     * \return A movable, copyable handle to the mock functor.
+     */
+    auto handle() const noexcept
+    {
+        return Handle{ *this };
+    }
+
+    MOCK_METHOD( Return_Type, call, ( Argument_Types... ), ( const ) );
+
+    /**
+     * \brief Call operator.
+     *
+     * \tparam Arguments Functor argument types.
+     *
+     * \param[in] arguments The arguments to the functor.
+     *
+     * \return The functor result.
+     */
+    template<typename... Arguments>
+    auto operator()( Arguments &&... arguments ) const
+    {
+        return call( std::forward<Arguments>( arguments )... );
+    }
+};
+
 } // namespace picolibrary::Testing::Unit
 
 #endif // PICOLIBRARY_TESTING_UNIT_FUNCTOR_H
