e1020e31b6da57024ea263af7453bfd10b2cac67
Andrew Countryman <apcountryman@gmail.com>
2020-08-21 14:51:17 -0700

    Add integer reflection
    Supports #72.

--- include/picolibrary/bit_manipulation.h
+++ include/picolibrary/bit_manipulation.h
@@ -22,7 +22,35 @@
 #ifndef PICOLIBRARY_BIT_MANIPULATION_H
 #define PICOLIBRARY_BIT_MANIPULATION_H
 
+#include <limits>
+
 namespace picolibrary {
+
+/**
+ * \brief Reflect the bits in an integer.
+ *
+ * \tparam Integer The integer type to reflect.
+ *
+ * \param[in] value The value to reflect.
+ *
+ * \return The reflected value.
+ */
+template<typename Integer>
+auto reflect( Integer value ) noexcept -> Integer
+{
+    auto result           = value;
+    auto remaining_shifts = std::numeric_limits<Integer>::digits - 1;
+
+    for ( value >>= 1; value; value >>= 1 ) {
+        result <<= 1;
+        result |= value & 1;
+
+        --remaining_shifts;
+    } // for
+
+    return result << remaining_shifts;
+}
+
 } // namespace picolibrary
 
 #endif // PICOLIBRARY_BIT_MANIPULATION_H
