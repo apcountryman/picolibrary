db260eae48bd9a0c0a345f7b1858b9c748090e46
Andrew Countryman <apcountryman@gmail.com>
2020-09-07 10:54:53 -0700

    Add calculator equivalence unit test
    Supports #83.

--- test/unit/picolibrary/crc/main.cc
+++ test/unit/picolibrary/crc/main.cc
@@ -19,9 +19,68 @@
  * \brief picolibrary::CRC unit test program.
  */
 
+#include <cstdint>
+#include <vector>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "picolibrary/crc.h"
+#include "picolibrary/testing/unit/random.h"
+
+namespace {
+
+using ::picolibrary::CRC::Augmented_Byte_Lookup_Table_Calculator;
+using ::picolibrary::CRC::Bitwise_Calculator;
+using ::picolibrary::CRC::Parameters;
+using ::picolibrary::Testing::Unit::random;
+using ::picolibrary::Testing::Unit::random_container;
+using ::testing::Test;
+
+} // namespace
+
+/**
+ * \brief Calculator equivalence unit test fixture.
+ *
+ * \tparam T The calculator register type.
+ */
+template<typename T>
+class calculators : public Test {
+};
+
+/**
+ * \brief Calculator register types.
+ */
+using Registers = ::testing::Types<std::uint8_t, std::uint16_t, std::uint32_t>;
+
+/**
+ * \brief Calculator equivalence unit test fixture type registration.
+ */
+TYPED_TEST_SUITE( calculators, Registers );
+
+/**
+ * \brief Verify picolibrary::CRC::Bitwise_Calculator, and
+ *        picolibrary::CRC::Augmented_Byte_Lookup_Table_Calculator are equivalent.
+ */
+TYPED_TEST( calculators, areEquivalent )
+{
+    using Register = TypeParam;
+
+    auto const parameters = Parameters<Register>{ .polynomial        = random<Register>(),
+                                                  .initial_remainder = random<Register>(),
+                                                  .input_is_reflected  = random<bool>(),
+                                                  .output_is_reflected = random<bool>(),
+                                                  .xor_output = random<Register>() };
+
+    auto const message = random_container<std::vector<std::uint8_t>>();
+
+    auto const expected_remainder = Bitwise_Calculator{ parameters }.calculate(
+        message.begin(), message.end() );
+
+    EXPECT_EQ(
+        Augmented_Byte_Lookup_Table_Calculator{ parameters }.calculate(
+            message.begin(), message.end() ),
+        expected_remainder );
+}
 
 /**
  * \brief Execute the picolibrary::CRC unit tests.
