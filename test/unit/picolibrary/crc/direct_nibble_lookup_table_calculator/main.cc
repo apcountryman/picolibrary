4e889246a9ef64ed770c55a4d0f7eabe3e6bfe12
Andrew Countryman <apcountryman@gmail.com>
2020-09-19 19:49:39 -0700

    Add direct nibble lookup table calculator unit tests
    Supports #93.

--- test/unit/picolibrary/crc/direct_nibble_lookup_table_calculator/main.cc
+++ test/unit/picolibrary/crc/direct_nibble_lookup_table_calculator/main.cc
@@ -19,9 +19,48 @@
  * \brief picolibrary::CRC::Direct_Nibble_Lookup_Table_Calculator unit test program.
  */
 
+#include <cstdint>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "picolibrary/crc.h"
+#include "picolibrary/testing/unit/crc.h"
+
+namespace {
+
+using ::picolibrary::CRC::Direct_Nibble_Lookup_Table_Calculator;
+
+} // namespace
+
+/**
+ * \brief Verify
+ *        picolibrary::CRC::Direct_Nibble_Lookup_Table_Calculator<std::uint8_t>::calculate()
+ *        works properly.
+ */
+INSTANTIATE_TYPED_TEST_SUITE_P(
+    directNibbleLookupTable,
+    CalculatorUint8Register,
+    Direct_Nibble_Lookup_Table_Calculator<std::uint8_t> );
+
+/**
+ * \brief Verify
+ *        picolibrary::CRC::Direct_Nibble_Lookup_Table_Calculator<std::uint16_t>::calculate()
+ *        works properly.
+ */
+INSTANTIATE_TYPED_TEST_SUITE_P(
+    directNibbleLookupTable,
+    CalculatorUint16Register,
+    Direct_Nibble_Lookup_Table_Calculator<std::uint16_t> );
+
+/**
+ * \brief Verify
+ *        picolibrary::CRC::Direct_Nibble_Lookup_Table_Calculator<std::uint32_t>::calculate()
+ *        works properly.
+ */
+INSTANTIATE_TYPED_TEST_SUITE_P(
+    directNibbleLookupTable,
+    CalculatorUint32Register,
+    Direct_Nibble_Lookup_Table_Calculator<std::uint32_t> );
 
 /**
  * \brief Execute the picolibrary::CRC::Direct_Nibble_Lookup_Table_Calculator unit tests.
