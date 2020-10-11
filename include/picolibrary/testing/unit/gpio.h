49a98a3dfd387e72a2d15963871cfb07ecdb28ca
Andrew Countryman <apcountryman@gmail.com>
2020-10-10 18:40:04 -0700

    Add GPIO mock internally pulled-up input pin
    Supports #100.

--- include/picolibrary/testing/unit/gpio.h
+++ include/picolibrary/testing/unit/gpio.h
@@ -165,6 +165,156 @@ class Mock_Input_Pin {
     MOCK_METHOD( (Result<::picolibrary::GPIO::Pin_State, Error_Code>), state, (), ( const ) );
 };
 
+/**
+ * \brief Mock internally pulled-up input pin.
+ */
+class Mock_Internally_Pulled_Up_Input_Pin {
+  public:
+    /**
+     * \brief Movable mock internally pulled-up input pin handle.
+     */
+    class Handle {
+      public:
+        /**
+         * \brief Constructor.
+         */
+        Handle() noexcept = default;
+
+        /**
+         * \brief Constructor.
+         *
+         * \param[in] mock_internally_pulled_up_input_pin The mock internally pulled-up
+         *            input pin.
+         */
+        Handle( Mock_Internally_Pulled_Up_Input_Pin & mock_internally_pulled_up_input_pin ) noexcept :
+            m_mock_internally_pulled_up_input_pin{ &mock_internally_pulled_up_input_pin }
+        {
+        }
+
+        /**
+         * \brief Constructor.
+         *
+         * \param[in] source The source of the move.
+         */
+        Handle( Handle && source ) noexcept = default;
+
+        /**
+         * \todo #29
+         */
+        Handle( Handle const & ) = delete;
+
+        /**
+         * \brief Destructor.
+         */
+        ~Handle() noexcept = default;
+
+        /**
+         * \brief Assignment operator.
+         *
+         * \param[in] expression The expression to be assigned.
+         *
+         * \return The assigned to object.
+         */
+        auto operator=( Handle && expression ) noexcept -> Handle & = default;
+
+        /**
+         * \todo #29
+         *
+         * \return
+         */
+        auto operator=( Handle const & ) = delete;
+
+        /**
+         * \brief Initialize the pin's hardware.
+         *
+         * \return Nothing if initializing the pin's hardware succeeded.
+         * \return An error code if initializing the pin's hardware failed.
+         */
+        auto initialize()
+        {
+            return m_mock_internally_pulled_up_input_pin->initialize();
+        }
+
+        /**
+         * \brief Enable the pin's internal pull-up resistor.
+         *
+         * \return Nothing if enabling the pin's internal pull-up resistor succeeded.
+         * \return An error code if enabling the pin's internal pull-up resistor failed.
+         */
+        auto enable_pull_up()
+        {
+            return m_mock_internally_pulled_up_input_pin->enable_pull_up();
+        }
+
+        /**
+         * \brief Get the state of the pin.
+         *
+         * \return High if the pin is high.
+         * \return Low if the pin is low.
+         * \return An error code if getting the state of the pin failed.
+         */
+        auto state() const
+        {
+            return m_mock_internally_pulled_up_input_pin->state();
+        }
+
+      private:
+        /**
+         * \brief The mock internally pulled-up input pin.
+         */
+        Mock_Internally_Pulled_Up_Input_Pin * m_mock_internally_pulled_up_input_pin{};
+    };
+
+    /**
+     * \brief Constructor.
+     */
+    Mock_Internally_Pulled_Up_Input_Pin() = default;
+
+    /**
+     * \todo #29
+     */
+    Mock_Internally_Pulled_Up_Input_Pin( Mock_Input_Pin && ) = delete;
+
+    /**
+     * \todo #29
+     */
+    Mock_Internally_Pulled_Up_Input_Pin( Mock_Input_Pin const & ) = delete;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Mock_Internally_Pulled_Up_Input_Pin && ) = delete;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Mock_Internally_Pulled_Up_Input_Pin const & ) = delete;
+
+    /**
+     * \brief Get a movable handle to the mock internally pulled-up input pin.
+     *
+     * \return A movable handle to the mock internally pulled-up input pin.
+     */
+    auto handle() noexcept
+    {
+        return Handle{ *this };
+    }
+
+    MOCK_METHOD( (Result<Void, Error_Code>), initialize, () );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), initialize, ( ::picolibrary::GPIO::Initial_Pull_Up_State ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), enable_pull_up, () );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), disable_pull_up, () );
+
+    MOCK_METHOD( (Result<::picolibrary::GPIO::Pin_State, Error_Code>), state, (), ( const ) );
+};
+
 } // namespace picolibrary::Testing::Unit::GPIO
 
 #endif // PICOLIBRARY_TESTING_UNIT_GPIO_H
