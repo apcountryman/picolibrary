8f669179cea9c14bc93b44515d2e75ea7da9bcf4
Andrew Countryman <apcountryman@gmail.com>
2020-11-12 11:41:40 -0800

    Add type safe output stream formatted output unit tests
    Supports #130.

--- test/unit/picolibrary/output_stream/main.cc
+++ test/unit/picolibrary/output_stream/main.cc
@@ -21,6 +21,7 @@
 
 #include <cstdint>
 #include <string>
+#include <type_traits>
 #include <vector>
 
 #include "gmock/gmock.h"
@@ -40,15 +41,93 @@ using ::picolibrary::Generic_Error;
 using ::picolibrary::Result;
 using ::picolibrary::Void;
 using ::picolibrary::Testing::Unit::Mock_Error;
+using ::picolibrary::Testing::Unit::Mock_Output_Formatter;
 using ::picolibrary::Testing::Unit::Mock_Output_Stream;
+using ::picolibrary::Testing::Unit::Output_String_Stream;
 using ::picolibrary::Testing::Unit::random;
 using ::picolibrary::Testing::Unit::random_container;
+using ::testing::_;
 using ::testing::A;
 using ::testing::Eq;
+using ::testing::InSequence;
+using ::testing::Ref;
 using ::testing::Return;
 using ::testing::SafeMatcherCast;
 
+/**
+ * \brief Formatted output test type.
+ */
+enum class Foo {};
+
 } // namespace
+
+/**
+ * \brief Foo output formatter.
+ */
+template<>
+class picolibrary::Output_Formatter<::Foo> {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    Output_Formatter() noexcept = default;
+
+    /**
+     * \todo #29
+     */
+    Output_Formatter( Output_Formatter && ) = delete;
+
+    /**
+     * \todo #29
+     */
+    Output_Formatter( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Output_Formatter() noexcept = default;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Output_Formatter && ) = delete;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Output_Formatter const & ) = delete;
+
+    /**
+     * \copydoc picolibrary::Output_Formatter::parse()
+     */
+    auto parse( char const * format ) noexcept
+    {
+        return Testing::Unit::Mock_Output_Formatter<::Foo>::instance().parse( format );
+    }
+
+    /**
+     * \copydoc picolibrary::Output_Formatter::print()
+     */
+    auto print( Output_Stream & stream, ::Foo const & value ) noexcept
+    {
+        return Testing::Unit::Mock_Output_Formatter<::Foo>::instance().print( stream, value );
+    }
+};
+
+/**
+ * \brief Generate a pseudo-random Foo.
+ *
+ * \return A pseudo-randomly generated Foo.
+ */
+template<>
+inline auto picolibrary::Testing::Unit::random<::Foo>()
+{
+    return static_cast<::Foo>( random<std::underlying_type_t<::Foo>>() );
+}
 
 /**
  * \brief Verify picolibrary::Output_Stream::put( char ) properly handles the presence of
@@ -500,6 +579,269 @@ TEST( putSignedByteBlock, worksProperly )
     EXPECT_FALSE( stream.put( &*values.begin(), &*values.end() ).is_error() );
 
     EXPECT_TRUE( stream.is_nominal() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Stream::print() properly handles the presence of an
+ *        I/O error and/or fatal error.
+ */
+TEST( print, errorPresent )
+{
+    {
+        auto stream = Mock_Output_Stream{};
+
+        stream.report_random_error();
+
+        auto const io_error_present    = stream.io_error_present();
+        auto const fatal_error_present = stream.fatal_error_present();
+
+        EXPECT_CALL( stream.buffer(), put( A<char>() ) ).Times( 0 );
+
+        auto const result = stream.print( random_container<std::string>().c_str() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), Generic_Error::IO_STREAM_DEGRADED );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_EQ( stream.io_error_present(), io_error_present );
+        EXPECT_EQ( stream.fatal_error_present(), fatal_error_present );
+    }
+
+    {
+        auto stream = Mock_Output_Stream{};
+
+        stream.report_random_error();
+
+        auto const io_error_present    = stream.io_error_present();
+        auto const fatal_error_present = stream.fatal_error_present();
+
+        EXPECT_CALL( stream.buffer(), put( A<char>() ) ).Times( 0 );
+
+        auto const result = stream.print(
+            ( random_container<std::string>() + "{}" + random_container<std::string>() ).c_str(),
+            random<Foo>() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), Generic_Error::IO_STREAM_DEGRADED );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_EQ( stream.io_error_present(), io_error_present );
+        EXPECT_EQ( stream.fatal_error_present(), fatal_error_present );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::Output_Stream::print() properly handles a print error.
+ */
+TEST( print, printError )
+{
+    {
+        auto stream = Mock_Output_Stream{};
+
+        auto const error = random<Mock_Error>();
+
+        EXPECT_CALL( stream.buffer(), put( A<char>() ) ).WillOnce( Return( error ) );
+
+        auto const result = stream.print( random_container<std::string>().c_str() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), error );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_FALSE( stream.io_error_present() );
+        EXPECT_TRUE( stream.fatal_error_present() );
+    }
+
+    {
+        auto stream = Mock_Output_Stream{};
+
+        auto const error = random<Mock_Error>();
+
+        EXPECT_CALL( stream.buffer(), put( A<char>() ) ).WillOnce( Return( error ) );
+
+        auto const result = stream.print(
+            ( random_container<std::string>() + "{}" + random_container<std::string>() ).c_str(),
+            random<Foo>() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), error );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_FALSE( stream.io_error_present() );
+        EXPECT_TRUE( stream.fatal_error_present() );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::Output_Stream::print() properly handles an invalid format
+ *        string.
+ */
+TEST( print, invalidFormatString )
+{
+    {
+        auto stream = Output_String_Stream{};
+
+        auto const result = stream.print(
+            ( random_container<std::string>() + '{' + random_container<std::string>() ).c_str() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), Generic_Error::INVALID_FORMAT );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_TRUE( stream.io_error_present() );
+        EXPECT_FALSE( stream.fatal_error_present() );
+    }
+
+    {
+        auto stream = Output_String_Stream{};
+
+        auto const result = stream.print(
+            ( random_container<std::string>() + '}' + random_container<std::string>() ).c_str() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), Generic_Error::INVALID_FORMAT );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_TRUE( stream.io_error_present() );
+        EXPECT_FALSE( stream.fatal_error_present() );
+    }
+
+    {
+        auto stream = Output_String_Stream{};
+
+        auto const result = stream.print(
+            ( random_container<std::string>() + '}' + random_container<std::string>()
+              + "{}" + random_container<std::string>() )
+                .c_str(),
+            random<Foo>() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), Generic_Error::INVALID_FORMAT );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_TRUE( stream.io_error_present() );
+        EXPECT_FALSE( stream.fatal_error_present() );
+    }
+
+    {
+        auto stream = Output_String_Stream{};
+
+        auto const result = stream.print( random_container<std::string>().c_str(), random<Foo>() );
+
+        EXPECT_TRUE( result.is_error() );
+        EXPECT_EQ( result.error(), Generic_Error::INVALID_FORMAT );
+
+        EXPECT_FALSE( stream.end_of_file_reached() );
+        EXPECT_TRUE( stream.io_error_present() );
+        EXPECT_FALSE( stream.fatal_error_present() );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::Output_Stream::print() properly handles a
+ *        picolibrary::Output_Formatter::parse() error.
+ */
+TEST( print, outputFormatterParseError )
+{
+    auto stream    = Output_String_Stream{};
+    auto formatter = Mock_Output_Formatter<Foo>{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( formatter, parse( _ ) ).WillOnce( Return( error ) );
+
+    auto const result = stream.print(
+        ( random_container<std::string>() + '{' + random_container<std::string>() + '}'
+          + random_container<std::string>() )
+            .c_str(),
+        random<Foo>() );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_TRUE( stream.io_error_present() );
+    EXPECT_FALSE( stream.fatal_error_present() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Stream::print() properly handles a
+ *        picolibrary::Output_Formatter::print() error.
+ */
+TEST( print, outputFormatterPrintError )
+{
+    auto stream    = Output_String_Stream{};
+    auto formatter = Mock_Output_Formatter<Foo>{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( formatter, parse( _ ) ).WillOnce( Return( "}" ) );
+    EXPECT_CALL( formatter, print( _, _ ) ).WillOnce( Return( error ) );
+
+    auto const result = stream.print(
+        ( random_container<std::string>() + '{' + random_container<std::string>() + '}'
+          + random_container<std::string>() )
+            .c_str(),
+        random<Foo>() );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_FALSE( stream.io_error_present() );
+    EXPECT_FALSE( stream.fatal_error_present() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Stream::print() works properly.
+ */
+TEST( print, worksProperly )
+{
+    {
+        auto const in_sequence = InSequence{};
+
+        auto stream = Output_String_Stream{};
+
+        auto const a = random_container<std::string>();
+        auto const b = random_container<std::string>();
+        auto const c = random_container<std::string>();
+
+        EXPECT_FALSE( stream.print( ( a + "{{" + b + "}}" + c ).c_str() ).is_error() );
+
+        EXPECT_TRUE( stream.is_nominal() );
+        EXPECT_EQ( stream.string(), a + '{' + b + '}' + c );
+    }
+
+    {
+        auto const in_sequence = InSequence{};
+
+        auto stream = Output_String_Stream{};
+
+        auto formatter = Mock_Output_Formatter<Foo>{};
+
+        auto const a = random_container<std::string>();
+        auto const b = random_container<std::string>();
+        auto const c = random_container<std::string>();
+        auto const d = random_container<std::string>();
+        auto const e = random_container<std::string>();
+
+        auto const format_specification_begin = d + '}' + e;
+        auto const format_specification_end   = std::string{ '}' } + e;
+
+        auto const foo = random<Foo>();
+
+        EXPECT_CALL( formatter, parse( format_specification_begin ) )
+            .WillOnce( Return( format_specification_end.c_str() ) );
+
+        EXPECT_CALL( formatter, print( Ref( stream ), Ref( foo ) ) )
+            .WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+        EXPECT_FALSE(
+            stream.print( ( a + "{{" + b + "}}" + c + '{' + d + '}' + e ).c_str(), foo ).is_error() );
+
+        EXPECT_TRUE( stream.is_nominal() );
+        EXPECT_EQ( stream.string(), a + '{' + b + '}' + c + e );
+    }
 }
 
 /**
