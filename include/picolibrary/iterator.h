7befc05b341bc80f7299fc4c03cf91fcf969f0e8
Andrew Countryman <apcountryman@gmail.com>
2020-11-14 16:02:17 -0800

    Add iterator category tag types
    Supports #144.

---
+++ include/picolibrary/iterator.h
@@ -0,0 +1,65 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020 Andrew Countryman <apcountryman@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Iterator interface.
+ */
+
+#ifndef PICOLIBRARY_ITERATOR_H
+#define PICOLIBRARY_ITERATOR_H
+
+namespace picolibrary {
+
+/**
+ * \brief Input iterator category tag.
+ */
+struct Input_Iterator_Tag {
+};
+
+/**
+ * \brief Output iterator category tag.
+ */
+struct Output_Iterator_Tag {
+};
+
+/**
+ * \brief Forward iterator category tag.
+ */
+struct Forward_Iterator_Tag : Input_Iterator_Tag {
+};
+
+/**
+ * \brief Bidirectional iterator category tag.
+ */
+struct Bidirectional_Iterator_Tag : Forward_Iterator_Tag {
+};
+
+/**
+ * \brief Random access iterator category tag.
+ */
+struct Random_Access_Iterator_Tag : Bidirectional_Iterator_Tag {
+};
+
+/**
+ * \brief Contiguous iterator category tag.
+ */
+struct Contiguous_Iterator_Tag : Random_Access_Iterator_Tag {
+};
+
+} // namespace picolibrary
+
+#endif // PICOLIBRARY_ITERATOR_H
