78978b128caf3caf56d55371678026f35282259a
Andrew Countryman <apcountryman@gmail.com>
2020-11-16 16:11:25 -0800

    Add binary integer output formatter unit tests
    Supports #141.

--- test/unit/picolibrary/format/binary/main.cc
+++ test/unit/picolibrary/format/binary/main.cc
@@ -19,9 +19,148 @@
  * \brief picolibrary::Format::Binary unit test program.
  */
 
+#include <bitset>
+#include <cstdint>
+#include <cstring>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "picolibrary/error.h"
 #include "picolibrary/format.h"
+#include "picolibrary/testing/unit/error.h"
+#include "picolibrary/testing/unit/random.h"
+#include "picolibrary/testing/unit/stream.h"
+
+namespace {
+
+using ::picolibrary::Generic_Error;
+using ::picolibrary::Format::Binary;
+using ::picolibrary::Testing::Unit::Mock_Error;
+using ::picolibrary::Testing::Unit::Mock_Output_Stream;
+using ::picolibrary::Testing::Unit::Output_String_Stream;
+using ::picolibrary::Testing::Unit::random;
+using ::picolibrary::Testing::Unit::random_container;
+using ::testing::A;
+using ::testing::Return;
+
+/**
+ * \brief Format an integer.
+ *
+ * \tparam Integer The type of integer to format.
+ *
+ * \param[in] value The value to format.
+ *
+ * \return The formatted integer.
+ */
+template<typename Integer>
+auto binary( Integer value )
+{
+    auto const unsigned_value = [value]() {
+        auto u = std::make_unsigned_t<Integer>{};
+
+        static_assert( sizeof( value ) == sizeof( u ) );
+
+        std::memcpy( &u, &value, sizeof( value ) );
+
+        return u;
+    }();
+
+    auto stream = std::ostringstream{};
+
+    stream << "0b" << std::bitset<std::numeric_limits<Integer>::digits>{ unsigned_value };
+
+    return stream.str();
+}
+
+} // namespace
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Binary> unit test fixture.
+ *
+ * \tparam Integer The type of integer to be printed.
+ */
+template<typename Integer>
+class outputFormatterBinary : public ::testing::Test {
+};
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Binary> unit test integer types.
+ */
+using Integers =
+    ::testing::Types<std::int8_t, std::uint8_t, std::int16_t, std::uint16_t, std::int32_t, std::uint32_t, std::int64_t, std::uint64_t>;
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Binary> unit test fixture.
+ */
+TYPED_TEST_SUITE( outputFormatterBinary, Integers );
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::Binary> properly handles an
+ *        invalid format string.
+ */
+TYPED_TEST( outputFormatterBinary, invalidFormatString )
+{
+    using Integer = TypeParam;
+
+    auto stream = Output_String_Stream{};
+
+    auto const result = stream.print(
+        ( std::string{ '{' } + random_container<std::string>( random<std::uint_fast8_t>( 1 ) ) + '}' )
+            .c_str(),
+        Binary{ random<Integer>() } );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), Generic_Error::INVALID_FORMAT );
+
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_TRUE( stream.io_error_present() );
+    EXPECT_FALSE( stream.fatal_error_present() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::Binary> properly handles a
+ *        print error.
+ */
+TYPED_TEST( outputFormatterBinary, printError )
+{
+    using Integer = TypeParam;
+
+    auto stream = Mock_Output_Stream{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( stream.buffer(), put( A<std::string>() ) ).WillOnce( Return( error ) );
+
+    auto const result = stream.print( "{}", Binary{ random<Integer>() } );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_FALSE( stream.io_error_present() );
+    EXPECT_TRUE( stream.fatal_error_present() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::Binary> works properly.
+ */
+TYPED_TEST( outputFormatterBinary, worksProperly )
+{
+    using Integer = TypeParam;
+
+    auto stream = Output_String_Stream{};
+
+    auto const value = random<Integer>();
+
+    EXPECT_FALSE( stream.print( "{}", Binary{ value } ).is_error() );
+
+    EXPECT_TRUE( stream.is_nominal() );
+    EXPECT_EQ( stream.string(), binary( value ) );
+}
 
 /**
  * \brief Execute the picolibrary::Format::Binary unit tests.
