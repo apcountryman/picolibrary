8e4ce967828acbba57cc090c6b1f202aae2b5b7d
Andrew Countryman <apcountryman@gmail.com>
2020-11-16 20:38:25 -0800

    Add decimal integer output formatter skeleton
    Supports #142.

--- include/picolibrary/format.h
+++ include/picolibrary/format.h
@@ -112,6 +112,56 @@ class Binary {
     Integer m_value{};
 };
 
+/**
+ * \brief Integer decimal output format specifier.
+ *
+ * \tparam Integer The type of integer to be printed.
+ */
+template<typename Integer>
+class Decimal {
+  public:
+    static_assert( std::is_integral_v<Integer> );
+
+    Decimal() = delete;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Decimal( Decimal && source ) noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] original The original to copy.
+     */
+    constexpr Decimal( Decimal const & original ) noexcept = default;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Decimal() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Decimal && expression ) noexcept -> Decimal & = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Decimal const & expression ) noexcept -> Decimal & = default;
+};
+
 } // namespace picolibrary::Format
 
 namespace picolibrary {
@@ -201,6 +251,81 @@ class Output_Formatter<Format::Binary<Integer>> {
         *i = '0';
 
         return stream.put( binary.begin(), binary.end() );
+    }
+};
+
+/**
+ * \brief Integer decimal output formatter.
+ *
+ * picolibrary::Format::Decimal only supports the default format specification ("{}").
+ *
+ * \tparam Integer The type of integer to be printed.
+ */
+template<typename Integer>
+class Output_Formatter<Format::Decimal<Integer>> {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Output_Formatter() noexcept = default;
+
+    /**
+     * \todo #29
+     */
+    Output_Formatter( Output_Formatter && ) = delete;
+
+    /**
+     * \todo #29
+     */
+    Output_Formatter( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Output_Formatter() noexcept = default;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Output_Formatter && ) = delete;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Parse the format specification for the integer to be formatted.
+     *
+     * \param[in] format The format specification for the integer to be formatted.
+     *
+     * \return format.
+     */
+    constexpr auto parse( char const * format ) noexcept -> Result<char const *, Void>
+    {
+        return format;
+    }
+
+    /**
+     * \brief Write the integer to the stream.
+     *
+     * \param[in] stream The stream to write the integer to.
+     * \param[in] integer The integer to write to the stream.
+     *
+     * \return Nothing if the write succeeded.
+     * \return An error code if the write failed.
+     */
+    auto print( Output_Stream & stream, Format::Decimal<Integer> integer ) noexcept
+        -> Result<Void, Error_Code>
+    {
+        static_cast<void>( stream );
+        static_cast<void>( integer );
+
+        return {};
     }
 };
 
