b3ad3cb2742b662aa5f85dca5f5eb085a045f805
Andrew Countryman <apcountryman@gmail.com>
2020-11-22 10:43:01 -0800

    Fix SPI mock basic controller exchange() return type
    Supports #155.

--- include/picolibrary/testing/unit/spi.h
+++ include/picolibrary/testing/unit/spi.h
@@ -203,7 +203,7 @@ class Mock_Basic_Controller {
 
     MOCK_METHOD( (Result<Void, Error_Code>), configure, ( Configuration ) );
 
-    MOCK_METHOD( (Result<Void, Error_Code>), exchange, ( std::uint8_t ) );
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), exchange, ( std::uint8_t ) );
 };
 
 /**
