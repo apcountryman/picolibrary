d55ccf95ea39d62ac44c0862e6dd7530f7a7386d
Andrew Countryman <apcountryman@gmail.com>
2020-11-27 17:20:29 -0800

    Add SPI device selection guard device selection/deselection unit tests
    Supports #170.

--- test/unit/picolibrary/spi/device_selection_guard/main.cc
+++ test/unit/picolibrary/spi/device_selection_guard/main.cc
@@ -21,12 +21,24 @@
 
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "picolibrary/error.h"
+#include "picolibrary/result.h"
 #include "picolibrary/spi.h"
+#include "picolibrary/testing/unit/error.h"
+#include "picolibrary/testing/unit/random.h"
 #include "picolibrary/testing/unit/spi.h"
+#include "picolibrary/utility.h"
 
 namespace {
 
+using ::picolibrary::Error_Code;
+using ::picolibrary::Result;
+using ::picolibrary::Void;
+using ::picolibrary::SPI::make_device_selection_guard;
+using ::picolibrary::Testing::Unit::Mock_Error;
+using ::picolibrary::Testing::Unit::random;
 using ::picolibrary::Testing::Unit::SPI::Mock_Device_Selector;
+using ::testing::Return;
 
 using Device_Selection_Guard = ::picolibrary::SPI::Device_Selection_Guard<Mock_Device_Selector>;
 
@@ -39,6 +51,58 @@ using Device_Selection_Guard = ::picolibrary::SPI::Device_Selection_Guard<Mock_D
 TEST( constructorDefault, worksProperly )
 {
     Device_Selection_Guard{};
+}
+
+/**
+ * \brief Verify picolibrary::SPI::make_device_selection_guard() properly handles a device
+ *        selection error.
+ */
+TEST( makeDeviceSelectionGuard, selectionError )
+{
+    auto device_selector = Mock_Device_Selector{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( device_selector, select() ).WillOnce( Return( error ) );
+
+    auto const result = make_device_selection_guard( device_selector );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify picolibrary::SPI::make_device_selection_guard() works properly.
+ */
+TEST( makeDeviceSelectionGuard, worksProperly )
+{
+    auto device_selector = Mock_Device_Selector{};
+
+    EXPECT_CALL( device_selector, select() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+    EXPECT_CALL( device_selector, deselect() ).Times( 0 );
+
+    auto const result = make_device_selection_guard( device_selector );
+
+    EXPECT_FALSE( result.is_error() );
+
+    EXPECT_CALL( device_selector, deselect() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+}
+
+/**
+ * \brief Verify picolibrary::SPI::Device_Selection_Guard::~Device_Selection_Guard()
+ *        properly handles a device deselection error.
+ */
+TEST( destructor, deselectionError )
+{
+    auto device_selector = Mock_Device_Selector{};
+
+    EXPECT_CALL( device_selector, select() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    auto const result = make_device_selection_guard( device_selector );
+
+    EXPECT_FALSE( result.is_error() );
+
+    EXPECT_CALL( device_selector, deselect() ).WillOnce( Return( random<Mock_Error>() ) );
 }
 
 /**
