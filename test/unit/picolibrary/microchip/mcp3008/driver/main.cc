c9886d40765cb81c3310bfb28a73ab96f6c6a803
Andrew Countryman <apcountryman@gmail.com>
2021-01-29 17:54:04 -0800

    Add Microchip MCP3008 driver unit tests
    Supports #192.

--- test/unit/picolibrary/microchip/mcp3008/driver/main.cc
+++ test/unit/picolibrary/microchip/mcp3008/driver/main.cc
@@ -19,9 +19,221 @@
  * \brief picolibrary::Microchip::MCP3008::Driver unit test program.
  */
 
+#include <cstdint>
+#include <limits>
+#include <vector>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "picolibrary/error.h"
 #include "picolibrary/microchip/mcp3008.h"
+#include "picolibrary/result.h"
+#include "picolibrary/testing/unit/error.h"
+#include "picolibrary/testing/unit/random.h"
+#include "picolibrary/testing/unit/spi.h"
+#include "picolibrary/utility.h"
+
+namespace {
+
+using ::picolibrary::Error_Code;
+using ::picolibrary::Result;
+using ::picolibrary::Void;
+using ::picolibrary::Microchip::MCP3008::Channel;
+using ::picolibrary::Microchip::MCP3008::Channel_Pair;
+using ::picolibrary::Microchip::MCP3008::Input;
+using ::picolibrary::Microchip::MCP3008::Sample;
+using ::picolibrary::Testing::Unit::Mock_Error;
+using ::picolibrary::Testing::Unit::random;
+using ::picolibrary::Testing::Unit::SPI::Mock_Controller;
+using ::picolibrary::Testing::Unit::SPI::Mock_Device_Selector;
+using ::testing::A;
+using ::testing::InSequence;
+using ::testing::Return;
+using ::testing::ReturnRef;
+
+using Driver =
+    ::picolibrary::Microchip::MCP3008::Driver<Mock_Controller, Mock_Device_Selector::Handle, ::picolibrary::Testing::Unit::SPI::Mock_Device>;
+
+} // namespace
+
+/**
+ * \brief Verify picolibrary::Microchip::MCP3008::Driver::sample() properly handles a
+ *        configuration error.
+ */
+TEST( sample, configurationError )
+{
+    auto mcp3008 = Driver{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( mcp3008, configure() ).WillOnce( Return( error ) );
+
+    auto const result = mcp3008.sample( {} );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify picolibrary::Microchip::MCP3008::Driver::sample() properly handles a
+ *        selection error.
+ */
+TEST( sample, selectionError )
+{
+    auto mcp3008 = Driver{};
+
+    auto device_selector        = Mock_Device_Selector{};
+    auto device_selector_handle = device_selector.handle();
+
+    EXPECT_CALL( mcp3008, configure() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    EXPECT_CALL( mcp3008, device_selector() ).WillOnce( ReturnRef( device_selector_handle ) );
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( device_selector, select() ).WillOnce( Return( error ) );
+
+    auto const result = mcp3008.sample( {} );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify picolibrary::Microchip::MCP3008::Driver::sample() properly handles a data
+ *        exchange error.
+ */
+TEST( sample, dataExchangeError )
+{
+    auto mcp3008 = Driver{};
+
+    auto device_selector        = Mock_Device_Selector{};
+    auto device_selector_handle = device_selector.handle();
+
+    EXPECT_CALL( mcp3008, configure() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    EXPECT_CALL( mcp3008, device_selector() ).WillOnce( ReturnRef( device_selector_handle ) );
+
+    EXPECT_CALL( device_selector, select() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( mcp3008, exchange( A<std::vector<std::uint8_t>>() ) ).WillOnce( Return( error ) );
+
+    EXPECT_CALL( device_selector, deselect() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    auto const result = mcp3008.sample( {} );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify picolibrary::Microchip::MCP3008::Driver::sample() properly handles a
+ *        nonresponsive device.
+ */
+TEST( sample, nonresponsiveDevice )
+{
+    auto controller = Mock_Controller{};
+
+    auto const nonresponsive = random<Mock_Error>();
+
+    auto mcp3008 = Driver{ controller, {}, {}, nonresponsive };
+
+    auto device_selector        = Mock_Device_Selector{};
+    auto device_selector_handle = device_selector.handle();
+
+    EXPECT_CALL( mcp3008, configure() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    EXPECT_CALL( mcp3008, device_selector() ).WillOnce( ReturnRef( device_selector_handle ) );
+
+    EXPECT_CALL( device_selector, select() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    auto const rx = std::vector<std::uint8_t>{
+        random<std::uint8_t>(),
+        static_cast<std::uint8_t>( random<std::uint8_t>() | 0b100 ),
+        random<std::uint8_t>(),
+    };
+
+    EXPECT_CALL( mcp3008, exchange( A<std::vector<std::uint8_t>>() ) ).WillOnce( Return( rx ) );
+
+    EXPECT_CALL( device_selector, deselect() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    auto const result = mcp3008.sample( {} );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), nonresponsive );
+}
+
+/**
+ * \brief Verify picolibrary::Microchip::MCP3008::Driver::sample() works properly.
+ */
+TEST( sample, worksProperly )
+{
+    struct {
+        Input input;
+    } const test_cases[]{
+        // clang-format off
+
+        { Channel::_0 },
+        { Channel::_1 },
+        { Channel::_2 },
+        { Channel::_3 },
+        { Channel::_4 },
+        { Channel::_5 },
+        { Channel::_6 },
+        { Channel::_7 },
+
+        { Channel_Pair::_0_1 },
+        { Channel_Pair::_1_0 },
+        { Channel_Pair::_2_3 },
+        { Channel_Pair::_3_2 },
+        { Channel_Pair::_4_5 },
+        { Channel_Pair::_5_4 },
+        { Channel_Pair::_6_7 },
+        { Channel_Pair::_7_6 },
+
+        // clang-format on
+    };
+
+    for ( auto const test_case : test_cases ) {
+        auto const in_sequence = InSequence{};
+
+        auto mcp3008 = Driver{};
+
+        auto device_selector        = Mock_Device_Selector{};
+        auto device_selector_handle = device_selector.handle();
+
+        EXPECT_CALL( mcp3008, configure() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+        EXPECT_CALL( mcp3008, device_selector() ).WillOnce( ReturnRef( device_selector_handle ) );
+
+        EXPECT_CALL( device_selector, select() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+        auto const sample = random<Sample::Value>( Sample::MIN, Sample::MAX );
+
+        auto const tx = std::vector<std::uint8_t>{
+            0x01,
+            static_cast<std::uint8_t>( test_case.input ),
+            0x00,
+        };
+        auto const rx = std::vector<std::uint8_t>{
+            random<std::uint8_t>(),
+            static_cast<std::uint8_t>(
+                ( random<std::uint8_t>( 0, 0x1F ) << 3 )
+                | ( sample >> std::numeric_limits<std::uint8_t>::digits ) ),
+            static_cast<std::uint8_t>( sample ),
+        };
+        EXPECT_CALL( mcp3008, exchange( tx ) ).WillOnce( Return( rx ) );
+
+        EXPECT_CALL( device_selector, deselect() ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+        auto const result = mcp3008.sample( test_case.input );
+
+        EXPECT_TRUE( result.is_value() );
+        EXPECT_EQ( static_cast<Sample::Value>( result.value() ), sample );
+    } // for
+}
 
 /**
  * \brief Execute the picolibrary::Microchip::MCP3008::Driver unit tests.
