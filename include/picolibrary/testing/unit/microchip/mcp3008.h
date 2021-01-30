41e41d507aab7c0c0728020ba74e0120d7ff6a15
Andrew Countryman <apcountryman@gmail.com>
2021-01-30 10:43:26 -0800

    Add mock Microchip MCP3008 driver
    Supports #192.

--- include/picolibrary/testing/unit/microchip/mcp3008.h
+++ include/picolibrary/testing/unit/microchip/mcp3008.h
@@ -22,10 +22,82 @@
 #ifndef PICOLIBRARY_TESTING_UNIT_MICROCHIP_MCP3008_H
 #define PICOLIBRARY_TESTING_UNIT_MICROCHIP_MCP3008_H
 
+#include "gmock/gmock.h"
+#include "picolibrary/error.h"
+#include "picolibrary/microchip/mcp3008.h"
+#include "picolibrary/result.h"
+#include "picolibrary/testing/unit/spi.h"
+#include "picolibrary/utility.h"
+
 /**
  * \brief Microchip MCP3008 unit testing facilities.
  */
 namespace picolibrary::Testing::Unit::Microchip::MCP3008 {
+
+/**
+ * \brief Mock Microchip MCP3008 driver.
+ */
+class Mock_Driver {
+  public:
+    /**
+     * \copydoc picolibrary::Microchip::MCP3008::Driver::Controller
+     */
+    using Controller = ::picolibrary::Testing::Unit::SPI::Mock_Controller;
+
+    /**
+     * \copydoc picolibrary::Microchip::MCP3008::Driver::Device_Selector
+     */
+    using Device_Selector = ::picolibrary::Testing::Unit::SPI::Mock_Device_Selector::Handle;
+
+    /**
+     * \brief Constructor.
+     */
+    Mock_Driver() = default;
+
+    /**
+     * \brief Constructor.
+     */
+    Mock_Driver( Controller &, Controller::Configuration, Device_Selector, Error_Code )
+    {
+    }
+
+    /**
+     * \todo #29
+     */
+    Mock_Driver( Mock_Driver && ) = delete;
+
+    /**
+     * \todo #29
+     */
+    Mock_Driver( Mock_Driver const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Mock_Driver() noexcept = default;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Mock_Driver && ) = delete;
+
+    /**
+     * \todo #29
+     *
+     * \return
+     */
+    auto operator=( Mock_Driver const & ) = delete;
+
+    MOCK_METHOD( (Result<Void, Error_Code>), initialize, () );
+
+    MOCK_METHOD(
+        (Result<::picolibrary::Microchip::MCP3008::Sample, Error_Code>),
+        sample,
+        ( ::picolibrary::Microchip::MCP3008::Input ) );
+};
+
 } // namespace picolibrary::Testing::Unit::Microchip::MCP3008
 
 #endif // PICOLIBRARY_TESTING_UNIT_MICROCHIP_MCP3008_H
