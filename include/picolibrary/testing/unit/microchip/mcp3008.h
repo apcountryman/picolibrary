29369d36cfe331668364cb8a12db77f66f12da8d
Andrew Countryman <apcountryman@gmail.com>
2021-01-31 12:45:34 -0800

    Specialize pseudo-random Microchip MCP3008 input mode/channel(s) generation
    Supports #198.

--- include/picolibrary/testing/unit/microchip/mcp3008.h
+++ include/picolibrary/testing/unit/microchip/mcp3008.h
@@ -22,12 +22,55 @@
 #ifndef PICOLIBRARY_TESTING_UNIT_MICROCHIP_MCP3008_H
 #define PICOLIBRARY_TESTING_UNIT_MICROCHIP_MCP3008_H
 
+#include <cstdint>
+
 #include "gmock/gmock.h"
 #include "picolibrary/error.h"
 #include "picolibrary/microchip/mcp3008.h"
 #include "picolibrary/result.h"
+#include "picolibrary/testing/unit/random.h"
 #include "picolibrary/testing/unit/spi.h"
 #include "picolibrary/utility.h"
+
+namespace picolibrary::Testing::Unit {
+
+/**
+ * \brief Generate a pseudo-random Microchip MCP3008 single-ended input channel.
+ *
+ * \return A pseudo-random Microchip MCP3008 single-ended input channel.
+ */
+template<>
+inline auto random<Microchip::MCP3008::Channel>()
+{
+    return static_cast<Microchip::MCP3008::Channel>(
+        ( 0b1'000 | random<std::uint8_t>( 0b000, 0b111 ) ) << 4 );
+}
+
+/**
+ * \brief Generate a pseudo-random Microchip MCP3008 differential input channel pair.
+ *
+ * \return A pseudo-random Microchip MCP3008 differential input channel pair.
+ */
+template<>
+inline auto random<Microchip::MCP3008::Channel_Pair>()
+{
+    return static_cast<Microchip::MCP3008::Channel_Pair>( random<std::uint8_t>( 0b000, 0b111 ) << 4 );
+}
+
+/**
+ * \brief Generate a pseudo-random Microchip MCP3008 input mode/channel(s).
+ *
+ * \return A pseudo-random Microchip MCP3008 input mode/channel(s).
+ */
+template<>
+inline auto random<Microchip::MCP3008::Input>()
+{
+    return random<bool>()
+               ? Microchip::MCP3008::Input{ random<Microchip::MCP3008::Channel>() }
+               : Microchip::MCP3008::Input{ random<Microchip::MCP3008::Channel_Pair>() };
+}
+
+} // namespace picolibrary::Testing::Unit
 
 /**
  * \brief Microchip MCP3008 unit testing facilities.
