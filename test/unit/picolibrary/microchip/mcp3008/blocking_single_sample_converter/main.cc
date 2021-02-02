413d1864692e4d45c70cbb02f753e7cb7ccae96f
Andrew Countryman <apcountryman@gmail.com>
2021-02-01 19:13:50 -0800

    Add Microchip MCP3008 blocking, single sample ADC unit tests
    Supports #198.

--- test/unit/picolibrary/microchip/mcp3008/blocking_single_sample_converter/main.cc
+++ test/unit/picolibrary/microchip/mcp3008/blocking_single_sample_converter/main.cc
@@ -23,6 +23,80 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "picolibrary/microchip/mcp3008.h"
+#include "picolibrary/testing/unit/error.h"
+#include "picolibrary/testing/unit/microchip/mcp3008.h"
+#include "picolibrary/testing/unit/random.h"
+
+namespace {
+
+using ::picolibrary::Microchip::MCP3008::Blocking_Single_Sample_Converter;
+using ::picolibrary::Microchip::MCP3008::Input;
+using ::picolibrary::Microchip::MCP3008::Sample;
+using ::picolibrary::Testing::Unit::Mock_Error;
+using ::picolibrary::Testing::Unit::random;
+using ::picolibrary::Testing::Unit::Microchip::MCP3008::Mock_Driver;
+using ::testing::_;
+using ::testing::Return;
+
+} // namespace
+
+/**
+ * \brief Verify
+ *        picolibrary::Microchip::MCP3008::Blocking_Single_Sample_Converter::initialize()
+ *        works properly.
+ */
+TEST( initialize, worksProperly )
+{
+    auto mcp3008 = Mock_Driver{};
+
+    auto adc = Blocking_Single_Sample_Converter{ mcp3008, random<Input>() };
+
+    EXPECT_FALSE( adc.initialize().is_error() );
+}
+
+/**
+ * \brief Verify
+ *        picolibrary::Microchip::MCP3008::Blocking_Single_Sample_Converter::sample()
+ *        properly handles a sampling error.
+ */
+TEST( sample, samplingError )
+{
+    auto mcp3008 = Mock_Driver{};
+
+    auto adc = Blocking_Single_Sample_Converter{ mcp3008, random<Input>() };
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( mcp3008, sample( _ ) ).WillOnce( Return( error ) );
+
+    auto const result = adc.sample();
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify
+ *        picolibrary::Microchip::MCP3008::Blocking_Single_Sample_Converter::sample()
+ *        works properly.
+ */
+TEST( sample, worksProperly )
+{
+    auto mcp3008 = Mock_Driver{};
+
+    auto const input = random<Input>();
+
+    auto adc = Blocking_Single_Sample_Converter{ mcp3008, input };
+
+    auto const sample = random<Sample::Value>( Sample::MIN, Sample::MAX );
+
+    EXPECT_CALL( mcp3008, sample( input ) ).WillOnce( Return( sample ) );
+
+    auto const result = adc.sample();
+
+    EXPECT_TRUE( result.is_value() );
+    EXPECT_EQ( result.value(), sample );
+}
 
 /**
  * \brief Execute the picolibrary::Microchip::MCP3008::Blocking_Single_Sample_Converter
