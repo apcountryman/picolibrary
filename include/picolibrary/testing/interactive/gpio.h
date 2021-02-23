84b751d932b4fbbf6298088bb0a40eb53e7e7897
Andrew Countryman <apcountryman@gmail.com>
2021-02-22 21:49:46 -0800

    Add error reporting to GPIO output pin toggle interactive test helper
    Supports #304.

--- include/picolibrary/testing/interactive/gpio.h
+++ include/picolibrary/testing/interactive/gpio.h
@@ -23,33 +23,55 @@
 #ifndef PICOLIBRARY_TESTING_INTERACTIVE_GPIO_H
 #define PICOLIBRARY_TESTING_INTERACTIVE_GPIO_H
 
+#include <utility>
+
 /**
  * \brief General Purpose Input/Output (GPIO) interactive testing facilities.
  */
 namespace picolibrary::Testing::Interactive::GPIO {
 
 /**
- * \brief Initialize an output pin and toggle it indefinitely.
+ * \brief GPIO output pin toggle interactive test helper.
  *
+ * \tparam Output_Stream The type of asynchronous serial output stream to use to output
+ *         information to the user.
+ * \tparam Transmitter The type of asynchronous serial transmitter to use to transmit
+ *         information to the user.
  * \tparam Output_Pin The type of output pin to toggle.
  * \tparam Delayer A nullary functor called to introduce a delay each time the pin is
  *         toggled.
  *
+ * \param[in] transmitter The asynchronous serial transmitter to use to transmit
+ *            information to the user.
  * \param[in] pin The pin to toggle.
  * \param[in] delay The nullary functor called to introduce a delay each time the pin is
  *            toggled.
  */
-template<typename Output_Pin, typename Delayer>
-void toggle( Output_Pin pin, Delayer delay ) noexcept
+template<template<typename> typename Output_Stream, typename Transmitter, typename Output_Pin, typename Delayer>
+void toggle( Transmitter transmitter, Output_Pin pin, Delayer delay ) noexcept
 {
-    if ( pin.initialize().is_error() ) {
+    auto stream = Output_Stream{ std::move( transmitter ) };
+
+    if ( stream.initialize().is_error() ) {
         return;
     } // if
+
+    {
+        auto const result = pin.initialize();
+        if ( result.is_error() ) {
+            static_cast<void>( stream.print( "pin initialization error: {}\n", result.error() ) );
+
+            return;
+        } // if
+    }
 
     for ( ;; ) {
         delay();
 
-        if ( pin.toggle().is_error() ) {
+        auto const result = pin.toggle();
+        if ( result.is_error() ) {
+            static_cast<void>( stream.print( "pin toggle error: {}\n", result.error() ) );
+
             return;
         } // if
     }     // for
