7cd1a998ca5e6bef79c89e8f0f7b4e761fc84e64
Andrew Countryman <apcountryman@gmail.com>
2021-02-23 19:18:17 -0800

    Add IOCON register definition
    Supports #308.

--- include/picolibrary/microchip/mcp23008.h
+++ include/picolibrary/microchip/mcp23008.h
@@ -105,6 +105,75 @@ struct INTCON {
     static constexpr auto POR = std::uint8_t{ 0x00 };
 };
 
+/**
+ * \brief Configuration (IOCON) register.
+ *
+ * This register has the following fields:
+ * - Interrupt Polarity (INTPOL)
+ * - Open-Drain (ODR)
+ * - Slew Rate Control (DISSLW)
+ * - Sequential Operation Mode (SEQOP)
+ *
+ * The combination of the INTPOL and ODR fields is referred to as the interrupt mode
+ * field.
+ */
+struct IOCON {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x05 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+
+    /**
+     * \brief Field size.
+     */
+    struct Size {
+        static constexpr auto RESERVED0 = std::uint_fast8_t{ 1 }; ///< Reserved.
+        static constexpr auto INTPOL    = std::uint_fast8_t{ 1 }; ///< INTPOL.
+        static constexpr auto ODR       = std::uint_fast8_t{ 1 }; ///< ODR.
+        static constexpr auto RESERVED3 = std::uint_fast8_t{ 1 }; ///< Reserved.
+        static constexpr auto DISSLW    = std::uint_fast8_t{ 1 }; ///< DISSLW.
+        static constexpr auto SEQOP     = std::uint_fast8_t{ 1 }; ///< SEQOP.
+        static constexpr auto RESERVED6 = std::uint_fast8_t{ 2 }; ///< Reserved.
+
+        static constexpr auto INTERRUPT_MODE = INTPOL + ODR; ///< Interrupt mode.
+    };
+
+    /**
+     * \brief Field bit positions.
+     */
+    struct Bit {
+        static constexpr auto RESERVED0 = std::uint_fast8_t{}; ///< Reserved.
+        static constexpr auto INTPOL = std::uint_fast8_t{ RESERVED0 + Size::RESERVED0 }; ///< INTPOL.
+        static constexpr auto ODR = std::uint_fast8_t{ INTPOL + Size::INTPOL }; ///< ODR.
+        static constexpr auto RESERVED3 = std::uint_fast8_t{ ODR + Size::ODR }; ///< Reserved.
+        static constexpr auto DISSLW = std::uint_fast8_t{ RESERVED3 + Size::RESERVED3 }; ///< DISSLW.
+        static constexpr auto SEQOP = std::uint_fast8_t{ DISSLW + Size::DISSLW }; ///< SEQOP.
+        static constexpr auto RESERVED6 = std::uint_fast8_t{ SEQOP + Size::SEQOP }; ///< Reserved.
+
+        static constexpr auto INTERRUPT_MODE = INTPOL; ///< Interrupt mode.
+    };
+
+    /**
+     * \brief Field bit masks.
+     */
+    struct Mask {
+        static constexpr auto RESERVED0 = std::uint8_t{ 0b1 << Bit::RESERVED0 }; ///< Reserved.
+        static constexpr auto INTPOL = std::uint8_t{ 0b1 << Bit::INTPOL }; ///< INTPOL.
+        static constexpr auto ODR    = std::uint8_t{ 0b1 << Bit::ODR };    ///< ODR.
+        static constexpr auto RESERVED3 = std::uint8_t{ 0b1 << Bit::RESERVED3 }; ///< Reserved.
+        static constexpr auto DISSLW = std::uint8_t{ 0b1 << Bit::DISSLW }; ///< DISSLW.
+        static constexpr auto SEQOP  = std::uint8_t{ 0b1 << Bit::SEQOP };  ///< SEQOP.
+        static constexpr auto RESERVED6 = std::uint8_t{ 0b11 << Bit::RESERVED6 }; ///< Reserved.
+
+        static constexpr auto INTERRUPT_MODE = std::uint8_t{ 0b11 << Bit::INTERRUPT_MODE }; ///< Interrupt mode.
+    };
+};
+
 } // namespace picolibrary::Microchip::MCP23008
 
 #endif // PICOLIBRARY_MICROCHIP_MCP23008_H
