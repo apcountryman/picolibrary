153eb4829b3f5377957a7712f57a5c790b9d7ef0
Andrew Countryman <apcountryman@users.noreply.github.com>
2021-02-23 20:41:11 -0800

    Add Microchip MCP23008 register definitions (#318)
    Resolves #308 (Add Microchip MCP23008 register definitions).

--- include/picolibrary/microchip/mcp23008.h
+++ include/picolibrary/microchip/mcp23008.h
@@ -23,10 +23,222 @@
 #ifndef PICOLIBRARY_MICROCHIP_MCP23008_H
 #define PICOLIBRARY_MICROCHIP_MCP23008_H
 
+#include <cstdint>
+
 /**
  * \brief Microchip MCP23008 facilities.
  */
 namespace picolibrary::Microchip::MCP23008 {
+
+/**
+ * \brief I/O Direction Register (IODIR).
+ */
+struct IODIR {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x00 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0xFF };
+};
+
+/**
+ * \brief Input Polarity Port Register (IPOL).
+ */
+struct IPOL {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x01 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+};
+
+/**
+ * \brief Interrupt-On-Change Pins Register (GPINTEN).
+ */
+struct GPINTEN {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x02 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+};
+
+/**
+ * \brief Default Value Register (DEFVAL).
+ */
+struct DEFVAL {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x03 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+};
+
+/**
+ * \brief Interrupt-On-Change Control Register (INTCON).
+ */
+struct INTCON {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x04 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+};
+
+/**
+ * \brief I/O Expander Configuration Register (IOCON).
+ *
+ * This register has the following fields:
+ * - Interrupt Polarity Control (INTPOL)
+ * - Open-Drain Control (ODR)
+ * - Slew Rate Control (DISSLW)
+ * - Sequential Operation Mode (SEQOP)
+ *
+ * The combination of the INTPOL and ODR fields is referred to as the interrupt mode
+ * field.
+ */
+struct IOCON {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x05 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+
+    /**
+     * \brief Field size.
+     */
+    struct Size {
+        static constexpr auto RESERVED0 = std::uint_fast8_t{ 1 }; ///< Reserved.
+        static constexpr auto INTPOL    = std::uint_fast8_t{ 1 }; ///< INTPOL.
+        static constexpr auto ODR       = std::uint_fast8_t{ 1 }; ///< ODR.
+        static constexpr auto RESERVED3 = std::uint_fast8_t{ 1 }; ///< Reserved.
+        static constexpr auto DISSLW    = std::uint_fast8_t{ 1 }; ///< DISSLW.
+        static constexpr auto SEQOP     = std::uint_fast8_t{ 1 }; ///< SEQOP.
+        static constexpr auto RESERVED6 = std::uint_fast8_t{ 2 }; ///< Reserved.
+
+        static constexpr auto INTERRUPT_MODE = INTPOL + ODR; ///< Interrupt mode.
+    };
+
+    /**
+     * \brief Field bit positions.
+     */
+    struct Bit {
+        static constexpr auto RESERVED0 = std::uint_fast8_t{}; ///< Reserved.
+        static constexpr auto INTPOL = std::uint_fast8_t{ RESERVED0 + Size::RESERVED0 }; ///< INTPOL.
+        static constexpr auto ODR = std::uint_fast8_t{ INTPOL + Size::INTPOL }; ///< ODR.
+        static constexpr auto RESERVED3 = std::uint_fast8_t{ ODR + Size::ODR }; ///< Reserved.
+        static constexpr auto DISSLW = std::uint_fast8_t{ RESERVED3 + Size::RESERVED3 }; ///< DISSLW.
+        static constexpr auto SEQOP = std::uint_fast8_t{ DISSLW + Size::DISSLW }; ///< SEQOP.
+        static constexpr auto RESERVED6 = std::uint_fast8_t{ SEQOP + Size::SEQOP }; ///< Reserved.
+
+        static constexpr auto INTERRUPT_MODE = INTPOL; ///< Interrupt mode.
+    };
+
+    /**
+     * \brief Field bit masks.
+     */
+    struct Mask {
+        static constexpr auto RESERVED0 = std::uint8_t{ 0b1 << Bit::RESERVED0 }; ///< Reserved.
+        static constexpr auto INTPOL = std::uint8_t{ 0b1 << Bit::INTPOL }; ///< INTPOL.
+        static constexpr auto ODR    = std::uint8_t{ 0b1 << Bit::ODR };    ///< ODR.
+        static constexpr auto RESERVED3 = std::uint8_t{ 0b1 << Bit::RESERVED3 }; ///< Reserved.
+        static constexpr auto DISSLW = std::uint8_t{ 0b1 << Bit::DISSLW }; ///< DISSLW.
+        static constexpr auto SEQOP  = std::uint8_t{ 0b1 << Bit::SEQOP };  ///< SEQOP.
+        static constexpr auto RESERVED6 = std::uint8_t{ 0b11 << Bit::RESERVED6 }; ///< Reserved.
+
+        static constexpr auto INTERRUPT_MODE = std::uint8_t{ 0b11 << Bit::INTERRUPT_MODE }; ///< Interrupt mode.
+    };
+};
+
+/**
+ * \brief GPIO Pull-Up Resistor Register (GPPU).
+ */
+struct GPPU {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x06 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+};
+
+/**
+ * \brief Interrupt Flag Register (INTF).
+ */
+struct INTF {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x07 };
+};
+
+/**
+ * \brief Interrupt Captured Value For Port Register (INTCAP).
+ */
+struct INTCAP {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x08 };
+};
+
+/**
+ * \brief General Purpose I/O Port Register (GPIO).
+ */
+struct GPIO {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x09 };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+};
+
+/**
+ * \brief Output Latch Register (OLAT).
+ */
+struct OLAT {
+    /**
+     * \brief Register address.
+     */
+    static constexpr auto ADDRESS = std::uint8_t{ 0x0A };
+
+    /**
+     * \brief POR value.
+     */
+    static constexpr auto POR = std::uint8_t{ 0x00 };
+};
+
 } // namespace picolibrary::Microchip::MCP23008
 
 #endif // PICOLIBRARY_MICROCHIP_MCP23008_H
