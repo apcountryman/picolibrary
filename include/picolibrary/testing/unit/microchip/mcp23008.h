b744e2702ce69ce9deeed10a756cada2851814f0
Andrew Countryman <apcountryman@gmail.com>
2021-02-26 19:32:55 -0800

    Add mock driver
    Supports #313.

--- include/picolibrary/testing/unit/microchip/mcp23008.h
+++ include/picolibrary/testing/unit/microchip/mcp23008.h
@@ -26,8 +26,12 @@
 #include <cstdint>
 
 #include "gmock/gmock.h"
+#include "picolibrary/error.h"
 #include "picolibrary/microchip/mcp23008.h"
+#include "picolibrary/result.h"
+#include "picolibrary/testing/unit/i2c.h"
 #include "picolibrary/testing/unit/random.h"
+#include "picolibrary/void.h"
 
 namespace picolibrary::Testing::Unit {
 
@@ -138,6 +142,76 @@ class Mock_Register_Cache {
     MOCK_METHOD( std::uint8_t, olat, (), ( const ) );
 
     MOCK_METHOD( void, cache_olat, ( std::uint8_t ) );
+};
+
+/**
+ * \brief Microchip MCP23008 mock driver.
+ */
+class Mock_Driver : public I2C::Mock_Device<std::uint8_t>, public Mock_Register_Cache {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    Mock_Driver() = default;
+
+    Mock_Driver( Mock_Driver && ) = delete;
+
+    Mock_Driver( Mock_Driver const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Mock_Driver() noexcept = default;
+
+    auto operator=( Mock_Driver && ) = delete;
+
+    auto operator=( Mock_Driver const & ) = delete;
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_iodir, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_iodir, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_ipol, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_ipol, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_gpinten, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_gpinten, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_defval, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_defval, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_intcon, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_intcon, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_iocon, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_iocon, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_gppu, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_gppu, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_intf, (), ( const ) );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_intcap, (), ( const ) );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_gpio, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_gpio, () );
+
+    MOCK_METHOD( (Result<std::uint8_t, Error_Code>), read_olat, (), ( const ) );
+
+    MOCK_METHOD( (Result<Void, Error_Code>), write_olat, () );
+
+    MOCK_METHOD(
+        (Result<::picolibrary::Microchip::MCP23008::Interrupt_Context, Error_Code>),
+        read_interrupt_context,
+        (),
+        ( const ) );
 };
 
 } // namespace picolibrary::Testing::Unit::Microchip::MCP23008
