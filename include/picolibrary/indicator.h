d79300ddda9162bcdf9d164e22fb6cd92136cc7e
Jay Lamb <jaylamb@gmail.com>
2021-02-28 13:10:24 -0800

    Add fixed intensity indicator concept
    Supports #284.

--- include/picolibrary/indicator.h
+++ include/picolibrary/indicator.h
@@ -36,6 +36,66 @@ enum class Initial_Indicator_State {
     EXTINGUISHED, ///< Extinguished.
 };
 
+/**
+ * \brief Fixed intensity indicator concept.
+ */
+class Fixed_Intensity_Indicator_Concept {
+  public:
+    Fixed_Intensity_Indicator_Concept() = delete;
+
+    Fixed_Intensity_Indicator_Concept( Fixed_Intensity_Indicator_Concept && ) = delete;
+
+    Fixed_Intensity_Indicator_Concept( Fixed_Intensity_Indicator_Concept const & ) = delete;
+
+    ~Fixed_Intensity_Indicator_Concept() = delete;
+
+    auto operator=( Fixed_Intensity_Indicator_Concept && ) = delete;
+
+    auto operator=( Fixed_Intensity_Indicator_Concept const & ) = delete;
+
+    /**
+     * \brief Initialize the indicator's hardware.
+     *
+     * \param[in] initial_indicator_state The initial state of the indicator.
+     *
+     * \return Nothing if indicator hardware initialization succeeded.
+     * \return An error code if indicator hardware initialization failed. If indicator
+     *         hardware initialization cannot fail, return
+     *         picolibrary::Result<picolibrary::Void, picolibrary::Void>.
+     */
+    auto initialize( Initial_Indicator_State initial_state = Initial_Pin_State::EXTINGUISHED ) noexcept
+        -> Result<Void, Error_Code>;
+
+    /**
+     * \brief Illuminate the indicator.
+     *
+     * \return Nothing if illuminating the indicator succeeded.
+     * \return An error code if illuminating the indicator failed. If illuminating the
+     *         indicator cannot fail, return picolibrary::Result<picolibrary::Void,
+     *         picolibrary::Void>.
+     */
+    auto illuminate() noexcept -> Result<Void, Error_Code>;
+
+    /**
+     * \brief Extinguish the indicator.
+     *
+     * \return Nothing if extinguishing the indicator succeeded.
+     * \return An error code if extinguishing the indicator failed. If extinguishing the
+     *         indicator cannot fail, return picolibrary::Result<picolibrary::Void,
+     *         picolibrary::Void>.
+     */
+    auto extinguish() noexcept -> Result<Void, Error_Code>;
+
+    /**
+     * \brief Toggle the indicator.
+     *
+     * \return Nothing if toggling the indicator succeeded.
+     * \return An error code if toggling the indicator failed. If toggling the indicator
+     *          cannot fail, return picolibrary::Result<picolibrary::Void,
+     *          picolibrary::Void>.
+     */
+    auto toggle() noexcept -> Result<Void, Error_Code>;
+
 } // namespace picolibrary::Indicator
 
 #endif // PICOLIBRARY_INDICATOR_H
