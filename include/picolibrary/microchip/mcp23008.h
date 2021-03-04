d08b7d450d46c9e9f7cfd90686a66efddf8f0ba2
Andrew Countryman <apcountryman@gmail.com>
2021-03-03 18:44:04 -0800

    Add push-pull output pin configuration
    Supports #356.

--- include/picolibrary/microchip/mcp23008.h
+++ include/picolibrary/microchip/mcp23008.h
@@ -1178,6 +1178,26 @@ class Driver : public Device, public Register_Cache {
     {
         return write_gpio( this->gpio() & ~mask );
     }
+
+    /**
+     * \brief Configure a pin to act as a push-pull output pin.
+     *
+     * \param[in] mask The mask identifying the pin to be configured as a push-pull output
+     *            pin.
+     *
+     * \return Nothing if configuration of the pin as a push-pull output pin.
+     * \return picolibrary::I2C::Device<Bus_Multiplexer_Aligner, Controller,
+     *         std::uint8_t>::nonresponsive_device_error() if the MCP23008 is not
+     *         responsive.
+     * \return picolibrary::Generic_Error::ARBITRATION_LOST if the controller lost
+     *         arbitration while attempting to communicate with the MCP23008.
+     * \return An error code if configuration of the pin as a push-pull output pin failed
+     *         for any other reason.
+     */
+    auto configure_pin_as_push_pull_output( std::uint8_t mask ) noexcept
+    {
+        return write_iodir( this->iodir() & ~mask );
+    }
 };
 
 /**
--- test/unit/picolibrary/microchip/mcp23008/driver/main.cc
+++ test/unit/picolibrary/microchip/mcp23008/driver/main.cc
@@ -1019,6 +1019,49 @@ TEST( configurePinAsOpenDrainOutput, worksProperly )
 }
 
 /**
+ * \brief Verify
+ *        picolibrary::Microchip::MCP23008::Driver::configure_pin_as_push_pull_output()
+ *        properly handles a write error.
+ */
+TEST( configurePinAsPushPullOutput, writeError )
+{
+    auto mcp23008 = Driver{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( mcp23008, iodir() ).WillOnce( Return( random<std::uint8_t>() ) );
+    EXPECT_CALL( mcp23008, write( _, A<std::uint8_t>() ) ).WillOnce( Return( error ) );
+    EXPECT_CALL( mcp23008, cache_iodir( _ ) ).Times( 0 );
+
+    auto const result = mcp23008.configure_pin_as_push_pull_output( random<std::uint8_t>() );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify
+ *        picolibrary::Microchip::MCP23008::Driver::configure_pin_as_push_pull_output()
+ *        works properly.
+ */
+TEST( configurePinAsPushPullOutput, worksProperly )
+{
+    auto const in_sequence = InSequence{};
+
+    auto mcp23008 = Driver{};
+
+    auto const iodir = random<std::uint8_t>();
+    auto const mask  = random<std::uint8_t>();
+    auto const data  = static_cast<std::uint8_t>( iodir & ~mask );
+
+    EXPECT_CALL( mcp23008, iodir() ).WillOnce( Return( iodir ) );
+    EXPECT_CALL( mcp23008, write( 0x00, data ) ).WillOnce( Return( Result<Void, Error_Code>{} ) );
+    EXPECT_CALL( mcp23008, cache_iodir( data ) );
+
+    EXPECT_FALSE( mcp23008.configure_pin_as_push_pull_output( mask ).is_error() );
+}
+
+/**
  * \brief Execute the picolibrary::Microchip::MCP23008::Driver unit tests.
  *
  * \param[in] argc The number of arguments to pass to testing::InitGoogleMock().
