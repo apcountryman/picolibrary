ffbbc9d3c6fde7a19a426225d7b4cc2c3e4404a1
Andrew Countryman <apcountryman@gmail.com>
2021-05-12 17:25:25 -0700

    Add HSM event category
    Resolves #451 (Add HSM event category).

--- include/picolibrary/testing/unit/hsm.h
+++ include/picolibrary/testing/unit/hsm.h
@@ -23,10 +23,54 @@
 #ifndef PICOLIBRARY_TESTING_UNIT_HSM_H
 #define PICOLIBRARY_TESTING_UNIT_HSM_H
 
+#include "gmock/gmock.h"
+#include "picolibrary/hsm.h"
+
 /**
  * \brief Hierarchical State Machine (HSM) unit testing facilities.
  */
 namespace picolibrary::Testing::Unit::HSM {
+
+/**
+ * \brief Mock event category.
+ */
+class Mock_Event_Category : public ::picolibrary::HSM::Event_Category {
+  public:
+    /**
+     * \brief Get a reference to the mock event category instance.
+     *
+     * \return A reference to the mock event category instance.
+     */
+    static auto const & instance()
+    {
+        static auto category = Mock_Event_Category{};
+
+        return category;
+    }
+
+    /**
+     * \brief Constructor.
+     */
+    Mock_Event_Category() = default;
+
+    Mock_Event_Category( Mock_Event_Category && ) = delete;
+
+    Mock_Event_Category( Mock_Event_Category const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Mock_Event_Category() noexcept = default;
+
+    auto operator=( Mock_Event_Category && ) = delete;
+
+    auto operator=( Mock_Event_Category const & ) = delete;
+
+    MOCK_METHOD( char const *, name, (), ( const, noexcept, override ) );
+
+    MOCK_METHOD( char const *, event_description, ( ::picolibrary::HSM::Event_ID ), ( const, noexcept, override ) );
+};
+
 } // namespace picolibrary::Testing::Unit::HSM
 
 #endif // PICOLIBRARY_TESTING_UNIT_HSM_H
