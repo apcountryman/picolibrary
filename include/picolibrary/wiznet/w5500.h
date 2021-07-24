fa461ac66d37fc8b1f60a68a14d3f439b9ee7721
Andrew Countryman <apcountryman@gmail.com>
2021-07-24 12:32:51 -0700

    Add SN_FRAG register read/write
    Supports #508.

--- include/picolibrary/wiznet/w5500.h
+++ include/picolibrary/wiznet/w5500.h
@@ -2577,6 +2577,34 @@ class Driver : public Communication_Controller {
         return write( socket_id, SN_IMR::OFFSET, data );
     }
 
+    /**
+     * \brief Read a socket's SN_FRAG register.
+     *
+     * \param[in] socket_id The ID of the socket whose SN_FRAG register is to be read.
+     *
+     * \return The data read from the socket's SN_FRAG register if the read succeeded.
+     * \return An error code if the read failed.
+     */
+    auto read_sn_frag( Socket_ID socket_id ) const noexcept
+    {
+        return read<SN_FRAG::Type>( socket_id, SN_FRAG::OFFSET );
+    }
+
+    /**
+     * \brief Write to a socket's SN_FRAG register.
+     *
+     * \param[in] socket_id The ID of the socket whose SN_FRAG register is to be written
+     *            to.
+     * \param[in] data The data to write to the socket's SN_FRAG register.
+     *
+     * \return Nothing if the write succeeded.
+     * \return An error code if the write failed.
+     */
+    auto write_sn_frag( Socket_ID socket_id, SN_FRAG::Type data ) noexcept
+    {
+        return write( socket_id, SN_FRAG::OFFSET, data );
+    }
+
   private:
     /**
      * \brief Read a common register.
--- test/unit/picolibrary/wiznet/w5500/driver/main.cc
+++ test/unit/picolibrary/wiznet/w5500/driver/main.cc
@@ -2624,6 +2624,77 @@ TEST( writeSNIMR, worksProperly )
 }
 
 /**
+ * \brief Verify picolibrary::WIZnet::W5500::Driver::read_sn_frag() properly handles a
+ *        read error.
+ */
+TEST( readSNFRAG, readError )
+{
+    auto const w5500 = Driver{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( w5500, read( _, _, _, _ ) ).WillOnce( Return( error ) );
+
+    auto const result = w5500.read_sn_frag( random<Socket_ID>() );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify picolibrary::WIZnet::W5500::Driver::read_sn_frag() works properly.
+ */
+TEST( readSNFRAG, worksProperly )
+{
+    auto const w5500 = Driver{};
+
+    auto const socket_id = random<Socket_ID>();
+    auto const data      = random<std::uint16_t>();
+
+    EXPECT_CALL( w5500, read( socket_id, Region::REGISTERS, 0x002D, _ ) )
+        .WillOnce( Return( convert_data_to_vector( data ) ) );
+
+    auto const result = w5500.read_sn_frag( socket_id );
+
+    EXPECT_TRUE( result.is_value() );
+    EXPECT_EQ( result.value(), data );
+}
+
+/**
+ * \brief Verify picolibrary::WIZnet::W5500::Driver::write_sn_frag() properly handles a
+ *        write error.
+ */
+TEST( writeSNFRAG, writeError )
+{
+    auto w5500 = Driver{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( w5500, write( _, _, _, A<std::vector<std::uint8_t>>() ) ).WillOnce( Return( error ) );
+
+    auto const result = w5500.write_sn_frag( random<Socket_ID>(), random<std::uint16_t>() );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+}
+
+/**
+ * \brief Verify picolibrary::WIZnet::W5500::Driver::write_sn_frag() works properly.
+ */
+TEST( writeSNFRAG, worksProperly )
+{
+    auto w5500 = Driver{};
+
+    auto const socket_id = random<Socket_ID>();
+    auto const data      = random<std::uint16_t>();
+
+    EXPECT_CALL( w5500, write( socket_id, Region::REGISTERS, 0x002D, convert_data_to_vector( data ) ) )
+        .WillOnce( Return( Result<Void, Error_Code>{} ) );
+
+    EXPECT_FALSE( w5500.write_sn_frag( socket_id, data ).is_error() );
+}
+
+/**
  * \brief Execute the picolibrary::WIZnet::W5500::Driver unit tests.
  *
  * \param[in] argc The number of arguments to pass to testing::InitGoogleMock().
