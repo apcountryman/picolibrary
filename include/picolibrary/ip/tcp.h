e2840f8e42aa497777a54aeebc8712445a8a9e7f
Andrew Countryman <apcountryman@gmail.com>
2021-08-30 14:28:27 -0700

    Add TCP over IP endpoint
    Resolves #637 (Add TCP over IP endpoint).

--- include/picolibrary/ip/tcp.h
+++ include/picolibrary/ip/tcp.h
@@ -25,6 +25,13 @@
 
 #include <cstdint>
 
+#include "picolibrary/error.h"
+#include "picolibrary/format.h"
+#include "picolibrary/ip.h"
+#include "picolibrary/result.h"
+#include "picolibrary/stream.h"
+#include "picolibrary/void.h"
+
 /**
  * \brief Transmission Control Protocol (TCP) over IP facilities.
  */
@@ -35,6 +42,263 @@ namespace picolibrary::IP::TCP {
  */
 using Port = std::uint16_t;
 
+/**
+ * \brief Endpoint.
+ */
+class Endpoint {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Endpoint() noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] port The endpoint's port number.
+     */
+    constexpr Endpoint( Port port ) noexcept : m_address{}, m_port{ port }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] address The endpoint's address.
+     * \param[in] port The endpoint's port number.
+     */
+    constexpr Endpoint( Address const & address, Port port ) noexcept :
+        m_address{ address },
+        m_port{ port }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Endpoint( Endpoint && source ) noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] original The original to copy.
+     */
+    constexpr Endpoint( Endpoint const & original ) noexcept = default;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Endpoint() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Endpoint && expression ) noexcept -> Endpoint & = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Endpoint const & expression ) noexcept -> Endpoint & = default;
+
+    /**
+     * \brief Get the endpoint's address.
+     *
+     * \return The endpoint's address.
+     */
+    constexpr auto const & address() const noexcept
+    {
+        return m_address;
+    }
+
+    /**
+     * \brief Get the endpoint's port number.
+     *
+     * \return The endpoint's port number.
+     */
+    constexpr auto port() const noexcept
+    {
+        return m_port;
+    }
+
+  private:
+    /**
+     * \brief The endpoint's address.
+     */
+    Address m_address{};
+
+    /**
+     * \brief The endpoint's port number.
+     */
+    Port m_port{};
+};
+
+/**
+ * \brief Equality operator.
+ *
+ * \relatedalso picolibrary::IP::TCP::Endpoint
+ *
+ * \param[in] lhs The left hand side of the comparison.
+ * \param[in] rhs The right hand side of the comparison.
+ *
+ * \return true if lhs is equal to rhs.
+ * \return false if lhs is not equal to rhs.
+ */
+constexpr auto operator==( Endpoint const & lhs, Endpoint const & rhs ) noexcept
+{
+    return lhs.address() == rhs.address() and lhs.port() == rhs.port();
+}
+
+/**
+ * \brief Inequality operator.
+ *
+ * \relatedalso picolibrary::IP::TCP::Endpoint
+ *
+ * \param[in] lhs The left hand side of the comparison.
+ * \param[in] rhs The right hand side of the comparison.
+ *
+ * \return true if lhs is not equal to rhs.
+ * \return false if lhs is equal to rhs.
+ */
+constexpr auto operator!=( Endpoint const & lhs, Endpoint const & rhs ) noexcept
+{
+    return not( lhs == rhs );
+}
+
+/**
+ * \brief Less than operator.
+ *
+ * \relatedalso picolibrary::IP::TCP::Endpoint
+ *
+ * \param[in] lhs The left hand side of the comparison.
+ * \param[in] rhs The right hand side of the comparison.
+ *
+ * \return true if lhs is less than rhs.
+ * \return false if lhs is not less than rhs.
+ */
+constexpr auto operator<( Endpoint const & lhs, Endpoint const & rhs ) noexcept
+{
+    return lhs.address() < rhs.address()
+           or ( lhs.address() == rhs.address() and lhs.port() < rhs.port() );
+}
+
+/**
+ * \brief Greater than operator.
+ *
+ * \relatedalso picolibrary::IP::TCP::Endpoint
+ *
+ * \param[in] lhs The left hand side of the comparison.
+ * \param[in] rhs The right hand side of the comparison.
+ *
+ * \return true if lhs is greater than rhs.
+ * \return false if lhs is not greater than rhs.
+ */
+constexpr auto operator>( Endpoint const & lhs, Endpoint const & rhs ) noexcept
+{
+    return rhs < lhs;
+}
+
+/**
+ * \brief Less than or equal to operator.
+ *
+ * \relatedalso picolibrary::IP::TCP::Endpoint
+ *
+ * \param[in] lhs The left hand side of the comparison.
+ * \param[in] rhs The right hand side of the comparison.
+ *
+ * \return true if lhs is less than or equal to rhs.
+ * \return false if lhs is not less than or equal to rhs.
+ */
+constexpr auto operator<=( Endpoint const & lhs, Endpoint const & rhs ) noexcept
+{
+    return not( lhs > rhs );
+}
+
+/**
+ * \brief Greater than or equal to operator.
+ *
+ * \relatedalso picolibrary::IP::TCP::Endpoint
+ *
+ * \param[in] lhs The left hand side of the comparison.
+ * \param[in] rhs The right hand side of the comparison.
+ *
+ * \return true if lhs is greater than or equal to rhs.
+ * \return false if lhs is not greater than or equal to rhs.
+ */
+constexpr auto operator>=( Endpoint const & lhs, Endpoint const & rhs ) noexcept
+{
+    return not( lhs < rhs );
+}
+
 } // namespace picolibrary::IP::TCP
+
+namespace picolibrary {
+
+/**
+ * \brief picolibrary::IP::TCP::Endpoint output formatter.
+ *
+ * picolibrary::IP::TCP::Endpoint only supports the default format specification ("{}").
+ */
+template<>
+class Output_Formatter<IP::TCP::Endpoint> {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Output_Formatter() noexcept = default;
+
+    Output_Formatter( Output_Formatter && ) = delete;
+
+    Output_Formatter( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Output_Formatter() noexcept = default;
+
+    auto operator=( Output_Formatter && ) = delete;
+
+    auto operator=( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Parse the format specification for the picolibrary::IP::TCP::Endpoint to be
+     *        formatted.
+     *
+     * \param[in] format The format specification for the picolibrary::IP::TCP::Endpoint
+     *            to be formatted.
+     *
+     * \return format.
+     */
+    constexpr auto parse( char const * format ) noexcept -> Result<char const *, Void>
+    {
+        return format;
+    }
+
+    /**
+     * \brief Write the picolibrary::IP::TCP::Endpoint to the stream.
+     *
+     * \param[in] stream The stream to write the picolibrary::IP::TCP::Endpoint to.
+     * \param[in] address The picolibrary::IP::TCP::Endpoint to write to the stream.
+     *
+     * \return Nothing if the write succeeded.
+     * \return An error code if the write failed.
+     */
+    auto print( Output_Stream & stream, IP::TCP::Endpoint const & endpoint ) noexcept
+        -> Result<Void, Error_Code>
+    {
+        return stream.print( "{}:{}", endpoint.address(), Format::Decimal{ endpoint.port() } );
+    }
+};
+
+} // namespace picolibrary
 
 #endif // PICOLIBRARY_IP_TCP_H
---
+++ test/unit/picolibrary/ip/tcp/endpoint/main.cc
@@ -0,0 +1,375 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2021, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::IP::TCP::Endpoint unit test program.
+ */
+
+#include <cstdint>
+#include <sstream>
+#include <string>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "picolibrary/error.h"
+#include "picolibrary/ip.h"
+#include "picolibrary/ip/tcp.h"
+#include "picolibrary/ipv4.h"
+#include "picolibrary/testing/unit/error.h"
+#include "picolibrary/testing/unit/ip.h"
+#include "picolibrary/testing/unit/random.h"
+#include "picolibrary/testing/unit/stream.h"
+
+namespace {
+
+using ::picolibrary::Generic_Error;
+using ::picolibrary::IP::Address;
+using ::picolibrary::IP::TCP::Endpoint;
+using ::picolibrary::IP::TCP::Port;
+using ::picolibrary::Testing::Unit::Mock_Error;
+using ::picolibrary::Testing::Unit::Mock_Output_Stream;
+using ::picolibrary::Testing::Unit::Output_String_Stream;
+using ::picolibrary::Testing::Unit::random;
+using ::picolibrary::Testing::Unit::random_container;
+using ::testing::A;
+using ::testing::Return;
+
+using IPv4_Address = ::picolibrary::IPv4::Address;
+
+auto unspecified_address_with_port( Port port )
+{
+    auto stream = std::ostringstream{};
+
+    stream << ':' << port;
+
+    return stream.str();
+}
+
+auto dot_decimal_with_port( IPv4_Address const & address, Port port )
+{
+    auto stream = std::ostringstream{};
+
+    stream << static_cast<std::uint_fast16_t>( address.as_byte_array()[ 0 ] ) << '.'
+           << static_cast<std::uint_fast16_t>( address.as_byte_array()[ 1 ] ) << '.'
+           << static_cast<std::uint_fast16_t>( address.as_byte_array()[ 2 ] ) << '.'
+           << static_cast<std::uint_fast16_t>( address.as_byte_array()[ 3 ] ) << ':' << port;
+
+    return stream.str();
+}
+
+} // namespace
+
+/**
+ * \brief Verify picolibrary::IP::TCP::Endpoint::Endpoint() works properly.
+ */
+TEST( constructorDefault, worksProperly )
+{
+    auto const endpoint = Endpoint{};
+
+    EXPECT_EQ( endpoint.address(), Address{} );
+    EXPECT_EQ( endpoint.port(), 0 );
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::Endpoint::Endpoint( picolibrary::IP::TCP::Port )
+ *        works properly.
+ */
+TEST( constructorPort, worksProperly )
+{
+    auto const port = random<Port>();
+
+    auto const endpoint = Endpoint{ port };
+
+    EXPECT_EQ( endpoint.address(), Address{} );
+    EXPECT_EQ( endpoint.port(), port );
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::Endpoint::Endpoint( picolibrary::IP::Address const
+ *        &, picolibrary::IP::TCP::Port ) works properly.
+ */
+TEST( constructorAddressPort, worksProperly )
+{
+    auto const address = random<Address>();
+    auto const port    = random<Port>();
+
+    auto const endpoint = Endpoint{ address, port };
+
+    EXPECT_EQ( endpoint.address(), address );
+    EXPECT_EQ( endpoint.port(), port );
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::operator==( picolibrary::IP::TCP::Endpoint const &,
+ *        picolibrary::IP::TCP::Endpoint const & ) works properly.
+ */
+TEST( equalityOperator, worksProperly )
+{
+    {
+        EXPECT_TRUE( Endpoint{} == Endpoint{} );
+    }
+
+    {
+        auto const lhs_port = random<Port>();
+        auto const rhs_port = random<Port>();
+
+        EXPECT_EQ( Endpoint{ lhs_port } == Endpoint{ rhs_port }, lhs_port == rhs_port );
+    }
+
+    {
+        auto const lhs_address = random<Address>();
+        auto const lhs_port    = random<Port>();
+        auto const rhs_address = random<Address>();
+        auto const rhs_port    = random<Port>();
+
+        EXPECT_EQ(
+            ( Endpoint{ lhs_address, lhs_port } == Endpoint{ rhs_address, rhs_port } ),
+            lhs_address == rhs_address and lhs_port == rhs_port );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::operator!=( picolibrary::IP::TCP::Endpoint const &,
+ *        picolibrary::IP::TCP::Endpoint const & ) works properly.
+ */
+TEST( inequalityOperator, worksProperly )
+{
+    {
+        EXPECT_FALSE( Endpoint{} != Endpoint{} );
+    }
+
+    {
+        auto const lhs_port = random<Port>();
+        auto const rhs_port = random<Port>();
+
+        EXPECT_EQ( Endpoint{ lhs_port } != Endpoint{ rhs_port }, lhs_port != rhs_port );
+    }
+
+    {
+        auto const lhs_address = random<Address>();
+        auto const lhs_port    = random<Port>();
+        auto const rhs_address = random<Address>();
+        auto const rhs_port    = random<Port>();
+
+        EXPECT_EQ(
+            ( Endpoint{ lhs_address, lhs_port } != Endpoint{ rhs_address, rhs_port } ),
+            lhs_address != rhs_address or lhs_port != rhs_port );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::operator<( picolibrary::IP::TCP::Endpoint const &,
+ *        picolibrary::IP::TCP::Endpoint const & ) works properly.
+ */
+TEST( lessThanOperator, worksProperly )
+{
+    {
+        EXPECT_FALSE( Endpoint{} < Endpoint{} );
+    }
+
+    {
+        auto const lhs_port = random<Port>();
+        auto const rhs_port = random<Port>();
+
+        EXPECT_EQ( Endpoint{ lhs_port } < Endpoint{ rhs_port }, lhs_port < rhs_port );
+    }
+
+    {
+        auto const lhs_address = random<Address>();
+        auto const lhs_port    = random<Port>();
+        auto const rhs_address = random<Address>();
+        auto const rhs_port    = random<Port>();
+
+        EXPECT_EQ(
+            ( Endpoint{ lhs_address, lhs_port } < Endpoint{ rhs_address, rhs_port } ),
+            lhs_address < rhs_address or ( lhs_address == rhs_address and lhs_port < rhs_port ) );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::operator>( picolibrary::IP::TCP::Endpoint const &,
+ *        picolibrary::IP::TCP::Endpoint const & ) works properly.
+ */
+TEST( greaterThanOperator, worksProperly )
+{
+    {
+        EXPECT_FALSE( Endpoint{} > Endpoint{} );
+    }
+
+    {
+        auto const lhs_port = random<Port>();
+        auto const rhs_port = random<Port>();
+
+        EXPECT_EQ( Endpoint{ lhs_port } > Endpoint{ rhs_port }, lhs_port > rhs_port );
+    }
+
+    {
+        auto const lhs_address = random<Address>();
+        auto const lhs_port    = random<Port>();
+        auto const rhs_address = random<Address>();
+        auto const rhs_port    = random<Port>();
+
+        EXPECT_EQ(
+            ( Endpoint{ lhs_address, lhs_port } > Endpoint{ rhs_address, rhs_port } ),
+            lhs_address > rhs_address or ( lhs_address == rhs_address and lhs_port > rhs_port ) );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::operator<=( picolibrary::IP::TCP::Endpoint const &,
+ *        picolibrary::IP::TCP::Endpoint const & ) works properly.
+ */
+TEST( lessThanOrEqualToOperator, worksProperly )
+{
+    {
+        EXPECT_TRUE( Endpoint{} <= Endpoint{} );
+    }
+
+    {
+        auto const lhs_port = random<Port>();
+        auto const rhs_port = random<Port>();
+
+        EXPECT_EQ( Endpoint{ lhs_port } <= Endpoint{ rhs_port }, lhs_port <= rhs_port );
+    }
+
+    {
+        auto const lhs_address = random<Address>();
+        auto const lhs_port    = random<Port>();
+        auto const rhs_address = random<Address>();
+        auto const rhs_port    = random<Port>();
+
+        EXPECT_EQ(
+            ( Endpoint{ lhs_address, lhs_port } <= Endpoint{ rhs_address, rhs_port } ),
+            lhs_address < rhs_address or ( lhs_address == rhs_address and lhs_port <= rhs_port ) );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::IP::TCP::operator>=( picolibrary::IP::TCP::Endpoint const &,
+ *        picolibrary::IP::TCP::Endpoint const & ) works properly.
+ */
+TEST( greaterThanOrEqualToOperator, worksProperly )
+{
+    {
+        EXPECT_TRUE( Endpoint{} >= Endpoint{} );
+    }
+
+    {
+        auto const lhs_port = random<Port>();
+        auto const rhs_port = random<Port>();
+
+        EXPECT_EQ( Endpoint{ lhs_port } >= Endpoint{ rhs_port }, lhs_port >= rhs_port );
+    }
+
+    {
+        auto const lhs_address = random<Address>();
+        auto const lhs_port    = random<Port>();
+        auto const rhs_address = random<Address>();
+        auto const rhs_port    = random<Port>();
+
+        EXPECT_EQ(
+            ( Endpoint{ lhs_address, lhs_port } >= Endpoint{ rhs_address, rhs_port } ),
+            lhs_address > rhs_address or ( lhs_address == rhs_address and lhs_port >= rhs_port ) );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::IP::TCP::Endpoint> properly
+ *        handles an invalid format string.
+ */
+TEST( outputFormatter, invalidFormatString )
+{
+    auto stream = Output_String_Stream{};
+
+    auto const result = stream.print(
+        ( std::string{ '{' } + random_container<std::string>( random<std::uint_fast8_t>( 1 ) ) + '}' )
+            .c_str(),
+        Endpoint{} );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), Generic_Error::INVALID_FORMAT );
+
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_TRUE( stream.io_error_present() );
+    EXPECT_FALSE( stream.fatal_error_present() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::IP::TCP::Endpoint> properly
+ *        handles a print error.
+ */
+TEST( outputFormatter, printError )
+{
+    auto stream = Mock_Output_Stream{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( stream.buffer(), put( A<std::string>() ) ).WillOnce( Return( error ) );
+
+    auto const result = stream.print( "{}", Endpoint{ random<IPv4_Address>(), random<Port>() } );
+
+    EXPECT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_FALSE( stream.io_error_present() );
+    EXPECT_TRUE( stream.fatal_error_present() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::IP::TCP::Endpoint> works
+ *        properly.
+ */
+TEST( outputFormatter, worksProperly )
+{
+    {
+        auto stream = Output_String_Stream{};
+
+        auto const port = random<Port>();
+
+        EXPECT_FALSE( stream.print( "{}", Endpoint{ port } ).is_error() );
+
+        EXPECT_EQ( stream.string(), unspecified_address_with_port( port ) );
+    }
+
+    {
+        auto stream = Output_String_Stream{};
+
+        auto const ipv4_address = random<IPv4_Address>();
+        auto const port         = random<Port>();
+
+        EXPECT_FALSE( stream.print( "{}", Endpoint{ ipv4_address, port } ).is_error() );
+
+        EXPECT_EQ( stream.string(), dot_decimal_with_port( ipv4_address, port ) );
+    }
+}
+
+/**
+ * \brief Execute the picolibrary::IP::TCP::Endpoint unit tests.
+ *
+ * \param[in] argc The number of arguments to pass to testing::InitGoogleMock().
+ * \param[in] argv The array  of arguments to pass to testing::InitGoogleMock().
+ *
+ * \return See Google Test's RUN_ALL_TESTS().
+ */
+int main( int argc, char * argv[] )
+{
+    ::testing::InitGoogleMock( &argc, argv );
+
+    return RUN_ALL_TESTS();
+}
