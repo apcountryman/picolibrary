710682cc1bba727d1316677e272f9c3307fb9c6f
Andrew Countryman <apcountryman@gmail.com>
2021-10-31 12:06:05 -0700

    Add class skeleton
    Supports #780.

--- include/picolibrary/wiznet/w5500/ip/tcp.h
+++ include/picolibrary/wiznet/w5500/ip/tcp.h
@@ -23,10 +23,56 @@
 #ifndef PICOLIBRARY_WIZNET_W5500_IP_TCP_H
 #define PICOLIBRARY_WIZNET_W5500_IP_TCP_H
 
+#include <cstdint>
+
 /**
  * \brief WIZnet W5500 TCP over IP facilities namespace.
  */
 namespace picolibrary::WIZnet::W5500::IP::TCP {
+
+/**
+ * \brief WIZnet W5500 TCP over IP client socket.
+ */
+template<typename Driver, typename Network_Stack>
+class Client {
+  public:
+    /**
+     * \brief The unsigned integral type used to report transmit/receive buffer
+     *        information.
+     */
+    using Size = std::uint16_t;
+
+    /**
+     * \brief Constructor.
+     */
+    constexpr Client() noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Client( Client && source ) noexcept = default;
+
+    Client( Client const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Client() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    auto operator=( Client && expression ) noexcept -> Client & = default;
+
+    auto operator=( Client const & ) = delete;
+};
+
 } // namespace picolibrary::WIZnet::W5500::IP::TCP
 
 #endif // PICOLIBRARY_WIZNET_W5500_IP_TCP_H
