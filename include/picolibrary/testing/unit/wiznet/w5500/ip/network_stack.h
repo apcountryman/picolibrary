9da6f90efef4444e64300bfc08c15ec17906a038
Andrew Countryman <apcountryman@gmail.com>
2021-12-05 10:52:50 -0800

    Fix mock WIZnet W5500 IP network stack specific socket allocation function return type (#822)
    Resolves #820 (Fix mock WIZnet W5500 IP network stack specific socket
    allocation function return type).

--- include/picolibrary/testing/unit/wiznet/w5500/ip/network_stack.h
+++ include/picolibrary/testing/unit/wiznet/w5500/ip/network_stack.h
@@ -113,10 +113,7 @@ class Mock_Network_Stack {
     MOCK_METHOD( (Result<Void, Error_Code>), service, () );
 
     MOCK_METHOD( (Result<::picolibrary::WIZnet::W5500::Socket_ID, Error_Code>), allocate_socket, () );
-    MOCK_METHOD(
-        (Result<::picolibrary::WIZnet::W5500::Socket_ID, Error_Code>),
-        allocate_socket,
-        ( ::picolibrary::WIZnet::W5500::Socket_ID ) );
+    MOCK_METHOD( (Result<Void, Error_Code>), allocate_socket, ( ::picolibrary::WIZnet::W5500::Socket_ID ) );
     MOCK_METHOD( void, deallocate_socket, ( ::picolibrary::WIZnet::W5500::Socket_ID ) );
 
     MOCK_METHOD(
