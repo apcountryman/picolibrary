2046ea2f11cfcd163119040abd16e5f5a17d8710
Andrew Countryman <apcountryman@gmail.com>
2022-01-03 19:00:22 -0800

    Add max algorithm
    Resolves #865 (Add max algorithm).

--- include/picolibrary/algorithm.h
+++ include/picolibrary/algorithm.h
@@ -61,6 +61,23 @@ constexpr auto const & min( T const & a, T const & b ) noexcept
 }
 
 /**
+ * \brief Get the maximum of two values.
+ *
+ * \tparam T The type to compare.
+ *
+ * \param[in] a The first of the two values to compare.
+ * \param[in] b The second of the two values to compare.
+ *
+ * \return The maximum of the two values. If the two values are equal, the first argument
+ *         will be returned.
+ */
+template<typename T>
+constexpr auto const & max( T const & a, T const & b ) noexcept
+{
+    return b > a ? b : a;
+}
+
+/**
  * Check if two ranges are equal.
  *
  * \tparam Iterator_1 Range iterator for the first of the two ranges to be compared.
