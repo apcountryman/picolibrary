a4c5948e2431394c5e394855e10fc7a5a57ca8ca
Andrew Countryman <apcountryman@gmail.com>
2022-01-12 20:04:38 -0800

    Add error code enum type property
    Resolves #882 (Add error code enum type property).

--- include/picolibrary/error.h
+++ include/picolibrary/error.h
@@ -24,6 +24,7 @@
 #define PICOLIBRARY_ERROR_H
 
 #include <cstdint>
+#include <type_traits>
 
 namespace picolibrary {
 
@@ -33,6 +34,23 @@ namespace picolibrary {
  * \attention This type is the expected underlying type for all error code enums.
  */
 using Error_ID = std::uint_fast8_t;
+
+/**
+ * \brief Check if an enum is an error code enum.
+ *
+ * \tparam Enum The enum to check.
+ */
+template<typename Enum>
+struct is_error_code_enum : std::false_type {
+};
+
+/**
+ * \brief Check if an enum is an error code enum.
+ *
+ * \tparam Enum The enum to check.
+ */
+template<typename Enum>
+constexpr auto is_error_code_enum_v = is_error_code_enum<Enum>::value;
 
 } // namespace picolibrary
 
