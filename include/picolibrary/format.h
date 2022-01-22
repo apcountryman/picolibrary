9772b863595df84f0aaac5b42ec0ef43d3b0edc1
Andrew Countryman <apcountryman@gmail.com>
2022-01-21 18:11:07 -0800

    Add decimal integer output format specifier (#978)
    Resolves #970 (Add decimal integer output format specifier).

--- include/picolibrary/format.h
+++ include/picolibrary/format.h
@@ -24,6 +24,7 @@
 #define PICOLIBRARY_FORMAT_H
 
 #include <cstddef>
+#include <cstdlib>
 #include <cstring>
 #include <limits>
 #include <type_traits>
@@ -95,6 +96,81 @@ class Binary {
      * \return The assigned to object.
      */
     constexpr auto operator=( Binary const & expression ) noexcept -> Binary & = default;
+
+    /**
+     * \brief Get the integer to be formatted.
+     *
+     * \return The integer to be formatted.
+     */
+    constexpr operator Integer() const noexcept
+    {
+        return m_value;
+    }
+
+  private:
+    /**
+     * \brief The integer to be formatted.
+     */
+    Integer m_value{};
+};
+
+/**
+ * \brief Integer decimal output format specifier.
+ *
+ * \tparam Integer The integer type to format.
+ */
+template<typename Integer>
+class Decimal {
+  public:
+    static_assert( std::is_integral_v<Integer> );
+
+    Decimal() = delete;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] value The integer to be formatted.
+     */
+    constexpr Decimal( Integer value ) noexcept : m_value{ value }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Decimal( Decimal && source ) noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] original The original to copy.
+     */
+    constexpr Decimal( Decimal const & original ) noexcept = default;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Decimal() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Decimal && expression ) noexcept -> Decimal & = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Decimal const & expression ) noexcept -> Decimal & = default;
 
     /**
      * \brief Get the integer to be formatted.
@@ -199,6 +275,163 @@ class Output_Formatter<Format::Binary<Integer>> {
         } // if
 
         return binary.size();
+    }
+};
+
+/**
+ * \brief picolibrary::Format::Decimal output formatter.
+ *
+ * picolibrary::Format::Decimal only supports the default format specification ("{}").
+ *
+ * \tparam Integer The type of integer to print.
+ */
+template<typename Integer>
+class Output_Formatter<Format::Decimal<Integer>, std::enable_if_t<std::is_signed_v<Integer>>> {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Output_Formatter() noexcept = default;
+
+    Output_Formatter( Output_Formatter && ) = delete;
+
+    Output_Formatter( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Output_Formatter() noexcept = default;
+
+    auto operator=( Output_Formatter && ) = delete;
+
+    auto operator=( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Parse the format specification for the picolibrary::Format::Decimal to be
+     *        formatted.
+     *
+     * \param[in] format The format specification for the picolibrary::Format::Decimal to
+     *            be formatted.
+     *
+     * \return format
+     */
+    constexpr auto parse( char const * format ) noexcept
+    {
+        return format;
+    }
+
+    /**
+     * \brief Write the formatted picolibrary::Format::Decimal to the stream.
+     *
+     * \param[in] stream The stream to write the formatted picolibrary::Format::Decimal
+     *            to.
+     * \param[in] value The integer to format.
+     *
+     * \return The number of characters written to the stream if the write succeeded.
+     * \return An error code if the write failed.
+     */
+    auto print( Output_Stream & stream, Integer value ) noexcept -> Result<std::size_t, Error_Code>
+    {
+        // #lizard forgives the length
+
+        Array<char, 1 + std::numeric_limits<Integer>::digits10 + 1> decimal;
+
+        auto is_negative = value < 0;
+
+        auto i = decimal.rbegin();
+
+        do {
+            *i = '0' + std::abs( value % 10 );
+
+            ++i;
+            value /= 10;
+        } while ( value );
+
+        if ( is_negative ) {
+            *i = '-';
+
+            ++i;
+        } // if
+
+        auto result = stream.put( i.base(), decimal.end() );
+        if ( result.is_error() ) {
+            return result.error();
+        } // if
+
+        return static_cast<std::size_t>( decimal.end() - i.base() );
+    }
+};
+
+/**
+ * \brief picolibrary::Format::Decimal output formatter.
+ *
+ * picolibrary::Format::Decimal only supports the default format specification ("{}").
+ *
+ * \tparam Integer The type of integer to print.
+ */
+template<typename Integer>
+class Output_Formatter<Format::Decimal<Integer>, std::enable_if_t<std::is_unsigned_v<Integer>>> {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Output_Formatter() noexcept = default;
+
+    Output_Formatter( Output_Formatter && ) = delete;
+
+    Output_Formatter( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Output_Formatter() noexcept = default;
+
+    auto operator=( Output_Formatter && ) = delete;
+
+    auto operator=( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Parse the format specification for the picolibrary::Format::Decimal to be
+     *        formatted.
+     *
+     * \param[in] format The format specification for the picolibrary::Format::Decimal to
+     *            be formatted.
+     *
+     * \return format
+     */
+    constexpr auto parse( char const * format ) noexcept
+    {
+        return format;
+    }
+
+    /**
+     * \brief Write the formatted picolibrary::Format::Decimal to the stream.
+     *
+     * \param[in] stream The stream to write the formatted picolibrary::Format::Decimal
+     *            to.
+     * \param[in] value The integer to format.
+     *
+     * \return The number of characters written to the stream if the write succeeded.
+     * \return An error code if the write failed.
+     */
+    auto print( Output_Stream & stream, Integer value ) noexcept -> Result<std::size_t, Error_Code>
+    {
+        Array<char, std::numeric_limits<Integer>::digits10 + 1> decimal;
+
+        auto i = decimal.rbegin();
+        do {
+            *i = '0' + ( value % 10 );
+
+            ++i;
+            value /= 10;
+        } while ( value );
+
+        auto result = stream.put( i.base(), decimal.end() );
+        if ( result.is_error() ) {
+            return result.error();
+        } // if
+
+        return static_cast<std::size_t>( decimal.end() - i.base() );
     }
 };
 
---
+++ test/unit/picolibrary/format/decimal/main.cc
@@ -0,0 +1,171 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2022, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Format::Decimal unit test program.
+ */
+
+#include <cstddef>
+#include <cstdint>
+#include <sstream>
+#include <string>
+#include <type_traits>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "picolibrary/format.h"
+#include "picolibrary/testing/unit/error.h"
+#include "picolibrary/testing/unit/random.h"
+#include "picolibrary/testing/unit/stream.h"
+
+namespace {
+
+using ::picolibrary::Format::Decimal;
+using ::picolibrary::Testing::Unit::Mock_Error;
+using ::picolibrary::Testing::Unit::Mock_Output_Stream;
+using ::picolibrary::Testing::Unit::Output_String_Stream;
+using ::picolibrary::Testing::Unit::random;
+using ::picolibrary::Testing::Unit::random_format_string;
+using ::testing::A;
+using ::testing::Return;
+
+template<typename Integer>
+auto decimal( Integer value )
+{
+    auto stream = std::ostringstream{};
+
+    stream << static_cast<
+        std::conditional_t<std::is_same_v<Integer, std::int8_t>, std::int_fast16_t, std::conditional_t<std::is_same_v<Integer, std::uint8_t>, std::uint_fast16_t, Integer>>>(
+        value );
+
+    return stream.str();
+}
+
+} // namespace
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Decimal> unit test fixture.
+ *
+ * \tparam Integer The type of integer to print.
+ */
+template<typename Integer>
+class outputFormatterDecimalDeathTest : public ::testing::Test {
+};
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Decimal> unit test fixture.
+ *
+ * \tparam Integer The type of integer to print.
+ */
+template<typename Integer>
+class outputFormatterDecimal : public ::testing::Test {
+};
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Decimal> unit test integer types.
+ */
+using Integers =
+    ::testing::Types<std::int8_t, std::uint8_t, std::int16_t, std::uint16_t, std::int32_t, std::uint32_t, std::int64_t, std::uint64_t>;
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Decimal> unit test fixture.
+ */
+TYPED_TEST_SUITE( outputFormatterDecimalDeathTest, Integers );
+
+/**
+ * \brief picolibrary::Output_Formatter<picolibrary::Decimal> unit test fixture.
+ */
+TYPED_TEST_SUITE( outputFormatterDecimal, Integers );
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::Format::Decimal> properly
+ *        handles an invalid format string.
+ */
+TYPED_TEST( outputFormatterDecimalDeathTest, invalidFormatString )
+{
+    using Integer = TypeParam;
+
+    EXPECT_DEATH(
+        {
+            static_cast<void>( Output_String_Stream{}.print(
+                ( std::string{ '{' } + random_format_string( random<std::size_t>( 1, 15 ) ) + '}' )
+                    .c_str(),
+                Decimal{ random<Integer>() } ) );
+        },
+        "::picolibrary::Generic_Error::INVALID_FORMAT" );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::Format::Decimal> properly
+ *        handles a put error.
+ */
+TYPED_TEST( outputFormatterDecimal, putError )
+{
+    using Integer = TypeParam;
+
+    auto stream = Mock_Output_Stream{};
+
+    auto const error = random<Mock_Error>();
+
+    EXPECT_CALL( stream.buffer(), put( A<std::string>() ) ).WillOnce( Return( error ) );
+
+    auto const result = stream.print( "{}", Decimal{ random<Integer>() } );
+
+    ASSERT_TRUE( result.is_error() );
+    EXPECT_EQ( result.error(), error );
+
+    EXPECT_FALSE( stream.end_of_file_reached() );
+    EXPECT_FALSE( stream.io_error_present() );
+    EXPECT_TRUE( stream.fatal_error_present() );
+}
+
+/**
+ * \brief Verify picolibrary::Output_Formatter<picolibrary::Format::Decimal> works
+ *        properly.
+ */
+TYPED_TEST( outputFormatterDecimal, worksProperly )
+{
+    using Integer = TypeParam;
+
+    auto stream = Output_String_Stream{};
+
+    auto const value = random<Integer>();
+
+    auto const result = stream.print( "{}", Decimal{ value } );
+
+    ASSERT_TRUE( result.is_value() );
+    EXPECT_EQ( result.value(), stream.string().size() );
+
+    EXPECT_TRUE( stream.is_nominal() );
+    EXPECT_EQ( stream.string(), decimal( value ) );
+}
+
+/**
+ * \brief Execute the picolibrary::Format::Decimal unit tests.
+ *
+ * \param[in] argc The number of arguments to pass to testing::InitGoogleMock().
+ * \param[in] argc The array of arguments to pass to testing::InitGoogleMock().
+ *
+ * \return See Google Test's RUN_ALL_TESTS().
+ */
+int main( int argc, char * argv[] )
+{
+    ::testing::InitGoogleMock( &argc, argv );
+
+    return RUN_ALL_TESTS();
+}
