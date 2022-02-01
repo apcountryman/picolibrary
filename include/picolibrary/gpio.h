716d8b9534accee641001e952f6681b30a1c66b4
Andrew Countryman <apcountryman@gmail.com>
2022-01-31 18:02:29 -0800

    Add GPIO facilities namespace
    Resolves #983 (Add GPIO facilities namespace).

---
+++ include/picolibrary/gpio.h
@@ -0,0 +1,32 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2022, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::GPIO interface.
+ */
+
+#ifndef PICOLIBRARY_GPIO_H
+#define PICOLIBRARY_GPIO_H
+
+/**
+ * \brief General Purpose Input/Output (GPIO) facilities.
+ */
+namespace picolibrary::GPIO {
+} // namespace picolibrary::GPIO
+
+#endif // PICOLIBRARY_GPIO_H
