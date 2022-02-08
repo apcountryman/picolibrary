e409d7a069d3b646e2d8a0e7d6430a744752620d
Andrew Countryman <apcountryman@gmail.com>
2022-02-07 16:51:52 -0800

    Add insufficient capacity generic error (#1017)
    Resolves #1015 (Add insufficient capacity generic error).

--- test/unit/picolibrary/generic_error_category/main.cc
+++ test/unit/picolibrary/generic_error_category/main.cc
@@ -52,12 +52,13 @@ TEST( errorDescription, worksProperly )
     } const test_cases[]{
         // clang-format off
 
-        { Generic_Error::INVALID_ARGUMENT,   "INVALID_ARGUMENT"   },
-        { Generic_Error::INVALID_FORMAT,     "INVALID_FORMAT"     },
-        { Generic_Error::IO_STREAM_DEGRADED, "IO_STREAM_DEGRADED" },
-        { Generic_Error::LOGIC_ERROR,        "LOGIC_ERROR"        },
-        { Generic_Error::OUT_OF_RANGE,       "OUT_OF_RANGE"       },
-        { Generic_Error::RUNTIME_ERROR,      "RUNTIME_ERROR"      },
+        { Generic_Error::INSUFFICIENT_CAPACITY, "INSUFFICIENT_CAPACITY" },
+        { Generic_Error::INVALID_ARGUMENT,      "INVALID_ARGUMENT"      },
+        { Generic_Error::INVALID_FORMAT,        "INVALID_FORMAT"        },
+        { Generic_Error::IO_STREAM_DEGRADED,    "IO_STREAM_DEGRADED"    },
+        { Generic_Error::LOGIC_ERROR,           "LOGIC_ERROR"           },
+        { Generic_Error::OUT_OF_RANGE,          "OUT_OF_RANGE"          },
+        { Generic_Error::RUNTIME_ERROR,         "RUNTIME_ERROR"         },
 
         // clang-format on
     };
