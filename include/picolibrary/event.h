77dc5c61a102ebd2d4c2435543206ecce265b0c6
Andrew Countryman <apcountryman@gmail.com>
2022-02-10 23:20:11 -0800

    Add event
    Resolves #1022 (Add event).

--- include/picolibrary/event.h
+++ include/picolibrary/event.h
@@ -23,7 +23,13 @@
 #ifndef PICOLIBRARY_EVENT_H
 #define PICOLIBRARY_EVENT_H
 
+#include <cstddef>
 #include <cstdint>
+
+#include "picolibrary/error.h"
+#include "picolibrary/result.h"
+#include "picolibrary/stream.h"
+#include "picolibrary/void.h"
 
 namespace picolibrary {
 
@@ -87,6 +93,202 @@ class Event_Category {
      * \brief Destructor.
      */
     ~Event_Category() noexcept = default;
+};
+
+/**
+ * \brief Event.
+ */
+class Event {
+  public:
+    Event() = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    virtual ~Event() noexcept = default;
+
+    /**
+     * \brief Get the event's category.
+     *
+     * \return The event's category.
+     */
+    constexpr auto const & category() const noexcept
+    {
+        return *m_category;
+    }
+
+    /**
+     * \brief Get the event's ID.
+     *
+     * \return The event's ID.
+     */
+    constexpr auto id() const noexcept
+    {
+        return m_id;
+    }
+
+    /**
+     * \brief Get the event's description.
+     *
+     * \return The event's description.
+     */
+    auto const * description() const noexcept
+    {
+        return category().event_description( id() );
+    }
+
+    /**
+     * \brief Write the event's details to a stream.
+     *
+     * \param[in] stream The stream to write the event's details to.
+     *
+     * \return 0.
+     */
+#ifndef PICOLIBRARY_SUPPRESS_HUMAN_READABLE_EVENT_INFORMATION
+    virtual auto print_details( Output_Stream & stream ) const noexcept -> Result<std::size_t, Error_Code>
+    {
+        static_cast<void>( stream );
+
+        return size_t{ 0 };
+    }
+#else  // PICOLIBRARY_SUPPRESS_HUMAN_READABLE_EVENT_INFORMATION
+    constexpr auto print_details( Output_Stream & stream ) const noexcept -> Result<std::size_t, Void>
+    {
+        static_cast<void>( stream );
+
+        return size_t{ 0 };
+    }
+#endif // PICOLIBRARY_SUPPRESS_HUMAN_READABLE_EVENT_INFORMATION
+
+  protected:
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] category The event's category.
+     * \param[in] id The event's ID.
+     */
+    constexpr Event( Event_Category const & category, Event_ID id ) noexcept :
+        m_category{ &category },
+        m_id{ id }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Event( Event && source ) noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] original The original to copy.
+     */
+    constexpr Event( Event const & original ) noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Event && expression ) noexcept -> Event & = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Event const & expression ) noexcept -> Event & = default;
+
+  private:
+    /**
+     * \brief The event's category.
+     */
+    Event_Category const * m_category{};
+
+    /**
+     * \brief The event's ID.
+     */
+    Event_ID m_id{};
+};
+
+/**
+ * \brief picolibrary::Event output formatter.
+ *
+ * picolibrary::Event only supports the default format specification ("{}").
+ */
+template<>
+class Output_Formatter<Event> {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Output_Formatter() noexcept = default;
+
+    Output_Formatter( Output_Formatter && ) = delete;
+
+    Output_Formatter( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Output_Formatter() noexcept = default;
+
+    auto operator=( Output_Formatter && ) = delete;
+
+    auto operator=( Output_Formatter const & ) = delete;
+
+    /**
+     * \brief Parse the format specification for the picolibrary::Event to be formatted.
+     *
+     * \param[in] format The format specification for the picolibrary::Event to be
+     *            formatted.
+     *
+     * \return format
+     */
+    constexpr auto parse( char const * format ) noexcept
+    {
+        return format;
+    }
+
+    /**
+     * \brief Write the formatted picolibrary::Event to the stream.
+     *
+     * \param[in] stream The stream to write the formatted picolibrary::Event to.
+     * \param[in] event The event to format.
+     *
+     * \return The number of characters written to the stream if the write succeeded.
+     * \return An error code if the write failed.
+     */
+    auto print( Output_Stream & stream, Event const & event ) noexcept -> Result<std::size_t, Error_Code>
+    {
+        auto characters_written = std::size_t{};
+
+        {
+            auto result = stream.print( "{}::{}", event.category().name(), event.description() );
+            if ( result.is_error() ) {
+                return result.error();
+            } // if
+
+            characters_written += result.value();
+        }
+
+        {
+            auto result = event.print_details( stream );
+            if ( result.is_error() ) {
+                return result.error();
+            } // if
+
+            characters_written += result.value();
+        }
+
+        return characters_written;
+    }
 };
 
 } // namespace picolibrary
