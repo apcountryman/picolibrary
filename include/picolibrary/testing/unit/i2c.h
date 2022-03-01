f5423a7add98948ee6dc309f7a68395359d89792
Andrew Countryman <apcountryman@gmail.com>
2022-03-01 13:25:09 -0800

    Add I2C address
    Resolves #1084 (Add I2C address).

--- include/picolibrary/testing/unit/i2c.h
+++ include/picolibrary/testing/unit/i2c.h
@@ -23,6 +23,99 @@
 #ifndef PICOLIBRARY_TESTING_UNIT_I2C_H
 #define PICOLIBRARY_TESTING_UNIT_I2C_H
 
+#include "picolibrary/i2c.h"
+#include "picolibrary/testing/unit/random.h"
+
+namespace picolibrary::Testing::Unit {
+
+/**
+ * \brief Generate a pseudo-random picolibrary::I2C::Address_Numeric within the specified
+ *        range.
+ *
+ * \param[in] min The lower bound of the allowable range.
+ * \param[in] max The upper bound of the allowable range.
+ *
+ * \return A pseudo-random picolibrary::I2C::Address_Numeric in the range [min,max].
+ */
+template<>
+inline auto random<I2C::Address_Numeric>( I2C::Address_Numeric min, I2C::Address_Numeric max )
+{
+    return I2C::Address_Numeric{ random<I2C::Address_Numeric::Unsigned_Integer>(
+        min.as_unsigned_integer(), max.as_unsigned_integer() ) };
+}
+
+/**
+ * \brief Generate a pseudo-random picolibrary::I2C::Address_Numeric greater than or equal
+ *        to a minimum picolibrary::I2C::Address_Numeric.
+ *
+ * \param[in] min The lower bound of the allowable range.
+ *
+ * \return A pseudo-random picolibrary::I2C::Address_Numeric in the range
+ *         [min,picolibrary::I2C::Address_Numeric::max()].
+ */
+template<>
+inline auto random<I2C::Address_Numeric>( I2C::Address_Numeric min )
+{
+    return random<I2C::Address_Numeric>( min, I2C::Address_Numeric::max() );
+}
+
+/**
+ * \brief Generate a pseudo-random picolibrary::I2C::Address_Numeric.
+ *
+ * \return A pseudo-random picolibrary::I2C::Address_Numeric.
+ */
+template<>
+inline auto random<I2C::Address_Numeric>()
+{
+    return random<I2C::Address_Numeric>( I2C::Address_Numeric::min(), I2C::Address_Numeric::max() );
+}
+
+/**
+ * \brief Generate a pseudo-random picolibrary::I2C::Address_Transmitted within the
+ *        specified range.
+ *
+ * \param[in] min The lower bound of the allowable range.
+ * \param[in] max The upper bound of the allowable range.
+ *
+ * \return A pseudo-random picolibrary::I2C::Address_Transmitted in the range [min,max].
+ */
+template<>
+inline auto random<I2C::Address_Transmitted>( I2C::Address_Transmitted min, I2C::Address_Transmitted max )
+{
+    return I2C::Address_Transmitted{ static_cast<I2C::Address_Transmitted::Unsigned_Integer>(
+        random<I2C::Address_Transmitted::Unsigned_Integer>( min.as_unsigned_integer(), max.as_unsigned_integer() )
+        & 0b1111111'0 ) };
+}
+
+/**
+ * \brief Generate a pseudo-random picolibrary::I2C::Address_Transmitted greater than or
+ *        equal to a minimum picolibrary::I2C::Address_Transmitted.
+ *
+ * \param[in] min The lower bound of the allowable range.
+ *
+ * \return A pseudo-random picolibrary::I2C::Address_Transmitted in the range
+ *         [min,picolibrary::I2C::Address_Transmitted::max()].
+ */
+template<>
+inline auto random<I2C::Address_Transmitted>( I2C::Address_Transmitted min )
+{
+    return random<I2C::Address_Transmitted>( min, I2C::Address_Transmitted::max() );
+}
+
+/**
+ * \brief Generate a pseudo-random picolibrary::I2C::Address_Transmitted.
+ *
+ * \return A pseudo-random picolibrary::I2C::Address_Transmitted.
+ */
+template<>
+inline auto random<I2C::Address_Transmitted>()
+{
+    return random<I2C::Address_Transmitted>(
+        I2C::Address_Transmitted::min(), I2C::Address_Transmitted::max() );
+}
+
+} // namespace picolibrary::Testing::Unit
+
 /**
  * \brief Inter-Integrated Circuit (I2C) unit testing facilities.
  */
---
+++ test/unit/picolibrary/i2c/address_transmitted/main.cc
@@ -0,0 +1,260 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2022, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::I2C::Address_Transmitted unit test program.
+ */
+
+#include <utility>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "picolibrary/i2c.h"
+#include "picolibrary/precondition.h"
+#include "picolibrary/testing/unit/i2c.h"
+#include "picolibrary/testing/unit/random.h"
+
+namespace {
+
+using ::picolibrary::BYPASS_PRECONDITION_EXPECTATION_CHECKS;
+using ::picolibrary::I2C::Address_Numeric;
+using ::picolibrary::I2C::Address_Transmitted;
+using ::picolibrary::Testing::Unit::random;
+
+auto random_address( Address_Transmitted::Unsigned_Integer min = 0b0000000'0, Address_Transmitted::Unsigned_Integer max = 0b1111111'0 )
+{
+    return static_cast<Address_Transmitted::Unsigned_Integer>(
+        random<Address_Transmitted::Unsigned_Integer>( min, max ) & 0b1111111'0 );
+}
+
+auto random_unique_address_pair()
+{
+    auto const a = random_address();
+    auto const b = random_address();
+
+    return std::pair<Address_Transmitted::Unsigned_Integer, Address_Transmitted::Unsigned_Integer>{
+        a, b != a ? b : b ^ random_address( 2 )
+    };
+}
+
+} // namespace
+
+/**
+ * \brief Verify picolibrary::I2C::Address_Transmitted::Address_Transmitted() works
+ *        properly.
+ */
+TEST( constructorDefault, worksProperly )
+{
+    auto const address_transmitted = Address_Transmitted{};
+
+    EXPECT_EQ( address_transmitted.as_unsigned_integer(), 0b0000000'0 );
+}
+
+/**
+ * \brief Verify picolibrary::I2C::Address_Transmitted::Address_Transmitted(
+ *        picolibrary::I2C::Address_Transmitted::Unsigned_Integer ) properly handles an
+ *        invalid address.
+ */
+TEST( constructorUnsignedIntegerDeathTest, invalidAddress )
+{
+    EXPECT_DEATH(
+        {
+            auto const address_transmitted = Address_Transmitted{ static_cast<Address_Transmitted::Unsigned_Integer>(
+                random<Address_Transmitted::Unsigned_Integer>() | 0b1 ) };
+        },
+        "::picolibrary::Generic_Error::INVALID_ARGUMENT" );
+}
+
+/**
+ * \brief Verify picolibrary::I2C::Address_Transmitted::Address_Transmitted(
+ *        picolibrary::I2C::Address_Transmitted::Unsigned_Integer ) works properly.
+ */
+TEST( constructorUnsignedInteger, worksProperly )
+{
+    auto const address = random_address();
+
+    auto const address_transmitted = Address_Transmitted{ address };
+
+    EXPECT_EQ( address_transmitted.as_unsigned_integer(), address );
+}
+
+/**
+ * \brief Verify picolibrary::I2C::Address_Transmitted::Address_Transmitted(
+ *        picolibrary::Bypass_Precondition_Expectation_Checks,
+ *        picolibrary::I2C::Address_Transmitted::Unsigned_Integer ) works properly.
+ */
+TEST( constructorBypassPreconditionExpectationChecks, worksProperly )
+{
+    auto const address = random_address();
+
+    auto const address_transmitted = Address_Transmitted{ BYPASS_PRECONDITION_EXPECTATION_CHECKS, address };
+
+    EXPECT_EQ( address_transmitted.as_unsigned_integer(), address );
+}
+
+/**
+ * \brief Verify picolibrary::I2C::Address_Transmitted::Address_Transmitted(
+ *        picolibrary::I2C::Address_Numeric ) works properly.
+ */
+TEST( constructorAddressNumeric, worksProperly )
+{
+    auto const address = random<Address_Numeric>();
+
+    auto const address_transmitted = Address_Transmitted{ address };
+
+    EXPECT_EQ( address_transmitted.as_unsigned_integer(), address.as_unsigned_integer() << 1 );
+}
+
+/**
+ * \brief Verify picolibrary::I2C::operator==( picolibrary::I2C::Address_Transmitted,
+ *        picolibrary::I2C::Address_Transmitted ) works properly.
+ */
+TEST( equalityOperator, worksProperly )
+{
+    {
+        auto const lhs = random_address();
+        auto const rhs = lhs;
+
+        EXPECT_TRUE( Address_Transmitted{ lhs } == Address_Transmitted{ rhs } );
+    }
+
+    {
+        auto const [ lhs, rhs ] = random_unique_address_pair();
+
+        EXPECT_FALSE( Address_Transmitted{ lhs } == Address_Transmitted{ rhs } );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::I2C::operator!=( picolibrary::I2C::Address_Transmitted,
+ *        picolibrary::I2C::Address_Transmitted ) works properly.
+ */
+TEST( inequalityOperator, worksProperly )
+{
+    {
+        auto const lhs = random_address();
+        auto const rhs = lhs;
+
+        EXPECT_FALSE( Address_Transmitted{ lhs } != Address_Transmitted{ rhs } );
+    }
+
+    {
+        auto const [ lhs, rhs ] = random_unique_address_pair();
+
+        EXPECT_TRUE( Address_Transmitted{ lhs } != Address_Transmitted{ rhs } );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::I2C::operator<( picolibrary::I2C::Address_Transmitted,
+ *        picolibrary::I2C::Address_Transmitted ) works properly.
+ */
+TEST( lessThanOperator, worksProperly )
+{
+    {
+        auto const rhs = random_address( 0b0000000 + 2 );
+        auto const lhs = random_address( 0b0000000, rhs - 2 );
+
+        EXPECT_TRUE( Address_Transmitted{ lhs } < Address_Transmitted{ rhs } );
+    }
+
+    {
+        auto const rhs = random_address();
+        auto const lhs = random_address( rhs );
+
+        EXPECT_FALSE( Address_Transmitted{ lhs } < Address_Transmitted{ rhs } );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::I2C::operator>( picolibrary::I2C::Address_Transmitted,
+ *        picolibrary::I2C::Address_Transmitted ) works properly.
+ */
+TEST( greaterThanOperator, worksProperly )
+{
+    {
+        auto const lhs = random_address( 0b0000000 + 2 );
+        auto const rhs = random_address( 0b0000000, lhs - 2 );
+
+        EXPECT_TRUE( Address_Transmitted{ lhs } > Address_Transmitted{ rhs } );
+    }
+
+    {
+        auto const lhs = random_address();
+        auto const rhs = random_address( lhs );
+
+        EXPECT_FALSE( Address_Transmitted{ lhs } > Address_Transmitted{ rhs } );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::I2C::operator<=( picolibrary::I2C::Address_Transmitted,
+ *        picolibrary::I2C::Address_Transmitted ) works properly.
+ */
+TEST( lessThanOrEqualToOperator, worksProperly )
+{
+    {
+        auto const lhs = random_address();
+        auto const rhs = random_address( lhs );
+
+        EXPECT_TRUE( Address_Transmitted{ lhs } <= Address_Transmitted{ rhs } );
+    }
+
+    {
+        auto const lhs = random_address( 0b0000000 + 2 );
+        auto const rhs = random_address( 0b0000000, lhs - 2 );
+
+        EXPECT_FALSE( Address_Transmitted{ lhs } <= Address_Transmitted{ rhs } );
+    }
+}
+
+/**
+ * \brief Verify picolibrary::I2C::operator>=( picolibrary::I2C::Address_Transmitted,
+ *        picolibrary::I2C::Address_Transmitted ) works properly.
+ */
+TEST( greaterThanOrEqualToOperator, worksProperly )
+{
+    {
+        auto const rhs = random_address();
+        auto const lhs = random_address( rhs );
+
+        EXPECT_TRUE( Address_Transmitted{ lhs } >= Address_Transmitted{ rhs } );
+    }
+
+    {
+        auto const rhs = random_address( 0b0000000 + 2 );
+        auto const lhs = random_address( 0b0000000, rhs - 2 );
+
+        EXPECT_FALSE( Address_Transmitted{ lhs } >= Address_Transmitted{ rhs } );
+    }
+}
+
+/**
+ * \brief Execute the picolibrary::I2C::Address_Transmitted unit tests.
+ *
+ * \param[in] argc The number of arguments to pass to testing::InitGoogleMock().
+ * \param[in] argc The array of arguments to pass to testing::InitGoogleMock().
+ *
+ * \return See Google Test's RUN_ALL_TESTS().
+ */
+int main( int argc, char * argv[] )
+{
+    ::testing::InitGoogleMock( &argc, argv );
+
+    return RUN_ALL_TESTS();
+}
