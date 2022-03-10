3387789d3822bb6968218d16be7a9758c11f3d09
Andrew Countryman <apcountryman@gmail.com>
2022-03-10 09:39:06 -0800

    Add SPI basic controller concept
    Resolves #1124 (Add SPI basic controller concept).

--- include/picolibrary/spi.h
+++ include/picolibrary/spi.h
@@ -23,10 +23,79 @@
 #ifndef PICOLIBRARY_SPI_H
 #define PICOLIBRARY_SPI_H
 
+#include <cstdint>
+
 /**
  * \brief Serial Peripheral Interface (SPI) facilities.
  */
 namespace picolibrary::SPI {
+
+/**
+ * \brief Basic controller concept.
+ */
+class Basic_Controller_Concept {
+  public:
+    /**
+     * \brief Clock (frequency, polarity, and phase), and data exchange bit order
+     *        configuration.
+     */
+    struct Configuration {
+    };
+
+    /**
+     * \brief Constructor.
+     */
+    Basic_Controller_Concept() noexcept;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    Basic_Controller_Concept( Basic_Controller_Concept && source ) noexcept;
+
+    Basic_Controller_Concept( Basic_Controller_Concept const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Basic_Controller_Concept() noexcept;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    auto operator=( Basic_Controller_Concept && expression ) noexcept -> Basic_Controller_Concept &;
+
+    auto operator=( Basic_Controller_Concept const & ) = delete;
+
+    /**
+     * \brief Initialize the controller's hardware.
+     */
+    void initialize() noexcept;
+
+    /**
+     * \brief Configure the controller's clock, and data exchange bit order to meet a
+     *        specific device's communication requirements.
+     *
+     * \param[in] configuration The clock, and data exchange bit order configuration that
+     *            meets the device's communication requirements.
+     */
+    void configure( Configuration const & configuration ) noexcept;
+
+    /**
+     * \brief Exchange data with a device.
+     *
+     * \param[in] data The data to transmit to the device.
+     *
+     * \return The data received from the device.
+     */
+    auto exchange( std::uint8_t data ) noexcept -> std::uint8_t;
+};
+
 } // namespace picolibrary::SPI
 
 #endif // PICOLIBRARY_SPI_H
--- include/picolibrary/testing/unit/spi.h
+++ include/picolibrary/testing/unit/spi.h
@@ -23,10 +23,84 @@
 #ifndef PICOLIBRARY_TESTING_UNIT_SPI_H
 #define PICOLIBRARY_TESTING_UNIT_SPI_H
 
+#include <cstdint>
+
+#include "gmock/gmock.h"
+#include "picolibrary/testing/unit/mock_handle.h"
+
 /**
  * \brief Serial Peripheral Interface (SPI) unit testing facilities.
  */
 namespace picolibrary::Testing::Unit::SPI {
+
+/**
+ * \brief Mock basic controller.
+ */
+class Mock_Basic_Controller {
+  public:
+    using Configuration = std::uint_fast8_t;
+
+    class Handle : public Mock_Handle<Mock_Basic_Controller> {
+      public:
+        using Configuration = Mock_Basic_Controller::Configuration;
+
+        constexpr Handle() noexcept = default;
+
+        constexpr Handle( Mock_Basic_Controller & mock ) noexcept :
+            Mock_Handle<Mock_Basic_Controller>{ mock }
+        {
+        }
+
+        constexpr Handle( Handle && source ) noexcept = default;
+
+        Handle( Handle const & ) = delete;
+
+        ~Handle() noexcept = default;
+
+        constexpr auto operator=( Handle && expression ) noexcept -> Handle & = default;
+
+        auto operator=( Handle const & ) = delete;
+
+        void initialize()
+        {
+            Mock_Handle<Mock_Basic_Controller>::mock().initialize();
+        }
+
+        void configure( Configuration const & configuration )
+        {
+            Mock_Handle<Mock_Basic_Controller>::mock().configure( configuration );
+        }
+
+        auto exchange( std::uint8_t data )
+        {
+            return Mock_Handle<Mock_Basic_Controller>::mock().exchange( data );
+        }
+    };
+
+    Mock_Basic_Controller() = default;
+
+    Mock_Basic_Controller( Mock_Basic_Controller && ) = delete;
+
+    Mock_Basic_Controller( Mock_Basic_Controller const & ) = delete;
+
+    ~Mock_Basic_Controller() noexcept = default;
+
+    auto operator=( Mock_Basic_Controller && ) = delete;
+
+    auto operator=( Mock_Basic_Controller const & ) = delete;
+
+    auto handle() noexcept
+    {
+        return Handle{ *this };
+    }
+
+    MOCK_METHOD( void, initialize, () );
+
+    MOCK_METHOD( void, configure, (Configuration const &));
+
+    MOCK_METHOD( std::uint8_t, exchange, ( std::uint8_t ) );
+};
+
 } // namespace picolibrary::Testing::Unit::SPI
 
 #endif // PICOLIBRARY_TESTING_UNIT_SPI_H
