c459ebf0cba872d71265820100f56d8d800349c8
Andrew Countryman <apcountryman@gmail.com>
2022-03-23 14:00:46 -0700

    Suppress clang-tidy warning
    Supports #1152.

--- source/picolibrary/microchip/mcp23x08.cc
+++ source/picolibrary/microchip/mcp23x08.cc
@@ -24,6 +24,6 @@
 
 namespace picolibrary::Microchip::MCP23X08 {
 
-static_assert( IOCON::Bit::RESERVED6 == 6 );
+static_assert( IOCON::Bit::RESERVED6 == 6 ); // NOLINT(cppcoreguidelines-avoid-magic-numbers)
 
 } // namespace picolibrary::Microchip::MCP23X08
