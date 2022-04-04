e5c6b231b508072197e91ca992ee61669ae220df
Andrew Countryman <apcountryman@gmail.com>
2022-04-03 20:36:22 -0700

    Add Microchip MCP23S08 open-drain I/O pin
    Resolves #1257 (Add Microchip MCP23S08 open-drain I/O pin).

--- include/picolibrary/microchip/mcp23s08.h
+++ include/picolibrary/microchip/mcp23s08.h
@@ -918,6 +918,8 @@ using MCP23X08::Pin;
 
 using MCP23X08::Internally_Pulled_Up_Input_Pin;
 
+using MCP23X08::Open_Drain_IO_Pin;
+
 } // namespace picolibrary::Microchip::MCP23S08
 
 #endif // PICOLIBRARY_MICROCHIP_MCP23S08_H
