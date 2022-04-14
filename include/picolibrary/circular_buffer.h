c3ec19a8a69e8ac4382485d1993de69d0244e894
Andrew Countryman <apcountryman@gmail.com>
2022-04-14 11:20:48 -0700

    Fix inconsistent circular buffer documentation (#1322)
    Resolves #1321 (Fix inconsistent circular buffer documentation).

--- include/picolibrary/circular_buffer.h
+++ include/picolibrary/circular_buffer.h
@@ -28,8 +28,8 @@ namespace picolibrary {
 /**
  * \brief Circular buffer use case.
  *
- * \attention These use cases assume that interrupts that interact with a circular buffer
- *            cannot be interrupted by other interrupts that interact with the same
+ * \attention These use cases assume that interrupts that interact with the circular
+ *            buffer cannot be interrupted by other interrupts that interact with the same
  *            circular buffer.
  */
 enum class Circular_Buffer_Use_Case {
@@ -90,15 +90,16 @@ enum class Circular_Buffer_Use_Case {
  * \brief Circular buffer.
  *
  * \tparam T The circular buffer element type.
- * \tparam Size_Type The unsigned integer type used to track the number of elements in a
+ * \tparam Size_Type The unsigned integer type used to track the number of elements in the
  *         circular buffer.
- * \tparam N The maximum number of elements in a circular buffer (must be a power of two).
+ * \tparam N The maximum number of elements in the circular buffer (must be a power of
+ *         two).
  * \tparam USE_CASE The circular buffer use case.
  * \tparam Interrupt_Controller The type of interrupt controller used by the main thread
  *         of execution to manipulate the interrupt enable state of any interrupts that
  *         also interact with the circular buffer (must be void if either only the main
- *         thread of execution interacts with a circular buffer or only interrupts
- *         interact with a circular buffer).
+ *         thread of execution interacts with the circular buffer or only interrupts
+ *         interact with the circular buffer).
  */
 template<typename T, typename Size_Type, Size_Type N, Circular_Buffer_Use_Case USE_CASE, typename Interrupt_Controller = void>
 class Circular_Buffer;
