fe022a855a24d5037855d51024a69897c39ee720
Andrew Countryman <apcountryman@gmail.com>
2022-04-15 11:47:02 -0700

    Add circular buffer (#1328)
    Resolves #1327 (Add circular buffer).

--- include/picolibrary/circular_buffer.h
+++ include/picolibrary/circular_buffer.h
@@ -24,6 +24,61 @@
 #define PICOLIBRARY_CIRCULAR_BUFFER_H
 
 namespace picolibrary {
+
+/**
+ * \brief Circular buffer use case.
+ *
+ * \attention These use cases assume that interrupts that interact with the circular
+ *            buffer cannot be interrupted by other interrupts that also interact with the
+ *            circular buffer.
+ */
+enum class Circular_Buffer_Use_Case {
+    /**
+     * The main thread of execution reads from and writes to the circular buffer.
+     * Interrupts do not interact with the circular buffer.
+     */
+    MAIN_READS_MAIN_WRITES,
+
+    /**
+     * The main thread of execution reads from the circular buffer. One or more interrupts
+     * write to the circular buffer.
+     */
+    MAIN_READS_INTERRUPTS_WRITE,
+
+    /**
+     * An interrupt reads from the circular buffer. Other interrupts do not interact with
+     * the circular buffer. The main thread of execution writes to the circular buffer.
+     */
+    INTERRUPT_READS_MAIN_WRITES,
+
+    /**
+     * An interrupt reads from the circular buffer. One or more other interrupts write to
+     * the circular buffer. The main thread of execution does not interact with the
+     * circular buffer.
+     */
+    INTERRUPT_READS_INTERRUPTS_WRITE,
+};
+
+/**
+ * \brief Circular buffer.
+ *
+ * \tparam T The circular buffer element type (must be trivially destructible if USE_CASE
+ *         is either picolibrary::Circular_Buffer_Use_Case::INTERRUPT_READS_MAIN_WRITES or
+ *         picolibrary::Circular_Buffer::INTERRUPT_READS_INTERRUPTS_WRITE).
+ * \tparam Size_Type The unsigned integer type used to track the number of elements in the
+ *         circular buffer.
+ * \tparam N The maximum number of elements in the circular buffer (must be a power of
+ *         two).
+ * \tparam USE_CASE The circular buffer use case.
+ * \tparam Interrupt_Controller The type of interrupt controller used by the main thread
+ *         of execution to manipulate the interrupt enable state of any interrupts that
+ *         also interact with the circular buffer (must be void if USE_CASE is either
+ *         picolibrary::Circular_Buffer_Use_Case::MAIN_READS_MAIN_WRITES or
+ *         picolibrary::Circular_Buffer_Use_Case::INTERRUPT_READS_INTERRUPTS_WRITE).
+ */
+template<typename T, typename Size_Type, Size_Type N, Circular_Buffer_Use_Case USE_CASE, typename Interrupt_Controller = void>
+class Circular_Buffer;
+
 } // namespace picolibrary
 
 #endif // PICOLIBRARY_CIRCULAR_BUFFER_H
