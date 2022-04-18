131eecb29afe821679311184fa73b36170cb2b63
Andrew Countryman <apcountryman@gmail.com>
2022-04-18 09:48:23 -0700

    Add interrupt reads, interrupts write circular buffer
    Resolves #1336 (Add interrupt reads, interrupts write circular buffer).

--- include/picolibrary/circular_buffer.h
+++ include/picolibrary/circular_buffer.h
@@ -1325,6 +1325,379 @@ class Circular_Buffer<T, Size_Type, N, Circular_Buffer_Use_Case::INTERRUPT_READS
     }
 };
 
+/**
+ * \brief Circular buffer.
+ *
+ * \tparam T The circular buffer element type (must be trivially destructible).
+ * \tparam Size_Type The unsigned integer type used to track the number of elements in the
+ *         circular buffer.
+ * \tparam N The maximum number of elements in the circular buffer (must be a power of
+ *         two).
+ */
+template<typename T, typename Size_Type, Size_Type N>
+class Circular_Buffer<T, Size_Type, N, Circular_Buffer_Use_Case::MAIN_READS_INTERRUPTS_WRITE, void> {
+  public:
+    static_assert( std::is_trivially_destructible_v<T> );
+
+    static_assert( std::is_unsigned_v<Size_Type> );
+
+    static_assert( is_power_of_two( N ) );
+
+    /**
+     * \brief The circular buffer element type.
+     */
+    using Value = T;
+
+    /**
+     * \brief The number of elements in the circular buffer.
+     */
+    using Size = Size_Type;
+
+    /**
+     * \brief Constructor.
+     */
+    Circular_Buffer() noexcept
+    {
+    }
+
+    Circular_Buffer( Circular_Buffer && ) = delete;
+
+    Circular_Buffer( Circular_Buffer const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Circular_Buffer() noexcept = default;
+
+    auto operator=( Circular_Buffer && ) = delete;
+
+    auto operator=( Circular_Buffer const & ) = delete;
+
+    /**
+     * \brief Access the element at the front of the circular buffer.
+     *
+     * \warning Calling this function on an empty circular buffer results in undefined
+     *          behavior.
+     *
+     * \return The element at the front of the circular buffer.
+     */
+    auto front_from_interrupt() noexcept -> Value volatile &
+    {
+        return *std::launder( reinterpret_cast<Value volatile *>( &m_storage[ m_read ] ) );
+    }
+
+    /**
+     * \brief Access the element at the front of the circular buffer.
+     *
+     * \warning Calling this function on an empty circular buffer results in undefined
+     *          behavior.
+     *
+     * \return The element at the front of the circular buffer.
+     */
+    auto front_from_interrupt() const noexcept -> Value volatile const &
+    {
+        return *std::launder( reinterpret_cast<Value volatile const *>( &m_storage[ m_read ] ) );
+    }
+
+    /**
+     * \brief Access the element at the back of the circular buffer.
+     *
+     * \warning Calling this function on an empty circular buffer results in undefined
+     *          behavior.
+     *
+     * \return The element at the back of the circular buffer.
+     */
+    auto back_from_interrupt() noexcept -> Value volatile &
+    {
+        return *std::launder( reinterpret_cast<Value volatile *>(
+            &m_storage[ ( m_read + size_from_interrupt() - 1 ) & index_wrap_around_mask() ] ) );
+    }
+
+    /**
+     * \brief Access the element at the back of the circular buffer.
+     *
+     * \warning Calling this function on an empty circular buffer results in undefined
+     *          behavior.
+     *
+     * \return The element at the back of the circular buffer.
+     */
+    auto back_from_interrupt() const noexcept -> Value volatile const &
+    {
+        return *std::launder( reinterpret_cast<Value volatile const *>(
+            &m_storage[ ( m_read + size_from_interrupt() - 1 ) & index_wrap_around_mask() ] ) );
+    }
+
+    /**
+     * \brief Check if the circular buffer is empty.
+     *
+     * \return true if the circular buffer is empty.
+     * \return false if the circular buffer is not empty.
+     */
+    [[nodiscard]] auto empty_from_interrupt() const noexcept -> bool
+    {
+        return size_from_interrupt() == 0;
+    }
+
+    /**
+     * \brief Check if the circular buffer is full.
+     *
+     * \return true if the circular buffer is full.
+     * \return false if the circular buffer is not full.
+     */
+    [[nodiscard]] auto full_from_interrupt() const noexcept -> bool
+    {
+        return size_from_interrupt() == max_size_from_interrupt();
+    }
+
+    /**
+     * \brief Get the number of elements in the circular buffer.
+     *
+     * \brief The number of elements in the circular buffer.
+     */
+    auto size_from_interrupt() const noexcept -> Size
+    {
+        return m_size;
+    }
+
+    /**
+     * \brief Get the maximum number of elements the circular buffer is able to hold.
+     *
+     * \return The maximum number of elements the circular buffer is able to hold.
+     */
+    auto max_size_from_interrupt() const noexcept -> Size
+    {
+        return N;
+    }
+
+    /**
+     * \brief Insert a value at the back of the circular buffer.
+     *
+     * \pre not picolibrary::Circular_Buffer::full_from_interrupt()
+     *
+     * \param[in] value The value to insert.
+     */
+    void push_from_interrupt( Value && value ) noexcept
+    {
+        expect( not full_from_interrupt(), Generic_Error::WOULD_OVERFLOW );
+
+        auto const write = Index{ m_write };
+
+        new ( &m_storage[ write ] ) Value{ std::move( value ) };
+
+        m_write = ( write + 1 ) & index_wrap_around_mask();
+
+        ++m_size;
+    }
+
+    /**
+     * \brief Insert a value at the back of the circular buffer.
+     *
+     * \warning Calling this function on a full circular buffer results in undefined
+     *          behavior.
+     *
+     * \param[in] value The value to insert.
+     */
+    void push_from_interrupt( Bypass_Precondition_Expectation_Checks, Value && value ) noexcept
+    {
+        auto const write = Index{ m_write };
+
+        new ( &m_storage[ write ] ) Value{ std::move( value ) };
+
+        m_write = ( write + 1 ) & index_wrap_around_mask();
+
+        ++m_size;
+    }
+
+    /**
+     * \brief Insert a value at the back of the circular buffer.
+     *
+     * \pre not picolibrary::Circular_Buffer::full_from_interrupt()
+     *
+     * \param[in] value The value to insert.
+     */
+    void push_from_interrupt( Value const & value ) noexcept
+    {
+        expect( not full_from_interrupt(), Generic_Error::WOULD_OVERFLOW );
+
+        auto const write = Index{ m_write };
+
+        new ( &m_storage[ write ] ) Value{ value };
+
+        m_write = ( write + 1 ) & index_wrap_around_mask();
+
+        ++m_size;
+    }
+
+    /**
+     * \brief Insert a value at the back of the circular buffer.
+     *
+     * \warning Calling this function on a full circular buffer results in undefined
+     *          behavior.
+     *
+     * \param[in] value The value to insert.
+     */
+    void push_from_interrupt( Bypass_Precondition_Expectation_Checks, Value const & value ) noexcept
+    {
+        auto const write = Index{ m_write };
+
+        new ( &m_storage[ write ] ) Value{ value };
+
+        m_write = ( write + 1 ) & index_wrap_around_mask();
+
+        ++m_size;
+    }
+
+    /**
+     * \brief Emplace a value at the back of the circular buffer.
+     *
+     * \pre not picolibrary::Circular_Buffer::full_from_interrupt()
+     *
+     * \tparam Arguments Value construction argument types.
+     *
+     * \param[in] arguments Value construction arguments.
+     */
+    template<typename... Arguments>
+    void emplace_from_interrupt( Arguments &&... arguments ) noexcept
+    {
+        expect( not full_from_interrupt(), Generic_Error::WOULD_OVERFLOW );
+
+        auto const write = Index{ m_write };
+
+        new ( &m_storage[ write ] ) Value{ std::forward<Arguments>( arguments )... };
+
+        m_write = ( write + 1 ) & index_wrap_around_mask();
+
+        ++m_size;
+    }
+
+    /**
+     * \brief Emplace a value at the back of the circular buffer.
+     *
+     * \pre not picolibrary::Circular_Buffer::full_from_interrupt()
+     *
+     * \tparam Arguments Value construction argument types.
+     *
+     * \param[in] arguments Value construction arguments.
+     */
+    template<typename... Arguments>
+    void emplace_from_interrupt( Run_Precondition_Expectation_Checks, Arguments &&... arguments ) noexcept
+    {
+        expect( not full_from_interrupt(), Generic_Error::WOULD_OVERFLOW );
+
+        auto const write = Index{ m_write };
+
+        new ( &m_storage[ write ] ) Value{ std::forward<Arguments>( arguments )... };
+
+        m_write = ( write + 1 ) & index_wrap_around_mask();
+
+        ++m_size;
+    }
+
+    /**
+     * \brief Emplace a value at the back of the circular buffer.
+     *
+     * \warning Calling this function on a full circular buffer results in undefined
+     *          behavior.
+     *
+     * \tparam Arguments Value construction argument types.
+     *
+     * \param[in] arguments Value construction arguments.
+     */
+    template<typename... Arguments>
+    void emplace_from_interrupt( Bypass_Precondition_Expectation_Checks, Arguments &&... arguments ) noexcept
+    {
+        auto const write = Index{ m_write };
+
+        new ( &m_storage[ write ] ) Value{ std::forward<Arguments>( arguments )... };
+
+        m_write = ( write + 1 ) & index_wrap_around_mask();
+
+        ++m_size;
+    }
+
+    /**
+     * \brief Remove the element at the front of the circular buffer.
+     *
+     * \pre not picolibrary::Circular_Buffer::empty_from_interrupt()
+     */
+    void pop_from_interrupt() noexcept
+    {
+        expect( not empty_from_interrupt(), Generic_Error::WOULD_UNDERFLOW );
+
+        front_from_interrupt().~Value();
+
+        m_read = ( m_read + 1 ) & index_wrap_around_mask();
+
+        --m_size;
+    }
+
+    /**
+     * \brief Remove the element at the front of the circular buffer.
+     *
+     * \warning Calling this function on an empty circular buffer results in undefined
+     *          behavior.
+     */
+    void pop_from_interrupt( Bypass_Precondition_Expectation_Checks ) noexcept
+    {
+        front_from_interrupt().~Value();
+
+        m_read = ( m_read + 1 ) & index_wrap_around_mask();
+
+        --m_size;
+    }
+
+    /**
+     * \brief Remove all circular buffer elements.
+     */
+    void clear_from_interrupt() noexcept
+    {
+        while ( not empty_from_interrupt() ) {
+            pop_from_interrupt( BYPASS_PRECONDITION_EXPECTATION_CHECKS );
+        } // while
+    }
+
+  private:
+    /**
+     * \brief Circular buffer element storage.
+     */
+    using Storage = std::aligned_storage_t<sizeof( Value ), alignof( Value )>;
+
+    /**
+     * \brief Circular buffer element storage index.
+     */
+    using Index = Size;
+
+    /**
+     * \brief The circular buffer element storage read index.
+     */
+    Index m_read{ 0 };
+
+    /**
+     * \brief The circular buffer element storage write index.
+     */
+    Index volatile m_write{ 0 };
+
+    /**
+     * \brief The number of elements in the circular buffer.
+     */
+    Size volatile m_size{ 0 };
+
+    /**
+     * \brief The circular buffer element storage.
+     */
+    Storage volatile m_storage[ N ];
+
+    /**
+     * \brief Get the circular buffer element storage index wrap around mask.
+     *
+     * \return The circular buffer element storage index wrap around mask.
+     */
+    static constexpr auto index_wrap_around_mask() noexcept -> Index
+    {
+        return N - 1;
+    }
+};
+
 } // namespace picolibrary
 
 #endif // PICOLIBRARY_CIRCULAR_BUFFER_H
