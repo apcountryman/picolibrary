172ef7db7403dfffa5925762d522e7c672a1bece
Andrew Countryman <apcountryman@gmail.com>
2022-04-21 10:04:28 -0700

    Add enum underlying type conversion utility
    Resolves #1361 (Add enum underlying type conversion utility).

---
+++ include/picolibrary/utility.h
@@ -0,0 +1,49 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2022, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary utility interface.
+ */
+
+#ifndef PICOLIBRARY_UTILITY_H
+#define PICOLIBRARY_UTILITY_H
+
+#include <type_traits>
+
+namespace picolibrary {
+
+/**
+ * \brief Convert an enum value to its underlying type.
+ *
+ * \tparam Enum The type of enum to convert.
+ *
+ * \param[in] value The enum value to convert.
+ *
+ * \return The enum value converted to its underlying type.
+ */
+template<typename Enum>
+constexpr auto to_underlying( Enum value ) noexcept
+{
+    static_assert( std::is_enum_v<Enum> );
+
+    return static_cast<std::underlying_type_t<Enum>>( value );
+}
+
+} // namespace picolibrary
+
+#endif // PICOLIBRARY_UTILITY_H
