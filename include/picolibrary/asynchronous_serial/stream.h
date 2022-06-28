fe05b64035cc4bd84b080cddd08d58a818f27052
Andrew Countryman <apcountryman@gmail.com>
2022-06-28 13:15:12 -0700

    Add asynchronous serial reliable unbuffered output stream buffer
    Resolves #1522 (Add asynchronous serial reliable unbuffered output
    stream buffer).

--- include/picolibrary/asynchronous_serial/stream.h
+++ include/picolibrary/asynchronous_serial/stream.h
@@ -381,6 +381,151 @@ class Unbuffered_Output_Stream : public Output_Stream {
     Unbuffered_Output_Stream_Buffer<Transmitter> m_buffer{};
 };
 
+/**
+ * \brief Reliable output stream device access buffer.
+ *
+ * \tpamara Transmitter The type of transmitter to abstract with the stream device access
+ *          buffer.
+ */
+template<typename Transmitter>
+class Reliable_Unbuffered_Output_Stream_Buffer final : public Reliable_Stream_Buffer {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Reliable_Unbuffered_Output_Stream_Buffer() noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] transmitter The transmitter to abstract with the stream device access
+     *            buffer.
+     */
+    constexpr Reliable_Unbuffered_Output_Stream_Buffer( Transmitter transmitter ) noexcept :
+        m_transmitter{ std::move( transmitter ) }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Reliable_Unbuffered_Output_Stream_Buffer(
+        Reliable_Unbuffered_Output_Stream_Buffer && source ) noexcept = default;
+
+    Reliable_Unbuffered_Output_Stream_Buffer( Reliable_Unbuffered_Output_Stream_Buffer const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Reliable_Unbuffered_Output_Stream_Buffer() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Reliable_Unbuffered_Output_Stream_Buffer && expression ) noexcept
+        -> Reliable_Unbuffered_Output_Stream_Buffer & = default;
+
+    auto operator=( Reliable_Unbuffered_Output_Stream_Buffer const & ) = delete;
+
+    /**
+     * \brief Transmit a character.
+     *
+     * \param[in] character The character to transmit.
+     */
+    void put( char character ) noexcept override final
+    {
+        m_transmitter.transmit( character );
+    }
+
+    /**
+     * \brief Transmit a block of characters.
+     *
+     * \param[in] begin The beginning of the block of characters to transmit.
+     * \param[in] end The end of the block of characters to transmit.
+     */
+    void put( char const * begin, char const * end ) noexcept override final
+    {
+        m_transmitter.transmit(
+            reinterpret_cast<std::uint8_t const *>( begin ),
+            reinterpret_cast<std::uint8_t const *>( end ) );
+    }
+
+    /**
+     * \brief Transmit a null-terminated string.
+     *
+     * \param[in] string The null-terminated string to transmit.
+     */
+    void put( char const * string ) noexcept override final
+    {
+        while ( auto const character = *string++ ) {
+            m_transmitter.transmit( character );
+        } // while
+    }
+
+    /**
+     * \brief Transmit an unsigned byte.
+     *
+     * \param[in] value The unsigned byte to transmit.
+     */
+    void put( std::uint8_t value ) noexcept override final
+    {
+        m_transmitter.transmit( value );
+    }
+
+    /**
+     * \brief Transmit a block of unsigned bytes.
+     *
+     * \param[in] begin The beginning of the block of unsigned bytes to transmit.
+     * \param[in] end The end of the block of unsigned bytes to transmit.
+     */
+    void put( std::uint8_t const * begin, std::uint8_t const * end ) noexcept override final
+    {
+        m_transmitter.transmit( begin, end );
+    }
+
+    /**
+     * \brief Transmit a signed byte.
+     *
+     * \param[in] value The signed byte to transmit.
+     */
+    void put( std::int8_t value ) noexcept override final
+    {
+        m_transmitter.transmit( value );
+    }
+
+    /**
+     * \brief Transmit a block of signed bytes.
+     *
+     * \param[in] begin The beginning of the block of signed bytes to transmit.
+     * \param[in] end The end of the block of signed bytes to transmit.
+     */
+    void put( std::int8_t const * begin, std::int8_t const * end ) noexcept override final
+    {
+        m_transmitter.transmit(
+            reinterpret_cast<std::uint8_t const *>( begin ),
+            reinterpret_cast<std::uint8_t const *>( end ) );
+    }
+
+    /**
+     * \brief Do nothing.
+     */
+    void flush() noexcept override final
+    {
+    }
+
+  private:
+    /**
+     * \brief The transmitter abstracted by the stream device access buffer.
+     */
+    Transmitter m_transmitter{};
+};
+
 } // namespace picolibrary::Asynchronous_Serial
 
 #endif // PICOLIBRARY_ASYNCHRONOUS_SERIAL_STREAM_H
