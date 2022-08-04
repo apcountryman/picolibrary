c0677b1308a05b29ce493288c3f82fda3582ae0e
Andrew Countryman <apcountryman@gmail.com>
2022-08-04 13:58:56 -0700

    Add WIZnet W5500 interrupt masks (#1619)
    Resolves #1604 (Add WIZnet W5500 interrupt masks).

--- include/picolibrary/wiznet/w5500.h
+++ include/picolibrary/wiznet/w5500.h
@@ -2959,6 +2959,20 @@ enum class Ping_Blocking : MR::Type {
 };
 
 /**
+ * \brief Interrupt masks.
+ */
+struct Interrupt {
+    static constexpr auto WOL_UDP_MAGIC_PACKET_RECEIVED = IR::Mask::MP; ///< WoL UDP magic packet received.
+    static constexpr auto PPPOE_CONNECTION_CLOSED = IR::Mask::PPPOE; ///< PPPoE connection closed.
+    static constexpr auto ICMP_DESTINATION_PORT_UNREACHABLE_PACKET_RECEIVED = IR::Mask::UNREACH; ///< ICMP destination port unreachable packet received.
+    static constexpr auto IP_ADDRESS_CONFLICT = IR::Mask::CONFLICT; ///< IP address conflict.
+
+    static constexpr auto ALL = IR::Type{ WOL_UDP_MAGIC_PACKET_RECEIVED | PPPOE_CONNECTION_CLOSED
+                                          | ICMP_DESTINATION_PORT_UNREACHABLE_PACKET_RECEIVED
+                                          | IP_ADDRESS_CONFLICT };
+};
+
+/**
  * \brief PHY mode.
  */
 enum class PHY_Mode : PHYCFGR::Type {
