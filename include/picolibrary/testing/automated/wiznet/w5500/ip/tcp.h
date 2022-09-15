58f2ebdd975b3bd84bae009594164e35821f3d81
Andrew Countryman <apcountryman@gmail.com>
2022-09-15 12:10:30 -0700

    Add WIZnet W5500 TCP over IP automated testing facilities namespace (#1683)
    Resolves #1677 (Add WIZnet W5500 TCP over IP automated testing
    facilities namespace).

---
+++ include/picolibrary/testing/automated/wiznet/w5500/ip/tcp.h
@@ -0,0 +1,33 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2022, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Testing::Automated::WIZnet::W5500::IP::TCP interface.
+ */
+
+#ifndef PICOLIBRARY_TESTING_AUTOMATED_WIZNET_W5500_IP_TCP_H
+#define PICOLIBRARY_TESTING_AUTOMATED_WIZNET_W5500_IP_TCP_H
+
+/**
+ * \brief WIZnet W5500 Transmission Control Protocol (TCP) over IP automated testing
+ *        facilities.
+ */
+namespace picolibrary::Testing::Automated::WIZnet::W5500::IP::TCP {
+} // namespace picolibrary::Testing::Automated::WIZnet::W5500::IP::TCP
+
+#endif // PICOLIBRARY_TESTING_AUTOMATED_WIZNET_W5500_IP_TCP_H
