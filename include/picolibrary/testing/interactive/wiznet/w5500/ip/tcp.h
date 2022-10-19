5ddba31b16416858833616ce0eeaed509746ea33
Andrew Countryman <apcountryman@gmail.com>
2022-10-19 14:55:08 -0700

    Add WIZnet W5500 TCP over IP client socket echo interactive test helper
    Resolves #1710 (Add WIZnet W5500 TCP over IP client socket echo
    interactive test helper).

--- include/picolibrary/testing/interactive/wiznet/w5500/ip/tcp.h
+++ include/picolibrary/testing/interactive/wiznet/w5500/ip/tcp.h
@@ -23,11 +23,149 @@
 #ifndef PICOLIBRARY_TESTING_INTERACTIVE_WIZNET_W5500_IP_TCP_H
 #define PICOLIBRARY_TESTING_INTERACTIVE_WIZNET_W5500_IP_TCP_H
 
+#include <cstdint>
+#include <utility>
+
+#include "picolibrary/error.h"
+#include "picolibrary/ip/tcp.h"
+#include "picolibrary/ipv4.h"
+#include "picolibrary/mac_address.h"
+#include "picolibrary/precondition.h"
+#include "picolibrary/stream.h"
+#include "picolibrary/testing/interactive/ip/tcp.h"
+#include "picolibrary/wiznet/w5500.h"
+#include "picolibrary/wiznet/w5500/ip.h"
+#include "picolibrary/wiznet/w5500/ip/network_stack.h"
+
 /**
  * \brief WIZnet W5500 Transmission Control Protocol (TCP) over IP interactive testing
  *        facilities.
  */
 namespace picolibrary::Testing::Interactive::WIZnet::W5500::IP::TCP {
+
+/**
+ * \brief Client socket echo interactive test helper.
+ *
+ * \tparam Controller The type of controller used to communicate with the W5500.
+ * \tparam Device_Selector The type of device selector used to select and deselect the
+ *         W5500.
+ *
+ * \param[in] stream The output stream to write information to.
+ * \param[in] controller The controller used to communicate with the W5500.
+ * \param[in] configuration The controller clock and data exchange bit order configuration
+ *            that meets the W5500's communication requirements.
+ * \param[in] device_selector The device selector used to select and deselect the W5500.
+ * \param[in] phy_mode The desired W5500 PHY mode.
+ * \param[in] ping_blocking_configuration The desired W5500 ping blocking configuration.
+ * \param[in] arp_forcing_configuration The desired W5500 ARP forcing configuration.
+ * \param[in] retransmission_retry_time The desired W5500 retransmission retry time (RTR
+ *            register value).
+ * \param[in] retransmission_retry_count The desired W5500 retransmission retry count (RCR
+ *            register value).
+ * \param[in] mac_address The desired W5500 MAC address.
+ * \param[in] ipv4_address The desired W5500 IPv4 address.
+ * \param[in] ipv4_gateway_address The desired W5500 IPv4 gateway address.
+ * \param[in] ipv4_subnet_mask The desired W5500 IPv4 subnet mask.
+ * \param[in] no_delayed_ack_usage_configuration The desired client socket no delayed ACK
+ *            usage configuration.
+ * \param[in] maximum_segment_size The desired client socket maximum segment size.
+ * \param[in] time_to_live The desired client socket IPv4 packet time to live field value.
+ * \param[in] keepalive_period The desired client socket keepalive packet transmission
+ *            period (SN_KPALVTR register value).
+ * \param[in] local_endpoint The local endpoint to bind client sockets to.
+ * \param[in] remote_endpoint The remote endpoint to connect to.
+ */
+template<typename Controller, typename Device_Selector>
+void echo_client(
+    Reliable_Output_Stream &                           stream,
+    Controller                                         controller,
+    typename Controller::Configuration                 configuration,
+    Device_Selector                                    device_selector,
+    ::picolibrary::WIZnet::W5500::PHY_Mode             phy_mode,
+    ::picolibrary::WIZnet::W5500::Ping_Blocking        ping_blocking_configuration,
+    ::picolibrary::WIZnet::W5500::ARP_Forcing          arp_forcing_configuration,
+    std::uint16_t                                      retransmission_retry_time,
+    std::uint8_t                                       retransmission_retry_count,
+    MAC_Address                                        mac_address,
+    IPv4::Address                                      ipv4_address,
+    IPv4::Address                                      ipv4_gateway_address,
+    IPv4::Address                                      ipv4_subnet_mask,
+    ::picolibrary::WIZnet::W5500::No_Delayed_ACK_Usage no_delayed_ack_usage_configuration,
+    std::uint16_t                                      maximum_segment_size,
+    std::uint8_t                                       time_to_live,
+    std::uint8_t                                       keepalive_period,
+    ::picolibrary::IP::TCP::Endpoint const &           local_endpoint,
+    ::picolibrary::IP::TCP::Endpoint const &           remote_endpoint ) noexcept
+{
+    controller.initialize();
+
+    auto w5500 = ::picolibrary::WIZnet::W5500::Driver{ controller, configuration, std::move( device_selector ) };
+
+    w5500.initialize();
+
+    auto network_stack = ::picolibrary::WIZnet::W5500::IP::Network_Stack{
+        w5500,
+        Generic_Error::NONRESPONSIVE_DEVICE,
+        ::picolibrary::WIZnet::W5500::IP::TCP_UDP_Port_Allocator_Without_Ephemeral_Port_Allocation{
+            ::picolibrary::WIZnet::W5500::Socket_Protocol::TCP }
+    };
+
+    expect( network_stack.w5500_is_responsive(), network_stack.nonresponsive_device_error() );
+
+    network_stack.configure_phy( phy_mode );
+
+    network_stack.configure_ping_blocking( ping_blocking_configuration );
+
+    network_stack.configure_arp_forcing( arp_forcing_configuration );
+
+    network_stack.configure_retransmission( retransmission_retry_time, retransmission_retry_count );
+
+    network_stack.configure_mac_address( mac_address );
+
+    network_stack.configure_ipv4_address( ipv4_address );
+    network_stack.configure_ipv4_gateway_address( ipv4_gateway_address );
+    network_stack.configure_ipv4_subnet_mask( ipv4_subnet_mask );
+
+    stream.put( "waiting for link to be established\n" );
+
+    while ( network_stack.link_status() != ::picolibrary::WIZnet::W5500::Link_Status::UP ) {} // while
+
+    auto const link_speed = network_stack.link_speed();
+    auto const link_mode  = network_stack.link_mode();
+
+    // clang-format off
+    stream.print(
+        "link established:\n"
+        "    speed: ", link_speed == ::picolibrary::WIZnet::W5500::Link_Speed::_10_MbPs  ? "10 Mb/s"
+                     : link_speed == ::picolibrary::WIZnet::W5500::Link_Speed::_100_MbPs ? "100 Mb/s"
+                                                                                         : "unknown", "\n"
+        "    mode: ", link_mode == ::picolibrary::WIZnet::W5500::Link_Mode::HALF_DUPLEX ? "half duplex"
+                    : link_mode == ::picolibrary::WIZnet::W5500::Link_Mode::FULL_DUPLEX ? "full duplex"
+                                                                                        : "unknown", "\n"
+        "MAC address: ", mac_address, "\n"
+        "IPv4 address: ", ipv4_address, "\n"
+        "IPv4 gateway address: ", ipv4_gateway_address, "\n"
+        "IPv4 subnet mask: ", ipv4_subnet_mask, "\n"
+    );
+    // clang-format on
+
+    ::picolibrary::Testing::Interactive::IP::TCP::echo_client(
+        stream,
+        network_stack,
+        [ no_delayed_ack_usage_configuration, maximum_segment_size, time_to_live, keepalive_period ](
+            auto & client ) noexcept {
+            client.configure_no_delayed_ack_usage( no_delayed_ack_usage_configuration );
+
+            client.configure_maximum_segment_size( maximum_segment_size );
+
+            client.configure_time_to_live( time_to_live );
+
+            client.configure_keepalive_period( keepalive_period );
+        },
+        local_endpoint,
+        remote_endpoint );
+}
+
 } // namespace picolibrary::Testing::Interactive::WIZnet::W5500::IP::TCP
 
 #endif // PICOLIBRARY_TESTING_INTERACTIVE_WIZNET_W5500_IP_TCP_H
