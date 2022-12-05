f50797103cbec5b0119bbbc5d39f2eab9b846545
Andrew Countryman <apcountryman@gmail.com>
2022-12-05 12:43:05 -0800

    Add null-terminated ROM string facilities
    Resolves #1771 (Add null-terminated ROM string facilities).

--- include/picolibrary/rom.h
+++ include/picolibrary/rom.h
@@ -31,6 +31,39 @@
  * \brief Read-Only Memory (ROM) facilities.
  */
 namespace picolibrary::ROM {
+
+#ifndef PICOLIBRARY_ROM_STRING_IS_HIL_SPECIFIC
+/**
+ * \brief A handle to a null-terminated string that can be stored in ROM.
+ *
+ * To create a string literal that can be placed in ROM, use `PICOLIBRARY_ROM_STRING()`.
+ *
+ * A HIL can replace this type with a HIL specific version by doing the following:
+ * - Configure the `PICOLIBRARY_HIL_INCLUDE_DIR` picolibrary project configuration option
+ * - Provide `picolibrary/hil/rom.h`
+ * - Define `PICOLIBRARY_ROM_STRING_IS_SPECIALIZED` in `picolibrary/hil/rom.h`
+ * - Define the HIL specific version of `::picolibrary::ROM::String` in
+ *   `picolibrary/hil/rom.h`
+ */
+using String = char const *;
+#endif // PICOLIBRARY_ROM_STRING_IS_HIL_SPECIFIC
+
+#ifndef PICOLIBRARY_ROM_STRING
+/**
+ * \brief Create a string literal that can be placed in ROM.
+ *
+ * \param[in] string The string literal that can be placed in ROM.
+ *
+ * \return A handle to the string literal.
+ *
+ * A HIL can replace this macro by doing the following:
+ * - Configure the `PICOLIBRARY_HIL_INCLUDE_DIR` picolibrary project configuration option
+ * - Provide `picolibrary/hil/rom.h`
+ * - Define the macro replacement in `picolibrary/hil/rom.h`
+ */
+#define PICOLIBRARY_ROM_STRING( string ) ( string )
+#endif // PICOLIBRARY_ROM_STRING
+
 } // namespace picolibrary::ROM
 
 #endif // PICOLIBRARY_ROM_H
