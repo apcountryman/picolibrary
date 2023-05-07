a8c7ebd76547bba62ac5453b1888b8e513275608
Andrew Countryman <apcountryman@gmail.com>
2023-05-06 17:52:15 -0700

    Overhaul error code automated tests (#2048)
    Resolves #2046 (Overhaul error code automated tests).

--- test/automated/picolibrary/error_code/main.cc
+++ test/automated/picolibrary/error_code/main.cc
@@ -20,13 +20,10 @@
  * \brief picolibrary::Error_Code automated test program.
  */
 
-#include <string>
-
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "picolibrary/error.h"
 #include "picolibrary/testing/automated/error.h"
-#include "picolibrary/testing/automated/random.h"
 
 namespace {
 
@@ -34,9 +31,6 @@ using ::picolibrary::Error_Code;
 using ::picolibrary::Error_ID;
 using ::picolibrary::Testing::Automated::Mock_Error;
 using ::picolibrary::Testing::Automated::Mock_Error_Category;
-using ::picolibrary::Testing::Automated::random;
-using ::picolibrary::Testing::Automated::random_container;
-using ::picolibrary::Testing::Automated::random_unique_pair;
 using ::testing::Return;
 
 } // namespace
@@ -61,18 +55,18 @@ TEST( constructorDefault, worksProperly )
 TEST( constructorCategoryID, worksProperly )
 {
     auto const category = Mock_Error_Category{};
-    auto const id       = random<Error_ID>();
+    auto const id       = Error_ID{ 195 };
 
     auto const error = Error_Code{ category, id };
 
     EXPECT_EQ( &error.category(), &category );
     EXPECT_EQ( error.id(), id );
 
-    auto const description = random_container<std::string>();
+    auto const description = "aGE931YlH5YAdR";
 
-    EXPECT_CALL( category, error_description( id ) ).WillOnce( Return( description.c_str() ) );
+    EXPECT_CALL( category, error_description( id ) ).WillOnce( Return( description ) );
 
-    EXPECT_STREQ( error.description(), description.c_str() );
+    EXPECT_STREQ( error.description(), description );
 }
 
 /**
@@ -80,93 +74,130 @@ TEST( constructorCategoryID, worksProperly )
  */
 TEST( constructorErrorCodeEnum, worksProperly )
 {
-    auto const id = random<Error_ID>();
+    auto const id = Error_ID{ 49 };
 
     auto const error = Error_Code{ static_cast<Mock_Error>( id ) };
 
     EXPECT_EQ( &error.category(), &Mock_Error_Category::instance() );
     EXPECT_EQ( error.id(), id );
 
-    auto const description = random_container<std::string>();
+    auto const description = "qMiNrCStx5Ch";
 
-    EXPECT_CALL( Mock_Error_Category::instance(), error_description( id ) )
-        .WillOnce( Return( description.c_str() ) );
+    EXPECT_CALL( Mock_Error_Category::instance(), error_description( id ) ).WillOnce( Return( description ) );
 
-    EXPECT_STREQ( error.description(), description.c_str() );
+    EXPECT_STREQ( error.description(), description );
 }
 
 /**
  * \brief Verify picolibrary::operator==( picolibrary::Error_Code const &,
- *        picolibrary::Error_Code const & ) works properly.
+ *        picolibrary::Error_Code const & ) works properly when the categories are equal
+ *        and the IDs are equal.
  */
-TEST( equalityOperator, worksProperly )
+TEST( equalityOperator, worksProperlyCategoryEqualIDEqual )
 {
-    {
-        auto const category = Mock_Error_Category{};
-        auto const id       = random<Error_ID>();
+    auto const category = Mock_Error_Category{};
+    auto const id       = Error_ID{ 204 };
 
-        EXPECT_TRUE( ( Error_Code{ category, id } == Error_Code{ category, id } ) );
-    }
+    ASSERT_TRUE( ( Error_Code{ category, id } == Error_Code{ category, id } ) );
+}
 
-    {
-        auto const category           = Mock_Error_Category{};
-        auto const [ lhs_id, rhs_id ] = random_unique_pair<Error_ID>();
+/**
+ * \brief Verify picolibrary::operator==( picolibrary::Error_Code const &,
+ *        picolibrary::Error_Code const & ) works properly when the categories are equal
+ *        and the IDs are not equal.
+ */
+TEST( equalityOperator, worksProperlyCategoryEqualIDNotEqual )
+{
+    auto const category = Mock_Error_Category{};
+    auto const lhs_id   = Error_ID{ 204 };
+    auto const rhs_id   = Error_ID{ 200 };
 
-        EXPECT_FALSE( ( Error_Code{ category, lhs_id } == Error_Code{ category, rhs_id } ) );
-    }
+    ASSERT_FALSE( ( Error_Code{ category, lhs_id } == Error_Code{ category, rhs_id } ) );
+}
 
-    {
-        auto const lhs_category = Mock_Error_Category{};
-        auto const rhs_category = Mock_Error_Category{};
-        auto const id           = random<Error_ID>();
+/**
+ * \brief Verify picolibrary::operator==( picolibrary::Error_Code const &,
+ *        picolibrary::Error_Code const & ) works properly when the categories are not
+ *        equal and the IDs are equal.
+ */
+TEST( equalityOperator, worksProperlyCategoryNotEqualIDEqual )
+{
+    auto const lhs_category = Mock_Error_Category{};
+    auto const rhs_category = Mock_Error_Category{};
+    auto const id           = Error_ID{ 204 };
 
-        EXPECT_FALSE( ( Error_Code{ lhs_category, id } == Error_Code{ rhs_category, id } ) );
-    }
+    ASSERT_FALSE( ( Error_Code{ lhs_category, id } == Error_Code{ rhs_category, id } ) );
+}
 
-    {
-        auto const lhs_category       = Mock_Error_Category{};
-        auto const rhs_category       = Mock_Error_Category{};
-        auto const [ lhs_id, rhs_id ] = random_unique_pair<Error_ID>();
+/**
+ * \brief Verify picolibrary::operator==( picolibrary::Error_Code const &,
+ *        picolibrary::Error_Code const & ) works properly when the categories are not
+ *        equal and the IDs are not equal.
+ */
+TEST( equalityOperator, worksProperlyCategoryNotEqualIDNotEqual )
+{
+    auto const lhs_category = Mock_Error_Category{};
+    auto const rhs_category = Mock_Error_Category{};
+    auto const lhs_id       = Error_ID{ 204 };
+    auto const rhs_id       = Error_ID{ 200 };
 
-        EXPECT_FALSE( ( Error_Code{ lhs_category, lhs_id } == Error_Code{ rhs_category, rhs_id } ) );
-    }
+    ASSERT_FALSE( ( Error_Code{ lhs_category, lhs_id } == Error_Code{ rhs_category, rhs_id } ) );
 }
 
 /**
  * \brief Verify picolibrary::operator!=( picolibrary::Error_Code const &,
- *        picolibrary::Error_Code const & ) works properly.
+ *        picolibrary::Error_Code const & ) works properly when the categories are equal
+ *        and the IDs are equal.
  */
-TEST( inequalityOperator, worksProperly )
+TEST( inequalityOperator, worksProperlyCategoryEqualIDEqual )
 {
-    {
-        auto const category = Mock_Error_Category{};
-        auto const id       = random<Error_ID>();
+    auto const category = Mock_Error_Category{};
+    auto const id       = Error_ID{ 204 };
 
-        EXPECT_FALSE( ( Error_Code{ category, id } != Error_Code{ category, id } ) );
-    }
+    ASSERT_FALSE( ( Error_Code{ category, id } != Error_Code{ category, id } ) );
+}
 
-    {
-        auto const category           = Mock_Error_Category{};
-        auto const [ lhs_id, rhs_id ] = random_unique_pair<Error_ID>();
+/**
+ * \brief Verify picolibrary::operator!=( picolibrary::Error_Code const &,
+ *        picolibrary::Error_Code const & ) works properly when the categories are equal
+ *        and the IDs are not equal.
+ */
+TEST( inequalityOperator, worksProperlyCategoryEqualIDNotEqual )
+{
+    auto const category = Mock_Error_Category{};
+    auto const lhs_id   = Error_ID{ 204 };
+    auto const rhs_id   = Error_ID{ 200 };
 
-        EXPECT_TRUE( ( Error_Code{ category, lhs_id } != Error_Code{ category, rhs_id } ) );
-    }
+    ASSERT_TRUE( ( Error_Code{ category, lhs_id } != Error_Code{ category, rhs_id } ) );
+}
 
-    {
-        auto const lhs_category = Mock_Error_Category{};
-        auto const rhs_category = Mock_Error_Category{};
-        auto const id           = random<Error_ID>();
+/**
+ * \brief Verify picolibrary::operator!=( picolibrary::Error_Code const &,
+ *        picolibrary::Error_Code const & ) works properly when the categories are not
+ *        equal and the IDs are equal.
+ */
+TEST( inequalityOperator, worksProperlyCategoryNotEqualIDEqual )
+{
+    auto const lhs_category = Mock_Error_Category{};
+    auto const rhs_category = Mock_Error_Category{};
+    auto const id           = Error_ID{ 204 };
 
-        EXPECT_TRUE( ( Error_Code{ lhs_category, id } != Error_Code{ rhs_category, id } ) );
-    }
+    ASSERT_TRUE( ( Error_Code{ lhs_category, id } != Error_Code{ rhs_category, id } ) );
+}
 
-    {
-        auto const lhs_category       = Mock_Error_Category{};
-        auto const rhs_category       = Mock_Error_Category{};
-        auto const [ lhs_id, rhs_id ] = random_unique_pair<Error_ID>();
+/**
+ * \brief Verify picolibrary::operator!=( picolibrary::Error_Code const &,
+ *        picolibrary::Error_Code const & ) works properly when the categories are not
+ *        equal and the IDs are not equal.
+ */
+TEST( inequalityOperator, worksProperlyCategoryNotEqualIDNotEqual )
+{
+    auto const lhs_category = Mock_Error_Category{};
+    auto const rhs_category = Mock_Error_Category{};
+    auto const lhs_id       = Error_ID{ 204 };
+    auto const rhs_id       = Error_ID{ 200 };
 
-        EXPECT_TRUE( ( Error_Code{ lhs_category, lhs_id } != Error_Code{ rhs_category, rhs_id } ) );
-    }
+    ASSERT_TRUE( ( Error_Code{ lhs_category, lhs_id } != Error_Code{ rhs_category, rhs_id } ) );
 }
 
 /**
