00c8c946469cf5657c5c59363d914b721bfe0f70
Andrew Countryman <apcountryman@gmail.com>
2023-05-08 09:48:43 -0700

    Overhaul generic error category automated tests
    Resolves #2061 (Overhaul generic error category automated tests).

--- test/automated/picolibrary/generic_error_category/main.cc
+++ test/automated/picolibrary/generic_error_category/main.cc
@@ -20,6 +20,8 @@
  * \brief picolibrary::Generic_Error_Category automated test program.
  */
 
+#include <ostream>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "picolibrary/error.h"
@@ -31,53 +33,79 @@ using ::picolibrary::Error_ID;
 using ::picolibrary::Generic_Error;
 using ::picolibrary::Generic_Error_Category;
 using ::picolibrary::to_underlying;
+using ::testing::TestWithParam;
+using ::testing::ValuesIn;
 
 } // namespace
 
 /**
+ * \brief picolibrary::Generic_Error_Category::error_description() test case.
+ */
+struct errorDescription_Test_Case {
+    /**
+     * \brief The error ID whose description is to be got.
+     */
+    Error_ID id;
+
+    /**
+     * \brief The error ID's description.
+     */
+    char const * error_description;
+};
+
+auto operator<<( std::ostream & stream, errorDescription_Test_Case const & test_case ) -> std::ostream &
+{
+    return stream << test_case.error_description;
+}
+
+/**
+ * \brief picolibrary::Generic_Error_Category::error_description() test cases.
+ */
+errorDescription_Test_Case const errorDescription_TEST_CASES[]{
+    // clang-format off
+
+    { to_underlying( Generic_Error::ARBITRATION_LOST ),                 "ARBITRATION_LOST"                 },
+    { to_underlying( Generic_Error::BUS_ERROR ),                        "BUS_ERROR"                        },
+    { to_underlying( Generic_Error::ENDPOINT_IN_USE ),                  "ENDPOINT_IN_USE"                  },
+    { to_underlying( Generic_Error::EPHEMERAL_PORTS_EXHAUSTED ),        "EPHEMERAL_PORTS_EXHAUSTED"        },
+    { to_underlying( Generic_Error::INSUFFICIENT_CAPACITY ),            "INSUFFICIENT_CAPACITY"            },
+    { to_underlying( Generic_Error::INSUFFICIENT_SOCKETS_AVAILABLE ),   "INSUFFICIENT_SOCKETS_AVAILABLE"   },
+    { to_underlying( Generic_Error::INVALID_ARGUMENT ),                 "INVALID_ARGUMENT"                 },
+    { to_underlying( Generic_Error::IO_STREAM_DEGRADED ),               "IO_STREAM_DEGRADED"               },
+    { to_underlying( Generic_Error::LOGIC_ERROR ),                      "LOGIC_ERROR"                      },
+    { to_underlying( Generic_Error::NONRESPONSIVE_DEVICE ),             "NONRESPONSIVE_DEVICE"             },
+    { to_underlying( Generic_Error::NOT_CONNECTED ),                    "NOT_CONNECTED"                    },
+    { to_underlying( Generic_Error::NO_SOCKETS_AVAILABLE ),             "NO_SOCKETS_AVAILABLE"             },
+    { to_underlying( Generic_Error::OPERATION_TIMEOUT ),                "OPERATION_TIMEOUT"                },
+    { to_underlying( Generic_Error::OUT_OF_RANGE ),                     "OUT_OF_RANGE"                     },
+    { to_underlying( Generic_Error::RUNTIME_ERROR ),                    "RUNTIME_ERROR"                    },
+    { to_underlying( Generic_Error::UNEXPECTED_EVENT_HANDLING_RESULT ), "UNEXPECTED_EVENT_HANDLING_RESULT" },
+    { to_underlying( Generic_Error::WOULD_BLOCK ),                      "WOULD_BLOCK"                      },
+    { to_underlying( Generic_Error::WOULD_OVERFLOW ),                   "WOULD_OVERFLOW"                   },
+    { to_underlying( Generic_Error::WOULD_UNDERFLOW ),                  "WOULD_UNDERFLOW"                  },
+
+    { to_underlying( Generic_Error::WOULD_UNDERFLOW ) + 1, "UNKNOWN" },
+
+    // clang-format on
+};
+
+/**
+ * \brief picolibrary::Generic_Error_Category::error_description() test fixture.
+ */
+class errorDescription : public TestWithParam<errorDescription_Test_Case> {
+};
+
+INSTANTIATE_TEST_SUITE_P( testCases, errorDescription, ValuesIn( errorDescription_TEST_CASES ) );
+
+/**
  * \brief Verify picolibrary::Generic_Error_Category::error_description() works properly.
  */
-TEST( errorDescription, worksProperly )
+TEST_P( errorDescription, worksProperly )
 {
-    struct {
-        Generic_Error id;
-        char const *  description;
-    } const test_cases[]{
-        // clang-format off
-
-        { Generic_Error::ARBITRATION_LOST,                 "ARBITRATION_LOST"                 },
-        { Generic_Error::BUS_ERROR,                        "BUS_ERROR"                        },
-        { Generic_Error::ENDPOINT_IN_USE,                  "ENDPOINT_IN_USE"                  },
-        { Generic_Error::EPHEMERAL_PORTS_EXHAUSTED,        "EPHEMERAL_PORTS_EXHAUSTED"        },
-        { Generic_Error::INSUFFICIENT_CAPACITY,            "INSUFFICIENT_CAPACITY"            },
-        { Generic_Error::INSUFFICIENT_SOCKETS_AVAILABLE,   "INSUFFICIENT_SOCKETS_AVAILABLE"   },
-        { Generic_Error::INVALID_ARGUMENT,                 "INVALID_ARGUMENT"                 },
-        { Generic_Error::IO_STREAM_DEGRADED,               "IO_STREAM_DEGRADED"               },
-        { Generic_Error::LOGIC_ERROR,                      "LOGIC_ERROR"                      },
-        { Generic_Error::NONRESPONSIVE_DEVICE,             "NONRESPONSIVE_DEVICE"             },
-        { Generic_Error::NOT_CONNECTED,                    "NOT_CONNECTED"                    },
-        { Generic_Error::NO_SOCKETS_AVAILABLE,             "NO_SOCKETS_AVAILABLE"             },
-        { Generic_Error::OPERATION_TIMEOUT,                "OPERATION_TIMEOUT"                },
-        { Generic_Error::OUT_OF_RANGE,                     "OUT_OF_RANGE"                     },
-        { Generic_Error::RUNTIME_ERROR,                    "RUNTIME_ERROR"                    },
-        { Generic_Error::UNEXPECTED_EVENT_HANDLING_RESULT, "UNEXPECTED_EVENT_HANDLING_RESULT" },
-        { Generic_Error::WOULD_BLOCK,                      "WOULD_BLOCK"                      },
-        { Generic_Error::WOULD_OVERFLOW,                   "WOULD_OVERFLOW"                   },
-        { Generic_Error::WOULD_UNDERFLOW,                  "WOULD_UNDERFLOW"                  },
-
-        // clang-format on
-    };
-
-    for ( auto const test_case : test_cases ) {
-        EXPECT_STREQ(
-            Generic_Error_Category::instance().error_description( to_underlying( test_case.id ) ),
-            test_case.description );
-    } // for
+    auto const test_case = GetParam();
 
     EXPECT_STREQ(
-        Generic_Error_Category::instance().error_description(
-            to_underlying( Generic_Error::WOULD_UNDERFLOW ) + 1 ),
-        "UNKNOWN" );
+        Generic_Error_Category::instance().error_description( test_case.id ), test_case.error_description );
 }
 
 /**
