126761b927dc9e60df34477386950b8ccb9566ab
Andrew Countryman <apcountryman@gmail.com>
2023-05-19 09:41:27 -0700

    Add Adafruit PID781 bit rate insertion operator to support automated testing
    Resolves #2101 (Add Adafruit PID781 bit rate insertion operator to
    support automated testing).

--- include/picolibrary/testing/automated/adafruit/pid781.h
+++ include/picolibrary/testing/automated/adafruit/pid781.h
@@ -23,6 +23,43 @@
 #ifndef PICOLIBRARY_TESTING_AUTOMATED_ADAFRUIT_PID781_H
 #define PICOLIBRARY_TESTING_AUTOMATED_ADAFRUIT_PID781_H
 
+#include <ostream>
+
+#include "picolibrary/adafruit/pid781.h"
+
+namespace picolibrary::Adafruit::PID781 {
+
+/**
+ * \brief Insertion operator.
+ *
+ * \param[in] stream The stream to write the picolibrary::Adafruit::PID781::Bit_Rate to.
+ * \param[in] stream The picolibrary::Adafruit::PID781::Bit_Rate to write to the stream.
+ *
+ * \return stream
+ */
+inline auto operator<<( std::ostream & stream, Bit_Rate bit_rate ) -> std::ostream &
+{
+    switch ( bit_rate ) {
+            // clang-format off
+
+        case Bit_Rate::_1200:   return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_1200";
+        case Bit_Rate::_2400:   return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_2400";
+        case Bit_Rate::_4800:   return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_4800";
+        case Bit_Rate::_9600:   return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_9600";
+        case Bit_Rate::_19200:  return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_19200";
+        case Bit_Rate::_28800:  return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_28800";
+        case Bit_Rate::_38400:  return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_38400";
+        case Bit_Rate::_57600:  return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_57600";
+        case Bit_Rate::_115200: return stream << "::picolibrary::Adafruit::PID781::Bit_Rate::_115200";
+
+            // clang-format on
+    } // switch
+
+    return stream << "UNKNOWN";
+}
+
+} // namespace picolibrary::Adafruit::PID781
+
 /**
  * \brief Adafruit PID781 automated testing facilities.
  */
