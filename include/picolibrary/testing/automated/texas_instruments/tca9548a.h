230f9890f880639587f9d6fab3f387e818f09f08
Andrew Countryman <apcountryman@gmail.com>
2023-07-13 15:22:47 -0700

    Add Texas Instruments TCA9548A driver
    Resolves #2341 (Add Texas Instruments TCA9548A driver).

--- include/picolibrary/testing/automated/texas_instruments/tca9548a.h
+++ include/picolibrary/testing/automated/texas_instruments/tca9548a.h
@@ -23,10 +23,48 @@
 #ifndef PICOLIBRARY_TESTING_AUTOMATED_TEXAS_INSTRUMENTS_TCA9548A_H
 #define PICOLIBRARY_TESTING_AUTOMATED_TEXAS_INSTRUMENTS_TCA9548A_H
 
+#include <cstdint>
+#include <functional>
+
+#include "gmock/gmock.h"
+#include "picolibrary/error.h"
+#include "picolibrary/testing/automated/i2c.h"
+#include "picolibrary/texas_instruments/tca9548a.h"
+
 /**
  * \brief Texas Instruments TCA9548A automated testing facilities.
  */
 namespace picolibrary::Testing::Automated::Texas_Instruments::TCA9548A {
+
+/**
+ * \brief Mock driver.
+ */
+class Mock_Driver : public I2C::Mock_Device {
+  public:
+    Mock_Driver() = default;
+
+    Mock_Driver(
+        std::function<void()>,
+        I2C::Mock_Controller &,
+        ::picolibrary::Texas_Instruments::TCA9548A::Address_Transmitted,
+        Error_Code const & )
+    {
+    }
+
+    Mock_Driver( Mock_Driver && ) = delete;
+
+    Mock_Driver( Mock_Driver const & ) = delete;
+
+    ~Mock_Driver() noexcept = default;
+
+    auto operator=( Mock_Driver && ) = delete;
+
+    auto operator=( Mock_Driver const & ) = delete;
+
+    MOCK_METHOD( std::uint8_t, read_control, (), ( const ) );
+    MOCK_METHOD( void, write_control, ( std::uint8_t ) );
+};
+
 } // namespace picolibrary::Testing::Automated::Texas_Instruments::TCA9548A
 
 #endif // PICOLIBRARY_TESTING_AUTOMATED_TEXAS_INSTRUMENTS_TCA9548A_H
