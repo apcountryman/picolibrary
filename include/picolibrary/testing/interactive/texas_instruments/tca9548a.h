892b7cbe10230b64a8fffc7c80cbb5452b63f5c9
Andrew Countryman <apcountryman@gmail.com>
2023-07-14 16:43:51 -0700

    Add Texas Instruments TCA9548A driver scan interactive test helper
    Resolves #2344 (Add Texas Instruments TCA9548A driver scan interactive
    test helper).

--- include/picolibrary/testing/interactive/texas_instruments/tca9548a.h
+++ include/picolibrary/testing/interactive/texas_instruments/tca9548a.h
@@ -23,10 +23,64 @@
 #ifndef PICOLIBRARY_TESTING_INTERACTIVE_TEXAS_INSTRUMENTS_TCA9548A_H
 #define PICOLIBRARY_TESTING_INTERACTIVE_TEXAS_INSTRUMENTS_TCA9548A_H
 
+#include <cstdint>
+#include <limits>
+#include <utility>
+
+#include "picolibrary/error.h"
+#include "picolibrary/format.h"
+#include "picolibrary/i2c.h"
+#include "picolibrary/rom.h"
+#include "picolibrary/stream.h"
+#include "picolibrary/texas_instruments/tca9548a.h"
+
 /**
  * \brief Texas Instruments TCA9548A interactive testing facilities.
  */
 namespace picolibrary::Testing::Interactive::Texas_Instruments::TCA9548A {
+
+/**
+ * \brief Driver scan interactive test helper.
+ *
+ * \tparam Controller The type of controller used to communicate with devices on the bus.
+ *
+ * \param[in] stream The output stream to write the scan results to.
+ * \param[in] controller The controller used to communicate with devices on the bus.
+ * \param[in] tca9548a_address the TCA9548A's address.
+ */
+template<typename Controller>
+void scan(
+    Reliable_Output_Stream &                                        stream,
+    Controller                                                      controller,
+    ::picolibrary::Texas_Instruments::TCA9548A::Address_Transmitted tca9548a_address ) noexcept
+{
+    controller.initialize();
+
+    auto tca9548a = ::picolibrary::Texas_Instruments::TCA9548A::Driver{
+        I2C::Bus_Multiplexer_Aligner{}, controller, std::move( tca9548a_address ), Generic_Error::NONRESPONSIVE_DEVICE
+    };
+
+    for ( auto channel = std::uint_fast8_t{ 0 }; channel < std::numeric_limits<std::uint8_t>::digits;
+          ++channel ) {
+        tca9548a.write_control( 1 << channel );
+
+        stream.print(
+            PICOLIBRARY_ROM_STRING( "channel " ), Format::Dec{ channel }, ":\n" );
+
+        I2C::scan( controller, [ &stream ]( I2C::Address_Numeric address, auto operation, auto response ) noexcept {
+            if ( response == I2C::Response::ACK ) {
+                stream.print(
+                    Format::Hex{ static_cast<std::uint8_t>( address.as_unsigned_integer() ) },
+                    PICOLIBRARY_ROM_STRING( " (" ),
+                    operation == I2C::Operation::READ ? 'R' : 'W',
+                    PICOLIBRARY_ROM_STRING( ")\n" ) );
+            } // if
+        } );
+    } // for
+
+    stream.flush();
+}
+
 } // namespace picolibrary::Testing::Interactive::Texas_Instruments::TCA9548A
 
 #endif // PICOLIBRARY_TESTING_INTERACTIVE_TEXAS_INSTRUMENTS_TCA9548A_H
