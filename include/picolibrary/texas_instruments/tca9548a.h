12704ddca18c15117a9a6b7c5357294575a66ad5
Andrew Countryman <apcountryman@gmail.com>
2023-07-14 08:34:22 -0700

    Add Texas Instruments TCA9548A driver (#2352)
    Resolves #2341 (Add Texas Instruments TCA9548A driver).

--- include/picolibrary/texas_instruments/tca9548a.h
+++ include/picolibrary/texas_instruments/tca9548a.h
@@ -24,7 +24,9 @@
 #define PICOLIBRARY_TEXAS_INSTRUMENTS_TCA9548A_H
 
 #include <cstdint>
+#include <utility>
 
+#include "picolibrary/error.h"
 #include "picolibrary/i2c.h"
 
 /**
@@ -51,6 +53,94 @@ using Address_Numeric = I2C::Device_Address_Numeric<0b1110'000, 0b1110'111>;
  * \brief Address, transmitted format.
  */
 using Address_Transmitted = I2C::Device_Address_Transmitted<0b1110'000'0, 0b1110'111'0>;
+
+/**
+ * \brief Driver.
+ *
+ * \tparam Bus_Multiplexer_Aligner A nullary functor. The functor must be default
+ *         constructable, move constructable, and move assignable. When called, the
+ *         functor should align the bus's multiplexer(s) (if any) to enable communication
+ *         with the TCA9548A.
+ * \tparam Controller The type of controller used to communicate with the TCA9548A.
+ * \tparam Device The type of device implementation used by the driver. The default device
+ *         implementation should be used unless a mock device implementation is being
+ *         injected to support automated testing of this driver.
+ */
+template<typename Bus_Multiplexer_Aligner, typename Controller, typename Device = I2C::Device<Bus_Multiplexer_Aligner, Controller>>
+class Driver : public Device {
+  public:
+    /**
+     * \brief Constructor.
+     */
+    constexpr Driver() noexcept = default;
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] bus_multiplexer_aligner The TCA9548A's bus multiplexer aligner.
+     * \param[in] controller The controller used to communicate with the TCA9548A.
+     * \param[in] address The TCA9548A's address.
+     * \param[in] nonresponsive_device_error The fatal error that occurs if the TCA9548A
+     *            does not respond when addressed or does not acknowledge a write.
+     */
+    constexpr Driver(
+        Bus_Multiplexer_Aligner bus_multiplexer_aligner,
+        Controller &            controller,
+        Address_Transmitted     address,
+        Error_Code const &      nonresponsive_device_error ) noexcept :
+        Device{ std::move( bus_multiplexer_aligner ), controller, std::move( address ), nonresponsive_device_error }
+    {
+    }
+
+    /**
+     * \brief Constructor.
+     *
+     * \param[in] source The source of the move.
+     */
+    constexpr Driver( Driver && source ) noexcept = default;
+
+    Driver( Driver const & ) = delete;
+
+    /**
+     * \brief Destructor.
+     */
+    ~Driver() noexcept = default;
+
+    /**
+     * \brief Assignment operator.
+     *
+     * \param[in] expression The expression to be assigned.
+     *
+     * \return The assigned to object.
+     */
+    constexpr auto operator=( Driver && expression ) noexcept -> Driver & = default;
+
+    auto operator=( Driver const & ) = delete;
+
+    /**
+     * \brief Read the Control register.
+     *
+     * \pre the TCA9548A is responsive
+     *
+     * \return The data read from the Control register.
+     */
+    auto read_control() const noexcept -> std::uint8_t
+    {
+        return this->read();
+    }
+
+    /**
+     * \brief Write to the Control register.
+     *
+     * \pre the TCA9548A is responsive
+     *
+     * \param[in] data The data to write to the Control register.
+     */
+    void write_control( std::uint8_t data ) noexcept
+    {
+        this->write( data );
+    }
+};
 
 } // namespace picolibrary::Texas_Instruments::TCA9548A
 
---
+++ test/automated/picolibrary/texas_instruments/tca9548a/driver/main.cc
@@ -0,0 +1,142 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2023, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Texas_Instruments::TCA9548A::Driver automated test program.
+ */
+
+#include <cstdint>
+#include <functional>
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+#include "picolibrary/testing/automated/error.h"
+#include "picolibrary/testing/automated/i2c.h"
+#include "picolibrary/texas_instruments/tca9548a.h"
+
+namespace {
+
+using ::picolibrary::Testing::Automated::Mock_Error;
+using ::picolibrary::Testing::Automated::I2C::Mock_Controller;
+using ::picolibrary::Testing::Automated::I2C::Mock_Device;
+using ::picolibrary::Texas_Instruments::TCA9548A::Address_Numeric;
+using ::picolibrary::Texas_Instruments::TCA9548A::Address_Transmitted;
+using ::testing::Return;
+using ::testing::TestWithParam;
+using ::testing::ValuesIn;
+
+using Driver = ::picolibrary::Texas_Instruments::TCA9548A::Driver<std::function<void()>, Mock_Controller, Mock_Device>;
+
+} // namespace
+
+/**
+ * \brief picolibrary::Texas_Instruments::TCA9548A::Driver::Driver(
+ *        Bus_Multiplexer_Aligner, Controller &,
+ *        picolibrary::Texas_Instruments::TCA9548A::Address_Transmitted,
+ *        picolibrary::Error_Code const & ) test fixture.
+ */
+class constructor : public TestWithParam<Address_Transmitted> {
+};
+
+/**
+ * \brief Verify picolibrary::Texas_Instruments::TCA9548A::Driver::Driver(
+ *        Bus_Multiplexer_Aligner, Controller &,
+ *        picolibrary::Texas_Instruments::TCA9548A::Address_Transmitted,
+ *        picolibrary::Error_Code const & ) works properly.
+ */
+TEST_P( constructor, worksProperly )
+{
+    auto       controller                 = Mock_Controller{};
+    auto const address                    = GetParam();
+    auto const nonresponsive_device_error = Mock_Error{ 29 };
+
+    auto const tca9548a = ::picolibrary::Texas_Instruments::TCA9548A::Driver{
+        std::function<void()>{}, controller, address, nonresponsive_device_error
+    };
+
+    EXPECT_EQ( tca9548a.address(), address );
+    EXPECT_EQ( tca9548a.nonresponsive_device_error(), nonresponsive_device_error );
+}
+
+/**
+ * \brief picolibrary::Texas_Instruments::TCA9548A::Driver::Driver(
+ *        Bus_Multiplexer_Aligner, Controller &,
+ *        picolibrary::Texas_Instruments::TCA9548A::Address_Transmitted,
+ *        picolibrary::Error_Code const & ) test cases.
+ */
+Address_Transmitted const constructor_TEST_CASES[]{
+    // clang-format off
+
+    { Address_Numeric{ 0b1110'000 } },
+    { Address_Numeric{ 0b1110'001 } },
+    { Address_Numeric{ 0b1110'010 } },
+    { Address_Numeric{ 0b1110'011 } },
+    { Address_Numeric{ 0b1110'100 } },
+    { Address_Numeric{ 0b1110'101 } },
+    { Address_Numeric{ 0b1110'110 } },
+    { Address_Numeric{ 0b1110'111 } },
+
+    // clang-format on
+};
+
+INSTANTIATE_TEST_SUITE_P( testCases, constructor, ValuesIn( constructor_TEST_CASES ) );
+
+/**
+ * \brief Verify picolibrary::Texas_Instruments::TCA9548A::Driver::read_control() works
+ *        properly.
+ */
+TEST( readControl, worksProperly )
+{
+    auto const tca9548a = Driver{};
+
+    auto const data = std::uint8_t{ 0xD6 };
+
+    EXPECT_CALL( tca9548a, read() ).WillOnce( Return( data ) );
+
+    EXPECT_EQ( tca9548a.read_control(), data );
+}
+
+/**
+ * \brief Verify picolibrary::Texas_Instruments::TCA9548A::Driver::write_control() works
+ *        properly.
+ */
+TEST( writeControl, worksProperly )
+{
+    auto tca9548a = Driver{};
+
+    auto const data = std::uint8_t{ 0xFD };
+
+    EXPECT_CALL( tca9548a, write( data ) );
+
+    tca9548a.write_control( data );
+}
+
+/**
+ * \brief Execute the picolibrary::Texas_Instruments::TCA9548A::Driver automated tests.
+ *
+ * \param[in] argc The number of arguments to pass to testing::InitGoogleMock().
+ * \param[in] argc The array of arguments to pass to testing::InitGoogleMock().
+ *
+ * \return See Google Test's RUN_ALL_TESTS().
+ */
+int main( int argc, char * argv[] )
+{
+    ::testing::InitGoogleMock( &argc, argv );
+
+    return RUN_ALL_TESTS();
+}
