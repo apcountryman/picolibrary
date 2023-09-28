cbb0e615d0b84c5aa9a7fa3567e881d10a895c19
Andrew Countryman <apcountryman@gmail.com>
2023-09-28 12:08:19 -0700

    Add WIZnet W5500 UDP over IP automated testing facilities namespace (#2474)
    Resolves #2466 (Add WIZnet W5500 UDP over IP automated testing
    facilities namespace).

---
+++ include/picolibrary/testing/automated/wiznet/w5500/ip/udp.h
@@ -0,0 +1,32 @@
+/**
+ * picolibrary
+ *
+ * Copyright 2020-2023, Andrew Countryman <apcountryman@gmail.com> and the picolibrary
+ * contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+/**
+ * \file
+ * \brief picolibrary::Testing::Automated::WIZnet::W5500::IP::UDP interface.
+ */
+
+#ifndef PICOLIBRARY_TESTING_AUTOMATED_WIZNET_W5500_IP_UDP_H
+#define PICOLIBRARY_TESTING_AUTOMATED_WIZNET_W5500_IP_UDP_H
+
+/**
+ * \brief WIZnet W5500 UDP over IP automated testing facilities.
+ */
+namespace picolibrary::Testing::Automated::WIZnet::W5500::IP::UDP {
+} // namespace picolibrary::Testing::Automated::WIZnet::W5500::IP::UDP
+
+#endif // PICOLIBRARY_TESTING_AUTOMATED_WIZNET_W5500_IP_UDP_H
